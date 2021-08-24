3e8a697ba9b3c13a3499a1750eafe963e5672756
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2021-08-24 04:58:24 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.7 MB (99.98%)

---
+++ my-folder/0066-plus-one/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n = digits.size()-1;
+        int forward = 0;
+        for(int i = n;  i >= 0; i--){
+            forward += digits[i];
+            if(i == n){
+                forward++;
+            }
+
+            digits[i] = forward % 10;
+            forward /= 10;
+        }
+        if(forward != 0) digits.insert(digits.begin(), forward);
+        return digits;
+    }
+};
