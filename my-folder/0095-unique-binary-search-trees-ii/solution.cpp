59069232c57168c7e6105196ce440c70d3032664
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2021-09-02 15:47:55 +0000

    [LeetCode Sync] Runtime - 30 ms (6.87%), Memory - 16.2 MB (100.00%)

---
+++ my-folder/0095-unique-binary-search-trees-ii/solution.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<TreeNode*> generateTrees(int start, int end) {
+        if (start > end) {
+            return { nullptr };
+        }
+        vector<TreeNode*> allTrees;
+        for (int i = start; i <= end; i++) {
+            vector<TreeNode*> leftTrees = generateTrees(start, i - 1);
+            vector<TreeNode*> rightTrees = generateTrees(i + 1, end);
+
+            for (auto& left : leftTrees) {
+                for (auto& right : rightTrees) {
+                    TreeNode* currTree = new TreeNode(i);
+                    currTree->left = left;
+                    currTree->right = right;
+                    allTrees.emplace_back(currTree);
+                }
+            }
+        }
+        return allTrees;
+    }
+
+    vector<TreeNode*> generateTrees(int n) {
+        if (!n) {
+            return {};
+        }
+        return generateTrees(1, n);
+    }
+};
+
