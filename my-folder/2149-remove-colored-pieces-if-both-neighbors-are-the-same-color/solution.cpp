77507ac001e7006b4ed8171276f92372b441c210
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2021-10-16 15:47:09 +0000

    [LeetCode Sync] Runtime - 508 ms (5.02%), Memory - 13.2 MB (100.00%)

---
+++ my-folder/2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool winnerOfGame(string colors) {
+        int a = 0, b = 0;
+        int n = colors.length();
+        int i = 0,j = 0;
+
+        while(i < n) {
+            cout << i << endl;
+            int flg = -1;
+            int tmp_a = 1, tmp_b = 1;
+            for(j = i+1; j < n; j++) {
+                if (colors[i] == colors[j] && colors[i] == 'A') {tmp_a++; flg = 0;}
+                else if (colors[i] == colors[j] && colors[i] == 'B') {tmp_b++; flg = 1;}
+                else break;
+            }
+            cout << tmp_a << ' ' << tmp_b << endl;
+            // cout << a << ' ' << b << endl;
+            if(flg == 0) a += max(0,tmp_a-2);
+            else if(flg == 1) b += max(0,tmp_b-2);
+            i = j;            
+        }
+        return a > b;
+    }
+};
