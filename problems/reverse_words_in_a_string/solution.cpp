6718bbc47bb00598aec7536afceed766f4121217
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2021-10-20 02:35:06 +0000

    Sync LeetCode submission - Reverse Words in a String (cpp)

---
+++ problems/reverse_words_in_a_string/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        stringstream ss;
+        ss << s;
+        string word;
+        string res;
+        stack<string> stk;
+        while(ss >> word) {
+            stk.push(word);
+        }
+        while(stk.size() != 1) {
+            res += stk.top();
+            res += ' ';
+            stk.pop();
+        }
+        res += stk.top();
+        return res;
+    }
+};
