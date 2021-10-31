576ae04df5f4d1d7d90521b94d1ddc62f984cf4f
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2021-10-31 15:17:01 +0000

    Sync LeetCode submission - Flatten a Multilevel Doubly Linked List (cpp)

---
+++ problems/flatten_a_multilevel_doubly_linked_list/solution.cpp
@@ -0,0 +1,38 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* prev;
+    Node* next;
+    Node* child;
+};
+*/
+
+class Solution {
+public:
+    Node* flatten(Node* head) {
+        Node *ptr = head, *tmp_next, *runner;
+        
+        while (ptr) {
+            if (ptr->child) {
+                
+                // Save the current next and connect the child to next
+                tmp_next = ptr->next;
+                ptr->next = ptr->child;
+                ptr->next->prev = ptr;
+                ptr->child = NULL;
+                
+                // Run till the end of the current list and connect last node to saved next
+                runner = ptr->next;
+                while (runner->next) runner = runner->next;
+                runner->next = tmp_next;
+                if (runner->next) runner->next->prev = runner;
+            }
+            
+            ptr = ptr->next;
+        }
+        
+        return head;
+    }
+};
