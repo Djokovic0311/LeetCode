fa03b2a3a566f0feb6918d15a08f7c083ba98c92
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-01-14 02:32:35 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 7 MB (100.00%)

---
+++ my-folder/0008-string-to-integer-atoi/solution.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        int len = s.length();
+        if(len == 0) return 0;
+        int index = 0;
+        bool isNeg = false;
+        while(index < len && s[index]==' ') index++;
+        if(index < len){
+          if(s[index] == '-'){
+            isNeg = true;
+            ++index;
+          } else if (s[index] == '+'){
+              ++index;
+          }
+        }
+    int result = 0;
+
+    while(index < len && isDigit(s[index])){
+      int digit = s[index] - '0';
+      if(result > (INT_MAX / 10) || (result == (INT_MAX / 10) && digit > 7)){
+          return isNeg ? INT_MIN : INT_MAX;
+      }
+      result = (result * 10) + digit; 
+
+      ++index;
+    }
+      
+    return isNeg ? -result : result;
+  }
+    
+private:
+  bool isDigit(char ch){
+    return ch >= '0' && ch <= '9';
+  }
+    
+};
