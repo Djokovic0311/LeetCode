5f660281ac87f9914a1de77038cf1f26eee237bb
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-01-26 04:20:10 +0000

    [LeetCode Sync] Runtime - 251 ms (5.65%), Memory - 85.1 MB (99.75%)

---
+++ my-folder/1427-all-elements-in-two-binary-search-trees/solution.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+     vector<int> res;
+     void dfs(TreeNode *root)
+     {
+          if (!root)
+               return;
+          res.push_back(root->val);
+          dfs(root->left);
+          dfs(root->right);
+     }
+     vector<int> getAllElements(TreeNode *root1, TreeNode *root2)
+     {
+          dfs(root1);
+          dfs(root2);
+          sort(begin(res), end(res));
+          return res;
+     }
+};
