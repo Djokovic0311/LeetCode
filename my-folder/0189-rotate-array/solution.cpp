f4ff444b89c6437e20026e82383deba203bebe22
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-01-30 04:16:37 +0000

    [LeetCode Sync] Runtime - 28 ms (2.17%), Memory - 25.5 MB (100.00%)

---
+++ my-folder/0189-rotate-array/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        vector<int> tmp = nums;
+        int n = nums.size();
+        k %= n;
+        for(int i = 0; i < k; i++) {
+            nums[i] = nums[i+n-k];
+        }
+        for(int i = k; i < n; i++) {
+            nums[i] = tmp[i-k];
+        }
+    }
+};
