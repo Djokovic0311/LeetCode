4a81da0295a47e5e1a9bc4c58d866a305e8a120b
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-02-03 15:23:26 +0000

    [LeetCode Sync] Runtime - 16 ms (5.31%), Memory - 12.4 MB (100.00%)

---
+++ my-folder/1221-element-appearing-more-than-25-in-sorted-array/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findSpecialInteger(vector<int>& arr) {
+		int c = 0;
+		for(int i = 1; i < arr.size(); i++){
+			if(arr[i] == arr[i - 1]){
+				c++;
+			}
+			else{
+				c = 1;
+			}
+			if(c > arr.size() / 4){
+				return arr[i];
+			}
+		}
+		return arr[0];
+    }
+};
