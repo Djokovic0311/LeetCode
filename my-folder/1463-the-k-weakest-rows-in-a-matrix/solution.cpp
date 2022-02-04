2f00b1627740721a11da6f105950925cddb01ac3
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-02-04 09:07:26 +0000

    [LeetCode Sync] Runtime - 12 ms (1.64%), Memory - 10.6 MB (100.00%)

---
+++ my-folder/1463-the-k-weakest-rows-in-a-matrix/solution.cpp
@@ -0,0 +1,49 @@
+struct CompareHeapElements {
+    bool operator()(pair<int,int> const& p1, pair<int,int> const& p2)
+    {
+        if(p1.first<p2.first){
+            return true;
+        }
+        else if(p1.first==p2.first && p1.second<p2.second){
+            return true;
+        }
+        return false;
+    }
+};
+
+class Solution {
+    // calculate the soldier count using binary search
+    int calculateSoldierCount(vector<int>& v) {
+        int l=0; int h=v.size()-1;
+        while(l<=h) {
+            int mid=l+(h-l)/2;
+            if(v[mid]==0) {
+                h=mid-1;
+            } else {
+                l=mid+1;
+            }
+        }
+        return l;
+    }
+    
+public:
+    vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
+
+        priority_queue<pair<int,int>, vector<pair<int,int>>,CompareHeapElements> max_heap;
+        for(int i=0;i<mat.size();++i){
+            max_heap.push({calculateSoldierCount(mat[i]),i});
+            if(max_heap.size()>k){
+                max_heap.pop();
+            }
+        }
+        
+        vector<int>ans;
+        while(max_heap.size()){ // pushing the top k smallest elements of min_heap to "ans"
+            ans.push_back(max_heap.top().second);
+            max_heap.pop();
+        }
+		
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
