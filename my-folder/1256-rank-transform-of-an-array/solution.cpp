db1a265d14d91581cc689ede639500b43b401de6
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-02-04 09:02:41 +0000

    [LeetCode Sync] Runtime - 259 ms (5.06%), Memory - 46.3 MB (20.60%)

---
+++ my-folder/1256-rank-transform-of-an-array/solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
+	set<int> st(arr.begin(),arr.end());
+	unordered_map<int,int> m;
+	int rank=1;
+	for(auto &i: st)  m[i]=rank++;
+	for(auto &i: arr) i = m[i];
+	return arr;
+    }
+};
