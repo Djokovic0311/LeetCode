11536dfa1d741e5bc736d80cda525c4c401d9ff3
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-02-05 04:33:44 +0000

    [LeetCode Sync] Runtime - 8 ms (6.56%), Memory - 10.4 MB (100.00%)

---
+++ my-folder/1468-check-if-n-and-its-double-exist/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        unordered_set<int> set;
+        for(int i=0;i<arr.size();i++){
+            if(set.count(2*arr[i])>0 || ((arr[i]%2==0) && set.count(arr[i]/2)>0))
+                return true;
+            set.insert(arr[i]);
+        }
+        return false;
+    }
+};
