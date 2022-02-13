f0664096284393a5b1cd3dc58852a5c62e207fb0
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-02-13 07:56:56 +0000

    Sync LeetCode submission - Count Common Words With One Occurrence (cpp)

---
+++ problems/count_common_words_with_one_occurrence/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int countWords(vector<string>& words1, vector<string>& words2) {
+    unordered_map<string, int> m;
+    for (auto &w : words1)
+        ++m[w];
+    for (auto &w : words2)
+        if (m[w] < 2)
+            --m[w];
+    return count_if(begin(m), end(m), [](const auto &p){ return p.second == 0; });
+    }
+};
