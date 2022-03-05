383b1bbb86dfd4782b59bb5d1ccac3302d3b8989
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-03-05 02:45:48 +0000

    [LeetCode Sync] Runtime - 14 ms (10.83%), Memory - 13.8 MB (100.00%)

---
+++ my-folder/0740-delete-and-earn/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int deleteAndEarn(vector<int>& nums) {
+        int n = 10001;
+
+        //take the total sum by each number
+        vector<int> sum(n, 0);
+        vector<int> dp(n, 0);
+        for(int num : nums) {
+            sum[num] += num;
+        }
+        dp[0] = 0;
+        dp[1] = sum[1];
+        for(int i = 2; i < n; i++) {
+            dp[i] = max(dp[i-2]+sum[i],dp[i-1]);
+        } 
+        return dp[n-1];
+    }
+};
