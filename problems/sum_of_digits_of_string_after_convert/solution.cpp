491dda24f2a9f4f8d4ca0e361570c10b3f73bfd0
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-03-05 04:03:19 +0000

    Sync LeetCode submission - Sum of Digits of String After Convert (cpp)

---
+++ problems/sum_of_digits_of_string_after_convert/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int getLucky(string s, int k) {
+        int n=s.length();
+        string ans="";
+        for(int i=0;i<n;i++){
+            ans+=to_string(int(s[i])-96);
+        }
+        int temp=0;
+        cout << ans;
+        while(k!=0){
+            for(int i=0;i<ans.length();i++){
+                temp+=(ans[i]-'0');
+            }
+            
+            ans=to_string(temp);
+            temp=0;
+            k--;
+        }
+        int fans=stoi(ans);
+        return fans;        
+        
+    }
+};
