f1d9d1def348519209e63bf880b590645928c8e8
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-03-07 07:32:15 +0000

    [LeetCode Sync] Runtime - 3 ms (3.82%), Memory - 14.8 MB (99.97%)

---
+++ my-folder/0021-merge-two-sorted-lists/solution.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+		if(l1 == NULL){
+			return l2;
+		}
+		if(l2 == NULL){
+			return l1;
+		}
+		ListNode* newHead = NULL;
+		ListNode* newTail = NULL;
+		if(l1->val < l2->val){
+			newHead = l1;
+			newTail = l1;
+			l1 = l1->next;
+		}
+		else{
+			newHead = l2;
+			newTail = l2;
+			l2 = l2->next;
+		}
+		while(l1 != NULL and l2 != NULL){
+			if(l1->val < l2->val){
+				newTail->next = l1;
+				newTail = newTail->next;
+				l1 = l1->next;
+			}
+			else{
+				newTail->next = l2;
+				newTail = newTail->next;
+				l2 = l2->next;
+			}
+		}
+		if(l1 != NULL){
+			newTail->next = l1;
+			l1 = l1->next;
+		}
+		if(l2 != NULL){
+			newTail->next = l2;
+		}
+		return newHead;
+    }
+};
