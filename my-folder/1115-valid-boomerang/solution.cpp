994144224aae9112d92e4d5a1ede41f510866019
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-03-09 02:51:51 +0000

    [LeetCode Sync] Runtime - 4 ms (0.53%), Memory - 10.3 MB (100.00%)

---
+++ my-folder/1115-valid-boomerang/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isBoomerang(vector<vector<int>>& points) {
+        int LHS = (points[0][0] - points[1][0])*(points[0][1]-points[2][1]);
+        int RHS = (points[0][0] - points[2][0])*(points[0][1]-points[1][1]);
+        
+        if(LHS == RHS) {
+            return false;
+        }
+        return true;        
+    }
+};
