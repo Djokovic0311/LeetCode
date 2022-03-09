223a5b7148f56d6872012de30f8410753d9604b2
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-03-09 02:35:34 +0000

    Sync LeetCode submission - Rectangle Overlap (cpp)

---
+++ problems/rectangle_overlap/solution.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2) {
+        int x1 = rec1[0], y1 = rec1[1], x2 = rec1[2], y2 = rec1[3];
+        int x3 = rec2[0], y3 = rec2[1], x4 = rec2[2], y4 = rec2[3];
+        return (x1 < x4 && x3 < x2 && y1 < y4 && y3 < y2);        
+    }
+};
