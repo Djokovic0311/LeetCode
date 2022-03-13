3e8b969b3ba83f3677cfbaa375c741aa9f612f2a
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-03-13 03:09:51 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 6.4 MB (100.00%)

--- my-folder/0020-valid-parentheses/solution.cpp
+++ my-folder/0020-valid-parentheses/solution.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     bool isValid(string s) {
-        int n = s.size();
-        stack<char> st;
-        for(auto i : s) {
-            if(i=='(' or i=='{' or i=='[') st.push(i);
-            else {
-                if(st.empty()or(st.top()=='(' and i!=')') or (st.top()=='{' and i!='}') or (st.top()=='[' and i!=']')) return false;
-                st.pop();
+       stack<char> st;  //taking stack for keep tracking the order of the brackets..
+        for(auto i:s)  //iterate over each and every elements
+        {
+            if(i=='(' or i=='{' or i=='[') st.push(i);  //if current element of the string will be opening bracket then we will just simply push it into the stack
+            else  //if control comes to else part, it means that current element is a closing bracket, so check two conditions  current element matches with top of the stack and the stack must not be empty...
+            {
+                if(st.empty() or (st.top()=='(' and i!=')') or (st.top()=='{' and i!='}') or (st.top()=='[' and i!=']')) return false;
+                st.pop();  //if control reaches to that line, it means we have got the right pair of brackets, so just pop it.
             }
-            
         }
-        return st.empty();
+        return st.empty();          
     }
 };
