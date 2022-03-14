efd5476388d75b947ea8a819b860a8c039729bde
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-03-14 02:44:13 +0000

    [LeetCode Sync] Runtime - 5 ms (0.44%), Memory - 6 MB (100.00%)

---
+++ my-folder/0191-number-of-1-bits/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int cnt=0;  // count of set bit
+        while(n>0){  // iterate until all bits are traversed
+            if((n&1)>0) // check the parity of first bit from right
+                ++cnt;
+            n=n>>1; //n=n/2, shift one bit to right
+        }
+        return cnt;        
+    }
+};
