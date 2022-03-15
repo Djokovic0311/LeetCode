197f7ba3adeefa87216aff01d7a93b481770feab
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-03-15 01:53:15 +0000

    Sync LeetCode submission - Binary Watch (cpp)

---
+++ problems/binary_watch/solution.cpp
@@ -0,0 +1,27 @@
+class Solution 
+{
+    vector<int> hour = {1, 2, 4, 8}, minute = {1, 2, 4, 8, 16, 32};
+public:
+    vector<string> readBinaryWatch(int num) {
+        vector<string> res;
+        helper(res, make_pair(0, 0), num, 0);
+        return res;
+    }
+    
+    void helper(vector<string>& res, pair<int, int> time, int num, int start_point) {
+        if (num == 0) {
+            res.push_back(to_string(time.first) +  (time.second < 10 ?  ":0" : ":") + to_string(time.second));
+            return;
+        }
+        for (int i = start_point; i < hour.size() + minute.size(); i ++)
+            if (i < hour.size()) {    
+                time.first += hour[i];
+                if (time.first < 12)     helper(res, time, num - 1, i + 1);    
+                time.first -= hour[i];
+            } else {     
+                time.second += minute[i - hour.size()];
+                if (time.second < 60)    helper(res, time, num - 1, i + 1);     
+                time.second -= minute[i - hour.size()];
+            }
+    }
+};
