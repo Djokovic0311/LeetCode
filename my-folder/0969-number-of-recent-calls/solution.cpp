7c2485818da81ccbec9b55d190c956d550c1cf8e
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-03-15 02:07:47 +0000

    [LeetCode Sync] Runtime - 255 ms (6.34%), Memory - 57.2 MB (100.00%)

---
+++ my-folder/0969-number-of-recent-calls/solution.cpp
@@ -0,0 +1,20 @@
+class RecentCounter {
+public:
+    deque<int> dq;
+    RecentCounter() {
+        return ;
+    }
+    
+    int ping(int t) {
+        dq.push_back(t);
+        while(!dq.empty() && dq.front()<t-3000)
+            dq.pop_front();
+        return dq.size();
+    }
+};
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter* obj = new RecentCounter();
+ * int param_1 = obj->ping(t);
+ */
