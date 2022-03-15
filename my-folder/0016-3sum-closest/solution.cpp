71d4569874dd23c2f10189aff2aa2fda75496fc5
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-03-15 15:59:12 +0000

    [LeetCode Sync] Runtime - 34 ms (9.25%), Memory - 9.8 MB (100.00%)

---
+++ my-folder/0016-3sum-closest/solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+
+        int sum=nums[0]+nums[1]+nums[2];
+        for(int i=0;i<n-2;i++){ 
+            int j=i+1;
+            int k=n-1;
+            while(j<k){
+                int temp=nums[i]+nums[j]+nums[k];
+                if(abs(temp-target) < abs(sum-target) ) sum=temp;
+                if(temp>target){
+                    k--;                 
+                } else if(temp<target){
+                    j++;             
+                    
+                }else return target;
+            }
+            
+        }
+        return sum;        
+    }
+};
