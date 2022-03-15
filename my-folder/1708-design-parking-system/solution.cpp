b529376ae684185615ed9fa6dd06a5d6aa1f7af7
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-03-15 04:14:12 +0000

    [LeetCode Sync] Runtime - 114 ms (6.48%), Memory - 33.2 MB (100.00%)

---
+++ my-folder/1708-design-parking-system/solution.cpp
@@ -0,0 +1,35 @@
+class ParkingSystem {
+public: vector<int> vehicle;
+public:
+    ParkingSystem(int big, int medium, int small) {
+        vehicle = {big,medium,small};
+    }
+    
+    bool addCar(int carType) {
+        if(carType == 1) {
+            if(vehicle[0] > 0){
+                vehicle[0]--;
+                return true;
+            }
+        }
+        else if(carType == 2) {
+            if(vehicle[1] > 0){
+                vehicle[1]--;
+                return true;
+            }
+        }
+        else if(carType == 3) {
+            if(vehicle[2] > 0){
+                vehicle[2]--;
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem* obj = new ParkingSystem(big, medium, small);
+ * bool param_1 = obj->addCar(carType);
+ */
