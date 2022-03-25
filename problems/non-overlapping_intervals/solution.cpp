f01380a3996ca27da3ae7ce1f1b5747276eafeb9
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-03-25 04:02:20 +0000

    Sync LeetCode submission - Non-overlapping Intervals (cpp)

---
+++ problems/non-overlapping_intervals/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    static bool cmp(vector<int> &a,vector<int> &b) {
+        return a[1]<b[1];
+    }
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        int res = -1;
+        sort(intervals.begin(),intervals.end(),cmp);
+        vector<int> prev = intervals[0];
+        for(vector<int> i : intervals) {
+            if(i[0] < prev[1]) {
+                // prev = i;
+                res++;
+            }
+            else prev = i;
+        }
+        return res;
+    }
+};
