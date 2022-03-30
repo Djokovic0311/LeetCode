5afe9fcc6c419b84703d2391a6df109baf2b5cbf
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-03-30 02:29:07 +0000

    [LeetCode Sync] Runtime - 95 ms (5.24%), Memory - 29.2 MB (25.65%)

---
+++ my-folder/0498-diagonal-traverse/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
+        int m = mat.size(), n = mat[0].size();
+        vector<int> res;
+        map<int, vector<int>>mp;
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                mp[i+j].push_back(mat[i][j]);
+            }
+        }
+                for(auto i:mp)
+        {   if(i.first%2==0)
+            {
+                for(int j=i.second.size()-1;j>=0;j--)
+                    res.push_back(i.second[j]);
+            }
+            else
+            {
+                for(int j=0;j<i.second.size();j++)
+                    res.push_back(i.second[j]);
+            }
+        }
+        return res;
+    }
+};
