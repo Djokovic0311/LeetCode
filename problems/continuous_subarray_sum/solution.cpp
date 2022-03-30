ca6364d44687fd35495540cee52ee4c120f1e6ad
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-03-30 05:50:23 +0000

    Sync LeetCode submission - Continuous Subarray Sum (cpp)

---
+++ problems/continuous_subarray_sum/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        int n = nums.size();
+        int pretemp=0, sum=0;
+        map<int,int> mods;
+        for (int i=0; i<n; i++){
+            sum = (sum + nums[i]) % k;
+            if (mods[sum]) return true;
+            mods[pretemp] ++;
+            pretemp = sum;
+        }
+        return false;
+    }
+};
