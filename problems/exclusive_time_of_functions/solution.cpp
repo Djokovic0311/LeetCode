8f202d20ac0a7e4b2580045ceb488936a1bc4167
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-04-02 09:08:48 +0000

    Sync LeetCode submission - Exclusive Time of Functions (cpp)

---
+++ problems/exclusive_time_of_functions/solution.cpp
@@ -0,0 +1,37 @@
+struct Log {
+    int id;
+    string status;
+    int timestamp;
+};
+class Solution {
+public:
+    vector<int> exclusiveTime(int n, vector<string>& logs) {
+        vector<int> times(n, 0);
+        stack<Log> st;
+        for(string log: logs) {
+            stringstream ss(log);
+            string temp, temp2, temp3;
+            getline(ss, temp, ':');
+            getline(ss, temp2, ':');
+            getline(ss, temp3, ':');
+
+            Log item = {stoi(temp), temp2, stoi(temp3)};
+            if(item.status == "start") {
+                st.push(item);
+            } else {
+                assert(st.top().id == item.id);
+
+                int time_added = item.timestamp - st.top().timestamp + 1;
+                times[item.id] += time_added;
+                st.pop();
+
+                if(!st.empty()) {
+                    assert(st.top().status == "start");
+                    times[st.top().id] -= time_added;
+                }
+            }
+        }
+
+        return times;
+    }
+};
