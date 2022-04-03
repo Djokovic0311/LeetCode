f9e304c36b68fc696439f7c50fb33bdf76df9425
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-04-03 12:13:25 +0000

    Sync LeetCode submission - Split Array into Consecutive Subsequences (cpp)

---
+++ problems/split_array_into_consecutive_subsequences/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isPossible(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        for(auto& it:nums)
+            mp[it]++;
+        for(auto& it:nums)
+        {
+            if(mp[it] == 0)
+                continue;
+            int freq = mp[it] , curr = it , count = 0;
+            while(mp[curr] >= freq)
+            {
+                freq = max(freq,mp[curr]);
+                mp[curr]--;
+                count++;
+                curr++;
+            }
+            if(count < 3)
+                return false;
+        }
+        return true;        
+    }
+};
