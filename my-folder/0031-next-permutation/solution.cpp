2dfba787c01dfbceac47a022c9d9a8278e908325
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-04-03 04:18:51 +0000

    [LeetCode Sync] Runtime - 8 ms (1.42%), Memory - 12.2 MB (100.00%)

--- my-folder/0031-next-permutation/solution.cpp
+++ my-folder/0031-next-permutation/solution.cpp
@@ -1,34 +1,15 @@
 class Solution {
 public:
-    void nextPermutation(vector<int>& nums) {
-    // initialize variable:
-    int breakPoint = -1;
-
-    for (int i = nums.size() - 1; i > 0; i--)
-    {
-        if (nums[i] > nums[i - 1])
+    void nextPermutation(vector<int>& num) {
+       if (num.empty()) return;
+        int i;
+        for (i = num.size()-2; i >= 0; --i)
         {
-            breakPoint = i - 1;
-            break;
+            if (num[i] < num[i+1]) break;
         }
-    }
-
-    // if no breakpoint
-    if (breakPoint < 0)
-    {
-        reverse(nums.begin(), nums.end());
-        return;
-    }
-
-    // if found a breakpoint
-    for (int i = nums.size() - 1; i >= 0; i--)
-    {
-        if (nums[i] > nums[breakPoint])
-        {
-            swap(nums[breakPoint], nums[i]);
-            reverse(nums.begin() + breakPoint + 1, nums.end());
-            break;
-        }
-    }
+        reverse(begin(num)+i+1, end(num));
+        if (i == -1) return;
+        auto itr = upper_bound(begin(num)+i+1, end(num), num[i]);
+        swap(num[i], *itr);        
     }
 };
