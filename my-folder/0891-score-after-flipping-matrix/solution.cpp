7dc7537374b0c316a00f706a37542aa5a84b3065
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-04-11 07:22:48 +0000

    [LeetCode Sync] Runtime - 12 ms (2.92%), Memory - 8.2 MB (100.00%)

---
+++ my-folder/0891-score-after-flipping-matrix/solution.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int matrixScore(vector<vector<int>>& A) {
+       int n=A.size();
+        int m=A[0].size();
+        for(int i=0;i<n;i++){
+            if(A[i][0]==0){
+                for(int j=0;j<m;j++){
+                    A[i][j]=1-A[i][j];
+                }
+                    
+            }
+        }
+        //if any column has more 0 than 1, flip the column
+        for(int j=0;j<m;j++){
+            int cz=0;
+            int co=0;
+            for(int i=0;i<n;i++){
+                if(A[i][j]==1) co++;
+                else cz++;
+            }
+            if(cz>co){
+              for(int i=0;i<n;i++){
+                A[i][j]=1-A[i][j];
+            }  
+            }
+        }
+        int ans=0;
+        for(int i=0;i<n;i++){
+            int p=0;
+            for(int j=m-1;j>=0;j--){
+                ans+=A[i][j]*pow(2,p);
+                p++;
+            }
+        }
+        return ans;        
+    }
+};
