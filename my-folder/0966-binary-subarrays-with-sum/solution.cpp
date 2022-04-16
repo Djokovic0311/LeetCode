01948dd46d065921edee215f85243f4044675584
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-04-16 04:51:27 +0000

    [LeetCode Sync] Runtime - 30 ms (9.59%), Memory - 28.9 MB (100.00%)

---
+++ my-folder/0966-binary-subarrays-with-sum/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int atMost(vector<int>& A, int S) {
+        int i=0,j=0,count=0,sum=0;
+        int size=A.size();
+        if (S < 0)return 0;
+
+        while(j<size){
+            sum+=A[j];
+            while(sum>S ){
+                sum-=A[i];
+                i++;
+            }
+            count+=j-i+1;
+            j++;
+        }
+        return count;
+    }
+
+    int numSubarraysWithSum(vector<int>& A, int S) {
+         return atMost(A, S) - atMost(A, S - 1);
+
+    }
+};
