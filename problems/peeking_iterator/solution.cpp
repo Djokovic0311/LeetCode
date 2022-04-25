d6997f047f8a2f0c7ebad941515ddbfd63913d30
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-04-25 01:40:12 +0000

    Sync LeetCode submission - Peeking Iterator (cpp)

--- problems/peeking_iterator/solution.cpp
+++ problems/peeking_iterator/solution.cpp
@@ -17,31 +17,25 @@
  *	};
  */
 
-class PeekingIterator : public Iterator {
-    int next_val; // keep a variable for storing the "next value" for use in "peek" function
-    bool iter_hasnext; // for storing the "has next value"
+class PeekingIterator : public Iterator
+{
 public:
-	PeekingIterator(const vector<int>& nums) : Iterator(nums) {
-        iter_hasnext=Iterator::hasNext(); // store current has_next value
-	    if (iter_hasnext)
-            next_val = Iterator::next(); // store the "next_val" for "future" use in "peek" function
-	}
-	
-	int peek() {
-        return next_val; // return current next_val
-	}
-	
-	int next() {
-	    int curr_next = next_val; // store the current next for returning
-        
-        iter_hasnext=Iterator::hasNext(); // change the iter_hasnext variable
-	    if (iter_hasnext) 
-            next_val = Iterator::next(); // change the next_val and move the iterator to next position
-        
-	    return curr_next; // return the previously stored current next
-	}
-	
-	bool hasNext() const {
-	    return iter_hasnext; // return current iter_hashnext value
-	}
+    PeekingIterator(const vector<int> &nums) : Iterator(nums)
+    {
+    }
+
+    int peek()
+    {
+        return Iterator(*this).next();
+    }
+
+    int next()
+    {
+        return Iterator::next();
+    }
+
+    bool hasNext() const
+    {
+        return Iterator::hasNext();
+    }
 };
