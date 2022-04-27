5eba00ef133a89a0edb5c173e2e07ad2c43ee51c
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-04-27 07:54:39 +0000

    [LeetCode Sync] Runtime - 20 ms (6.17%), Memory - 10.5 MB (100.00%)

---
+++ my-folder/1184-car-pooling/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool carPooling(vector<vector<int>>& trips, int capacity) {
+        int n = trips.size();
+        vector<pair<int, int>> v;
+        
+        for(int i = 0; i < n; i++){
+            v.push_back({trips[i][1], trips[i][0]});
+            v.push_back({trips[i][2], -trips[i][0]});
+        }
+        
+        sort(v.begin(), v.end());
+        
+        int filled = 0;
+        
+        for(int i = 0; i < v.size(); i++){
+            filled += v[i].second;
+            if(filled > capacity) return false;
+        }
+        return true;
+    }
+};
