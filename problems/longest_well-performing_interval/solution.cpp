82a56007912ee435221f58e734f729085a285b20
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-04-28 11:12:05 +0000

    Sync LeetCode submission - Longest Well-Performing Interval (cpp)

---
+++ problems/longest_well-performing_interval/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int longestWPI(vector<int>& hours) {
+        int res = 0, score = 0, n = hours.size();
+        unordered_map<int, int> seen;
+        for (int i = 0; i < n; ++i) {
+            score += hours[i] > 8 ? 1 : -1;
+            if (score > 0) {
+                res = i + 1;
+            } else {
+                if (seen.find(score) == seen.end())
+                    seen[score] = i;
+                if (seen.find(score - 1) != seen.end())
+                    res = max(res, i - seen[score - 1]);
+            }
+        }
+        return res;
+    }
+};
