f65c7ed20b34180ba8176e77b3a41a2db23e533f
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-04-29 02:08:03 +0000

    Sync LeetCode submission - Is Graph Bipartite? (cpp)

---
+++ problems/is_graph_bipartite?/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n = graph.size();
+        queue<int> q;
+        vector<int> color(n, 0);
+        for(int i = 0; i < n; i++) {
+            if(color[i]) continue;
+            color[i] = 1;
+            q.push(i);
+            while(!q.empty()) {
+                int top = q.front();
+                q.pop();
+                for(auto neighbor : graph[top]) {
+                    if(!color[neighbor]) {
+                        color[neighbor] = -color[top];
+                        q.push(neighbor);
+                    }
+                    else if(color[neighbor] == color[top])
+                        return false;
+                }
+            }
+        }
+        return true;
+    }
+};
