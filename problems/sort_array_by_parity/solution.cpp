5efc80ca58d47144d56696689904b0ae470c3de2
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-05-02 01:22:03 +0000

    Sync LeetCode submission - Sort Array By Parity (cpp)

--- problems/sort_array_by_parity/solution.cpp
+++ problems/sort_array_by_parity/solution.cpp
@@ -1,15 +1,8 @@
 class Solution {
 public:
-    vector<int> sortArrayByParity(vector<int>& nums) {
-        int j=0;
-        for(int i=0;i<nums.size();i++)
-        {
-            if(nums[i]%2==0)
-            {
-                swap(nums[i],nums[j]);
-                j++;
-            }
-        }
-        return nums;        
+    vector<int> sortArrayByParity(vector<int> &A) {
+        for (int i = 0, j = 0; j < A.size(); j++)
+            if (A[j] % 2 == 0) swap(A[i++], A[j]);
+        return A;
     }
 };
