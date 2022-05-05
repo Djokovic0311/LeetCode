a9873112f0ccdd3e95f1549d0117489e7bf779ea
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-05-05 02:18:08 +0000

    Sync LeetCode submission - Implement Stack using Queues (cpp)

--- problems/implement_stack_using_queues/solution.cpp
+++ problems/implement_stack_using_queues/solution.cpp
@@ -1,29 +1,36 @@
 class MyStack {
 public:
-    queue<int> que;
+    /** Initialize your data structure here. */
+    queue<int> q1;
+    queue<int> q2;
     MyStack() {
         
     }
     
+    /** Push element x onto stack. */
     void push(int x) {
-        que.push(x);
-        for(int i=0;i<que.size()-1;++i){
-			que.push(que.front());
-			que.pop();
-		}
+        q2.push(x);
+        while(!q1.empty()){
+            q2.push(q1.front());    q1.pop();
+        }
+        swap(q1, q2);
     }
     
-int pop() {
-    int front = que.front();
-    que.pop();
-    return front;
-}
+    /** Removes the element on top of the stack and returns that element. */
+    int pop() {
+        int result = top();
+        q1.pop();
+        return result;
+    }
+    
+    /** Get the top element. */
     int top() {
-        return que.front();
+        return q1.front();
     }
     
+    /** Returns whether the stack is empty. */
     bool empty() {
-        return que.empty();
+        return q1.empty();
     }
 };
 
