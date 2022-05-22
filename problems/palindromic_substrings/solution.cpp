699adda9bb67f7a6154284797d0ceb742104359d
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-05-22 04:05:22 +0000

    Sync LeetCode submission - Palindromic Substrings (cpp)

---
+++ problems/palindromic_substrings/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+int countSubstrings(string s) {
+        if(s.length()==0) return 0;
+        int res= 0;
+        for(int i=0; i<s.length(); i++) {
+            expandAroundCenter(s, i, i, res);
+            expandAroundCenter(s, i, i+1, res);
+        }
+        return res;
+    }
+    
+    int expandAroundCenter(string s, int left, int right, int& res) {
+        if(s.length()==0 || left>right) return 0;
+        while(left>=0 && right<s.length() && s[left]==s[right]) left--, right++, res++;
+        return res;
+    }
+};
