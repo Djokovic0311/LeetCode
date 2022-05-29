2d334dd22a4c20fe2d83868774d66a7e885b4f1b
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-05-29 18:32:03 +0000

    [LeetCode Sync] Runtime - 371 ms (5.14%), Memory - 97.7 MB (5.73%)

---
+++ my-folder/1464-reduce-array-size-to-the-half/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minSetSize(vector<int>& arr) {
+        unordered_map<int, int>mp;
+        multiset<int, greater<int>> s;
+		for(auto num : arr) mp[num]++;                  
+        for(auto [num, freq] : mp) s.insert(freq);      
+        for(int deleted{}, ans{}; auto freq : s) {     
+            deleted += freq, ans++;
+            if(deleted >= size(arr) / 2) return ans;    
+        }
+        return -1;  // won't ever be reached
+    }
+};
