17d94c71f8fba6470b0f2e0b0609fb76ca0d7448
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-07-09 14:42:54 +0000

    [LeetCode Sync] Runtime - 14 ms (1.43%), Memory - 14.9 MB (100.00%)

---
+++ my-folder/2416-evaluate-boolean-binary-tree/solution.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool evaluateTree(TreeNode* root) {
+        if(root->left == NULL && root->right == NULL)
+            return root->val;
+        if(root->val == 2) {
+            return root->val = evaluateTree(root->left) | evaluateTree(root->right);
+        }
+        if(root->val == 3) {
+            return root->val = evaluateTree(root->left) & evaluateTree(root->right);
+        }
+        
+        return root->val;
+    }
+};
