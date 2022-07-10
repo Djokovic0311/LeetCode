9412b62754aa3c97fecddffbd70332a36a51f35e
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-07-10 04:03:30 +0000

    [LeetCode Sync] Runtime - 170 ms (5.09%), Memory - 46.1 MB (27.53%)

---
+++ my-folder/2413-smallest-number-in-infinite-set/solution.cpp
@@ -0,0 +1,25 @@
+class SmallestInfiniteSet {
+    private:
+    set<int>st;
+public:
+    SmallestInfiniteSet() {
+        for(int i=1;i<1001;i++) st.insert(i);
+    }
+    
+    int popSmallest() {
+        int p = *st.begin();
+        st.erase(st.begin());
+        return p;
+    }
+    
+    void addBack(int num) {
+        st.insert(num);
+    }
+};
+
+/**
+ * Your SmallestInfiniteSet object will be instantiated and called as such:
+ * SmallestInfiniteSet* obj = new SmallestInfiniteSet();
+ * int param_1 = obj->popSmallest();
+ * obj->addBack(num);
+ */
