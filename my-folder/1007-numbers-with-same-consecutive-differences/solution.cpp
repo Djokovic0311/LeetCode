7508c838dddb60c357bd092d0391d7b0fd0a1bfc
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-07-25 01:43:55 +0000

    [LeetCode Sync] Runtime - 17 ms (7.31%), Memory - 7.4 MB (100.00%)

---
+++ my-folder/1007-numbers-with-same-consecutive-differences/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> numsSameConsecDiff(int n, int k) {
+        vector<int> cur = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        for (int i = 2; i <= n; ++i) {
+            vector<int> cur2;
+            for (int x : cur) {
+                int y = x % 10;
+                if (y + k < 10)
+                    cur2.push_back(x * 10 + y + k);
+                if (k > 0 && y - k >= 0)
+                    cur2.push_back(x * 10 + y - k);
+            }
+            for(int t : cur2) {
+                cout << t << ' ' ;
+            }
+            cout << endl;
+            cur = cur2;
+        }
+        return cur;        
+    }
+};
