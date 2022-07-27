7c3ad3868e71b96eb2d6e55ed652dbb3be8c8c4a
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-07-27 04:06:38 +0000

    Sync LeetCode submission - Lexicographical Numbers (cpp)

---
+++ problems/lexicographical_numbers/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> lexicalOrder(int n) {
+        if(n==0) return {};
+        vector<int> res;
+        int cur = 1;
+        for(int i = 0; i < n; i++) {
+            res.push_back(cur);
+            cur *= 10;
+            while(cur>n) {
+                cur /= 10;
+                cur++;
+                while(cur%10==0) cur/=10;
+            }
+        }
+        return res;
+    }
+};
