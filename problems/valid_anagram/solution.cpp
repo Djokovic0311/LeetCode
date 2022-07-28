84f43a0877c07067c4148b3664b773911e63d2ca
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-07-28 02:03:05 +0000

    Sync LeetCode submission - Valid Anagram (cpp)

--- problems/valid_anagram/solution.cpp
+++ problems/valid_anagram/solution.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        vector<int> dict(26,0);
-        int l = s.length();
-        if(t.length() != l) return false;
-        for(int i = 0; i < l; i++) {
-            dict[s[i]-'a']++;
-            dict[t[i]-'a']--;
-        }
-        for(int i = 0; i < 26; i++) {
-            if(dict[i] != 0) return false;
-        }
-        return true;
+        if (s.size()!=t.size()) return false;
+        
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        
+        return s==t;            
     }
 };
