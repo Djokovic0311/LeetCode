567e13a6cc577744ca8925e2f1e66fcfe270f21e
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-07-28 08:31:55 +0000

    Sync LeetCode submission - Maximum Level Sum of a Binary Tree (cpp)

---
+++ problems/maximum_level_sum_of_a_binary_tree/solution.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxLevelSum(TreeNode* root) {
+        int mx = INT_MIN;
+        int curlevel = 0;
+        queue<TreeNode*> q;
+        q.push(root);
+        int level = 0;
+        while(!q.empty()) {
+            int sz = q.size();
+            int cur = 0;
+            for(int i = 0; i < sz; i++) {
+                auto head = q.front();
+                q.pop();
+                cur += head->val;
+                if(head->left) q.push(head->left);
+                if(head->right) q.push(head->right);
+                
+            }
+            level++;
+            cout << cur << ' ' << mx << endl;
+            if(cur > mx) {
+                curlevel = level;
+                mx = cur;
+            }
+            cout << curlevel;
+        }
+        return curlevel;
+    }
+};
