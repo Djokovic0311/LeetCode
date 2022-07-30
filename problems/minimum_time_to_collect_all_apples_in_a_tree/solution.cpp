ad69b12c01514a7ac9e51e837be2aeb938efe02d
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-07-30 07:04:50 +0000

    Sync LeetCode submission - Minimum Time to Collect All Apples in a Tree (cpp)

---
+++ problems/minimum_time_to_collect_all_apples_in_a_tree/solution.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int minTime(int n, vector<vector<int>>& edges, vector<bool>& hasApple) {
+        
+        unordered_map<int, int> nodeParent;
+        for (auto &e : edges){
+            if (nodeParent.find(e[1]) == nodeParent.end()) nodeParent[e[1]] = e[0];
+            else    nodeParent[e[0]] = e[1];
+        }
+        
+        queue<int> q;
+        
+        for (int i = 0; i < hasApple.size(); ++i)
+            if (hasApple[i])  q.push(i);
+        
+        while (!q.empty()){
+            int currIdx = q.front();
+            q.pop();
+            if (currIdx != 0){
+                int nextIdx = nodeParent[currIdx];
+				
+				// if the node has NOT yet visited, then add to the queue 
+                if (!hasApple[nextIdx]){
+                    hasApple[nextIdx] = true;
+                    q.push(nextIdx);
+                }
+            }
+        }
+        
+        int countPath = accumulate(hasApple.begin(), hasApple.end(), -1);
+        
+        return countPath <= 0 ? 0 : 2 * countPath;
+    }
+};
