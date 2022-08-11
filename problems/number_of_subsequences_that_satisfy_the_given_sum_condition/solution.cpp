b3a57678808758bc3823d3c43d2cb221dd031a09
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-08-11 03:19:54 +0000

    Sync LeetCode submission - Number of Subsequences That Satisfy the Given Sum Condition (cpp)

---
+++ problems/number_of_subsequences_that_satisfy_the_given_sum_condition/solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int numSubseq(vector<int>& nums, int target) {
+        int MOD = 1e9 + 7;
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        int l = 0, r = n-1;
+        vector<int>pow2(n, 1);
+        
+        sort(nums.begin(),nums.end());
+        for(int i = 1; i < n; i++) {
+            pow2[i] = 2 * pow2[i-1]% MOD;
+        }
+        int res = 0;
+        while(l <= r) {
+            if(nums[l] + nums[r] <= target) {
+                res = (res+pow2[r-l++]) % MOD;
+            }
+            else{
+                r--;
+            }
+        }
+        return res;
+    }
+};
