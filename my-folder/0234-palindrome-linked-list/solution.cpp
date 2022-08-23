9c665ea363ba90bbadb86d0ec20fc9acb9340980
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-08-23 04:33:36 +0000

    [LeetCode Sync] Runtime - 476 ms (5.08%), Memory - 121 MB (56.49%)

--- my-folder/0234-palindrome-linked-list/solution.cpp
+++ my-folder/0234-palindrome-linked-list/solution.cpp
@@ -10,17 +10,17 @@
  */
 class Solution {
 public:
+    ListNode* temp;
+    
+    bool check(ListNode* head) {
+        if (NULL == head) return true;
+        bool res = check(head->next) & (temp->val == head->val);
+        temp = temp->next;
+        return res;
+    }
+    
     bool isPalindrome(ListNode* head) {
-        ListNode *slow = head, *fast = head, *prev, *temp;
-        while (fast && fast->next)
-            slow = slow->next, fast = fast->next->next;
-        prev = slow, slow = slow->next, prev->next = NULL;
-        while (slow)
-            temp = slow->next, slow->next = prev, prev = slow, slow = temp;
-        fast = head, slow = prev;
-        while (slow)
-            if (fast->val != slow->val) return false;
-            else fast = fast->next, slow = slow->next;
-        return true;        
+        temp = head;
+        return check(head);
     }
 };
