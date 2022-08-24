3b0def44ccf2e73b96cb8bd3e3be2b4657b55dfb
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-08-24 01:31:34 +0000

    [LeetCode Sync] Runtime - 32 ms (6.71%), Memory - 6.1 MB (100.00%)

--- my-folder/0326-power-of-three/solution.cpp
+++ my-folder/0326-power-of-three/solution.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        if(n==0) return false;
-        while(n % 3 == 0) n /= 3;
-        return n == 1;
+    double res=log10(n)/log10(3);
+    return (n > 0 ) && (res == (int)res);        
     }
 };
