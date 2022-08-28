9cd62f4d1e14e58c9539055d9735b06c2c6258b3
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-08-28 18:49:48 +0000

    [LeetCode Sync] Runtime - 16 ms (6.01%), Memory - 8.7 MB (100.00%)

--- my-folder/1253-sort-the-matrix-diagonally/solution.cpp
+++ my-folder/1253-sort-the-matrix-diagonally/solution.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
-    unordered_map<int, vector<int>> mp;
     vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
-        int m=mat.size();
-        int n=mat[0].size();
-
-        for(int i=0;i<m;i++) {
-            for(int j=0;j<n;j++) {
-                mp[i-j].push_back(mat[i][j]);
-            }
+        vector<int> k;
+        int m = mat.size() , n = mat[0].size();
+        for (int r=0; r<m; r++)        // r-> row
+        {
+            k.clear();
+            for (int j=0,i=r; j<n && i<m ; j++,i++) k.push_back(mat[i][j]);    // add to vector
+            sort(k.begin(),k.end());
+            for (int j=0,i=r; j<n && i<m ; j++,i++) mat[i][j]=k[j];            // replace
         }
-
-        for(int k=-(n-1);k<m;k++) {
-            sort(mp[k].begin(),mp[k].end());
-        }
-
-        for(int i=m-1;i>=0;i--) {
-            for(int j=n-1;j>=0;j--) {
-                mat[i][j]=mp[i-j].back();
-                mp[i-j].pop_back();
-            }
+        for (int c=1; c<n; c++)       // c->column
+        {
+            k.clear();
+            for (int i=0, j=c; j<n && i<m; i++, j++) k.push_back(mat[i][j]);   // add to vector
+            sort(k.begin(),k.end());
+            for (int i=0, j=c; j<n && i<m; i++, j++) mat[i][j] = k[i];         //replace
         }
         return mat;
     }
 };
+
