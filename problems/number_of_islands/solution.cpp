c9c6a8d0c6a14c24fff0dafdb73a20c58acf6aaa
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-08-29 14:44:27 +0000

    Sync LeetCode submission - Number of Islands (cpp)

--- problems/number_of_islands/solution.cpp
+++ problems/number_of_islands/solution.cpp
@@ -1,39 +1,32 @@
 class Solution {
 public:
-    void DFS(vector<vector<char>>& grid, int i, int j) {
-        // boundary checking
-        if(i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size())
-            return;
-        // return if current position is of water or is already visited
-        if(grid[i][j] == '2' || grid[i][j] == '0')
-            return;
-        
-        // mark the current as visited
-        grid[i][j] = '2';
-        
-        // do DFS in all 4 directions
-        DFS(grid, i+1, j);
-        DFS(grid, i, j-1);
-        DFS(grid, i-1, j);
-        DFS(grid, i, j+1);
-    }
-    
+    void explore(vector<vector<char>>& grid, int r, int c, int R, int C){
+        if(r < 0 || r >= R || c < 0 || c >= C) return;
+        if(grid[r][c] == '0') return;
+        if(grid[r][c] == '1'){
+            grid[r][c] = 'm';
+            explore(grid, r+1, c, R, C);
+            explore(grid, r, c+1, R, C);
+            explore(grid, r-1, c, R, C);
+            explore(grid, r, c-1, R, C);
+        }
+        return;
+    } 
+
     int numIslands(vector<vector<char>>& grid) {
-        // We can treat the matrix grid as a grid. Each Island is a
-        // connected component. The task is to find no. of disconnectedd components
-        // in the graph.
-        
-        int islands = 0;
-        // We make each 1 as 2 in when it is visited
-        for(int i = 0; i < grid.size(); i++) {
-            for(int j = 0; j < grid[0].size(); j++) {
-                // do DFS in case has not been visited and there is land
-                if(grid[i][j] == '1') {
-                    DFS(grid, i, j);
-                    ++islands;
-                } 
+        int count = 0;
+        int R = grid.size();
+        int C = grid[0].size();
+
+        for(int i=0;i<R;i++){
+            for(int j=0;j<C;j++){
+                if(grid[i][j] == 'm') continue;
+                if(grid[i][j] == '1'){
+                    count++;
+                    explore(grid, i,j, R, C);
+                }
             }
         }
-        return islands;
+        return count;
     }
 };
