429c96981c7dbb81a5358062cd3349a6b6f1d4fe
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-09-07 03:08:51 +0000

    Sync LeetCode submission - Construct String from Binary Tree (cpp)

--- problems/construct_string_from_binary_tree/solution.cpp
+++ problems/construct_string_from_binary_tree/solution.cpp
@@ -11,29 +11,12 @@
  */
 class Solution {
 public:
-    void traverse(TreeNode* root, string &res){
-        if(root == NULL || (root->left == NULL && root->right == NULL))
-            return;
-        
-        if(root->left == NULL && root->right!=NULL){
-            res+="()";
-        }
-        if(root->left){
-            res+="(" + to_string(root->left->val);
-            traverse(root->left,res);
-            res.push_back(')');
-        }
-        if(root->right){
-            res+="(" + to_string(root->right->val);
-            traverse(root->right,res);
-            res.push_back(')');
-        } 
-        return;            
-    }
-    string tree2str(TreeNode* root) {
-        string res = "";
-        res+=to_string(root->val);
-        traverse(root,res);
-        return res;
+    string tree2str(TreeNode* t) {
+        if (t == NULL) return "";
+        string s = to_string(t->val);
+        if (t->left) s += '(' + tree2str(t->left) + ')';
+        else if (t->right) s += "()";
+        if (t->right) s += '(' + tree2str(t->right) + ')';
+        return s;
     }
 };
