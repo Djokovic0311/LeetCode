50a7bee294834edb5041cd9e1e1a64be69f594e3
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-09-12 21:50:21 +0000

    [LeetCode Sync] Runtime - 165 ms (6.47%), Memory - 14.6 MB (95.30%)

--- my-folder/0322-coin-change/solution.cpp
+++ my-folder/0322-coin-change/solution.cpp
@@ -1,30 +1,19 @@
 class Solution {
 public:
-    int dp[10000 + 1][12 + 1];
-    
-    int tabulation(vector<int> wt, int w, int n)
-    {
-        for (int i = 0; i < w + 1; i++)
-            for (int j = 0; j < n + 1; j++)
-                if (i == 0 || j == 0)
-                    dp[i][j] = (i == 0) ? 0 : INT_MAX - 1;
-        
-        for (int i = 1; i < w + 1; i++) {
-            for (int j = 1; j < n + 1; j++) {
-                if (wt[j - 1] > i) 
-                    dp[i][j] = 0 + dp[i - 0][j - 1];
-                else 
-                    dp[i][j] = min(0 + dp[i - 0][j - 1], 1 + dp[i - wt[j - 1]][j - 0]);
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        vector<int> dp(++amount);
+        dp[0] = 0;
+        sort(coins.begin(), coins.end());
+        for(int i = 1; i < amount; i++) {
+            dp[i] = INT_MAX;
+            for(int j = 0; j < n; j++) {
+                if(i-coins[j]<0) break;
+                if(dp[i-coins[j]] != INT_MAX)
+                    dp[i] = min(dp[i], dp[i-coins[j]]+1);
+                
             }
         }
-        
-        return dp[w][n];
-    }
-    
-    int coinChange(vector<int>& coins, int amount) 
-    {
-        memset(dp, -1, sizeof(dp));
-        int minCoins = tabulation(coins, amount, coins.size());
-        return minCoins == INT_MAX - 1 ? -1 : minCoins;    
+        return dp[--amount] == INT_MAX ? -1: dp[amount];
     }
 };
