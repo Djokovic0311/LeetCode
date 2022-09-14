fa8fa04e449f1908e6a175a3f34250d91ab75815
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-09-14 03:14:50 +0000

    Sync LeetCode submission - Word Break (cpp)

--- problems/word_break/solution.cpp
+++ problems/word_break/solution.cpp
@@ -4,13 +4,12 @@ public:
         vector<bool> dp(s.size()+1, false);
         dp[0] = true;
         
-        // we mark as true every index that we managed to segment so far
         for (int i = 1; i <= s.size(); i++)
             for (int j = 0; j < i; j++)
                 if ((dp[j]) && (find(wordDict.begin(), wordDict.end(), s.substr(j, i-j)) != wordDict.end())) {
                     dp[i] = true;
                     break;
                 }
-        return dp.back();
+        return dp.back();        
     }
 };
