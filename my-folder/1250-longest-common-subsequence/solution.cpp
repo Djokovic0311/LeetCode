e9e81921bccb01fa4bd8e7c13e8062597bc895ee
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-09-14 02:54:25 +0000

    [LeetCode Sync] Runtime - 64 ms (5.14%), Memory - 13.1 MB (82.95%)

--- my-folder/1250-longest-common-subsequence/solution.cpp
+++ my-folder/1250-longest-common-subsequence/solution.cpp
@@ -1,14 +1,21 @@
 class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) {
-        int m = text1.length();
-        int n = text2.length();
-        
-        vector<vector<short>> pic(m + 1, vector<short>(n+ 1));
-          for (auto i = 1; i <= m; ++i)
-            for (auto j = 1; j <= n; ++j)
-              if (text1[i - 1] == text2[j - 1]) pic[i][j] = pic[i - 1][j - 1] + 1;
-              else pic[i][j] = max(pic[i - 1][j], pic[i][j - 1]);
-          return pic[m][n];
+        int m = text1.size(), n = text2.size();
+        vector<vector<int>> dp(m+1, vector<int>(n+1));
+        for(int i = 0; i < m; i++) {
+            dp[i][0] = 0;
+        }
+        for(int j = 0; j < n; j++) {
+            dp[0][j] = 0;
+        }
+        for(int i = 1; i < m+1; i++) {
+            for(int j = 1; j < n+1; j++) {
+                if(text1[i-1] == text2[j-1])
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        return dp[m][n];
     }
 };
