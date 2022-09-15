1fff522ee696d47e4a6e39238874be89972d6a64
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-09-15 03:37:52 +0000

    [LeetCode Sync] Runtime - 3 ms (39.28%), Memory - 6.6 MB (100.00%)

--- my-folder/0377-combination-sum-iv/solution.cpp
+++ my-folder/0377-combination-sum-iv/solution.cpp
@@ -1,11 +1,16 @@
 class Solution {
 public:
     int combinationSum4(vector<int>& nums, int target) {
+        int n = nums.size();
         vector<unsigned int> dp(target+1,0);
         dp[0] = 1;
-        for (int i = 1; i <= target; i++)
-            for (int num : nums)
-                if (num <= i) dp[i] += dp[i-num];
-        return dp[target];        
+        sort(nums.begin(), nums.end());
+        for(int i = 1; i <= target; i++) {
+            for(int j = 0; j < n; j++) {
+                if(nums[j] <= i) 
+                    dp[i] += dp[i-nums[j]];
+            }
+        }
+        return dp[target];
     }
 };
