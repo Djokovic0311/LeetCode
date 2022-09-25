ae1e7a204e9e5767bf191d26623da3f0ace76b2d
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-09-25 02:22:14 +0000

    Sync LeetCode submission - Design Circular Queue (cpp)

--- problems/design_circular_queue/solution.cpp
+++ problems/design_circular_queue/solution.cpp
@@ -4,35 +4,29 @@ public:
         data.resize(k);
         maxSize = k;
     }
-    
-    bool enQueue(int value) {
+    bool enQueue(int val) {
         if (isFull()) return false;
         tail = (tail + 1) % maxSize;
-        data[tail] = value;
-        return true;        
+        data[tail] = val;
+        return true;
     }
-    
     bool deQueue() {
         if (isEmpty()) return false;
         if (head == tail) head = 0, tail = -1;
         else head = (head + 1) % maxSize;
-        return true;        
+        return true;
     }
-    
     int Front() {
         return isEmpty() ? -1 : data[head];
     }
-    
     int Rear() {
         return isEmpty() ? -1 : data[tail];
     }
-    
     bool isEmpty() {
         return tail == -1;
     }
-    
     bool isFull() {
-        return !isEmpty() && (tail+1) % maxSize == head;
+        return !isEmpty() && (tail + 1) % maxSize == head;
     }
 private:
     int maxSize, head = 0, tail = -1;
