2864aeb3d0e57aa1dbb652b703f41b1a6686dbe4
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-09-26 01:11:30 +0000

    Sync LeetCode submission - Satisfiability of Equality Equations (cpp)

--- problems/satisfiability_of_equality_equations/solution.cpp
+++ problems/satisfiability_of_equality_equations/solution.cpp
@@ -1,19 +1,43 @@
 class Solution {
 public:
-    int uf[26];
+    //Find Parent function
+ 	int find(char x, unordered_map<char, char>& parent) {
+ 		if (parent[x] != x) parent[x] = find(parent[x], parent);
+        return parent[x];
+ 	}
+    
+    //Main Problem Solver Function
     bool equationsPossible(vector<string>& equations) {
-        for (int i = 0; i < 26; ++i) uf[i] = i;
-        for (string e : equations)
-            if (e[1] == '=')
-                uf[find(e[0] - 'a')] = find(e[3] - 'a');
-        for (string e : equations)
-            if (e[1] == '!' && find(e[0] - 'a') == find(e[3] - 'a'))
-                return false;
+        
+        //Mapping each char with itself, 
+        //meaning assigning each character parent of itself.
+        unordered_map<char, char> parent;
+        for(auto x: equations){
+            parent[x[0]] = x[0];
+            parent[x[3]] = x[3];
+        }
+        
+        //Idea here is simple if two characters "are equal (==)" we are 
+        //basically grouping/union-ing them together by assigning the 
+        //left side character as parent of the right side character. 
+        for(auto it: equations){
+            char x = find(it[0], parent);
+            char y = find(it[3], parent);
+            
+            if(it[1] == '=') parent[y] = x; //union
+        }
+        
+        //Now here what we are simply doing is that we are checking if 
+        //some character is "not equal (!=)" to other but their parent 
+        //are same, that means that is the "imposter" equation.
+        for(auto it: equations){
+            char x = find(it[0], parent);
+            char y = find(it[3], parent);
+            
+            if(it[1] == '!' && x==y) return false;
+        }
+            
+        //If everything is fine...
         return true;
-    }
-
-    int find(int x) {
-        if (x != uf[x]) uf[x] = find(uf[x]);
-        return uf[x];
     }
 };
