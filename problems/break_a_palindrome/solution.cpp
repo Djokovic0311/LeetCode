884c639a67f5dd329126eaaa2bf0d8566956b70d
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-10-10 21:18:24 +0000

    Sync LeetCode submission - Break a Palindrome (cpp)

--- problems/break_a_palindrome/solution.cpp
+++ problems/break_a_palindrome/solution.cpp
@@ -3,15 +3,16 @@ public:
     string breakPalindrome(string palindrome) {
         int n = palindrome.length();
         if(n == 1) return "";
-        for(int i = 0; i < n / 2; i++) {
-            if(palindrome[i] != 'a'){
-                palindrome[i] = 'a';   
+        for (int i = 0; i < n / 2; i++) {
+            //  here we know that as long as palindrome[i] is not "a", we skip it
+            if (palindrome[i] != 'a') {
+                // otherwise, we replace the first character that is not "a"
+                palindrome[i] = 'a';
+                // replaced -> we can return the result here
                 return palindrome;
             }
-
-
         }
-        palindrome[n-1] = 'b';
+        palindrome[n - 1] = 'b';
         return palindrome;
     }
 };
