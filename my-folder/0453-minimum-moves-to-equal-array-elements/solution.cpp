419c37c28923da7ae74ed23e6e84db62f5422d60
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-10-13 01:54:43 +0000

    [LeetCode Sync] Runtime - 63 ms (5.04%), Memory - 28.4 MB (100.00%)

--- my-folder/0453-minimum-moves-to-equal-array-elements/solution.cpp
+++ my-folder/0453-minimum-moves-to-equal-array-elements/solution.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
-    int minMoves(vector<int>& nums) {
-        int sum =0 , min_no =INT_MAX;
-        for(int i:nums)
-        {
-            sum+=i;
-            min_no = min(min_no, i);
-        }
-        return sum-nums.size()*min_no;
+int minMoves(vector<int>& nums) {
+        int m=INT_MAX;
+        for(int n:nums) m = min(m,n);
+        int ans=0;
+        for(int n:nums) ans+=n-m;
+        return ans;
     }
 };
