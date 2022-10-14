6da93cddd52bd101dbb18b7275795d03b7ac0de6
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-10-14 19:44:03 +0000

    Sync LeetCode submission - Convert Sorted List to Binary Search Tree (cpp)

---
+++ problems/convert_sorted_list_to_binary_search_tree/solution.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* helper(ListNode* head, ListNode* tail) {
+        if(head == tail)
+            return NULL;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast != tail && fast->next != tail) {
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        
+        TreeNode* node = new TreeNode(slow->val);
+        node->left = helper(head, slow);
+        node->right = helper(slow->next, tail);
+        return node;
+    }
+    TreeNode* sortedListToBST(ListNode* head) {
+        return helper(head, NULL);
+    }
+};
