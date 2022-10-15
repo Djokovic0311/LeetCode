c5845c4f2f437557a4e54ba1cc2b419eeb58e310
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-10-15 00:37:31 +0000

    Sync LeetCode submission - House Robber II (cpp)

--- problems/house_robber_ii/solution.cpp
+++ problems/house_robber_ii/solution.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
-    int houseRobber(vector<int>& nums) {
-        // dynamic programming - decide each problem by its sub-problems:
-        int dp[nums.size()+1];
-        dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
-        for (int i=2; i<nums.size(); i++)
-            dp[i] = max(dp[i-1], nums[i]+dp[i-2]);
-        return dp[nums.size()-1];
-    }
     int rob(vector<int>& nums) {
-        // edge cases:
-        if (nums.size() == 0) return 0;
-        if (nums.size() == 1) return nums[0];
-        if (nums.size() == 2) return max(nums[0], nums[1]);
-        
-        // either use first house and can't use last or last and not first:
-        vector<int> v1(nums.begin(), nums.end()-1);
-        vector<int> v2(nums.begin()+1, nums.end());
-        return max(houseRobber(v1), houseRobber(v2));        
+        int n = nums.size(); 
+        if (n < 2) return n ? nums[0] : 0;
+        return max(robber(nums, 0, n - 2), robber(nums, 1, n - 1));
     }
+private:
+    int robber(vector<int>& nums, int l, int r) {
+        int pre = 0, cur = 0;
+        for (int i = l; i <= r; i++) {
+            int temp = max(pre + nums[i], cur);
+            pre = cur;
+            cur = temp;
+        }
+        return cur;   
+    }
+
 };
