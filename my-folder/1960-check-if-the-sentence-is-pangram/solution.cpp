fc4fb9b3de1128e2c886b3f74fe436853ae47898
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-10-17 19:29:31 +0000

    [LeetCode Sync] Runtime - 14 ms (0.60%), Memory - 6.7 MB (100.00%)

--- my-folder/1960-check-if-the-sentence-is-pangram/solution.cpp
+++ my-folder/1960-check-if-the-sentence-is-pangram/solution.cpp
@@ -1,10 +1,7 @@
 class Solution {
 public:
     bool checkIfPangram(string sentence) {
-        set<char> s;
-        for(auto ch:sentence) {
-            s.insert(ch);
-        }
-        return s.size()==26;
+        set<char> st(sentence.begin(), sentence.end());
+        return st.size() == 26;
     }
 };
