608499becd42d7060bd81d2dab6cbd058b581843
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-01 18:24:02 +0000

    [LeetCode Sync] Runtime - 1821 ms (5.01%), Memory - 192.2 MB (5.07%)

--- my-folder/0003-longest-substring-without-repeating-characters/solution.cpp
+++ my-folder/0003-longest-substring-without-repeating-characters/solution.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-		unordered_set<char> set;
-    
-		int i = 0, j = 0, n = s.size(), ans = 0;
-    
-		while( i<n && j<n)
-		{
-			if(set.find(s[j]) == set.end()) //If the character does not in the set
-			{
-				set.insert(s[j++]); //Insert the character in set and update the j counter
-				ans = max(ans, j-i); //Check if the new distance is longer than the current answer
-			}
-			else
-			{
-				set.erase(s[i++]); 
-				/*If character does exist in the set, ie. it is a repeated character, 
-				we update the left side counter i, and continue with the checking for substring. */
-			}
-		}
-    
-		return ans;        
+        unordered_set<char> st;
+        int n = s.length();
+        string tmp = "";
+        int res = 0;
+        for(int i = 0; i < n; i++) {
+            tmp += s[i];
+            if(st.find(s[i]) == st.end()) {
+                st.insert(s[i]);
+                // tmp += s[i];
+                // cout << int(tmp.length()) << endl;
+                res = max(res,int(tmp.length()) );
+            }
+            else {
+                int newStart = tmp.find(s[i]) + 1;
+                tmp = string(tmp.begin()+newStart, tmp.end());
+                
+                st = unordered_set(tmp.begin(), tmp.end());
+            }
+        }
+        return res;
     }
 };
