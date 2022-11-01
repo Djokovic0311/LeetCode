d0a28cf768d77e42e587ed64fa5dc55f1fe6187a
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-01 22:47:34 +0000

    [LeetCode Sync] Runtime - 265 ms (5.57%), Memory - 59.1 MB (99.96%)

--- my-folder/0011-container-with-most-water/solution.cpp
+++ my-folder/0011-container-with-most-water/solution.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int i = 0, j = height.size()-1, res = 0;
-        int tmp = 0;
-        while(i < j) {
-            if(height[i] <= height[j]) {
-                tmp = (j-i) * height[i];
-                i++;
-            }
-            else {
-                tmp = (j-i) * height[j];
-                j--;
-            }
-            res = max(res, tmp);
+        int res = 0;
+        int n = height.size();
+        int l = 0, r = n-1;
+        while(l < r) {
+            res = max(res, (r-l) * min(height[r], height[l]));
+            if(height[l] < height[r])
+                l++;
+            else r--;
         }
         return res;
     }
