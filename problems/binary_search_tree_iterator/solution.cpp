bbe090b10e367e34a1f0f92f0b0524dba71dff0c
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-01 18:48:36 +0000

    Sync LeetCode submission - Binary Search Tree Iterator (cpp)

--- problems/binary_search_tree_iterator/solution.cpp
+++ problems/binary_search_tree_iterator/solution.cpp
@@ -13,20 +13,19 @@ class BSTIterator {
 public:
     stack<TreeNode*> s;
     BSTIterator(TreeNode* root) {
-        partialInorder(root);
-    }
-    
-    void partialInorder(TreeNode* root){
         while(root != NULL){
             s.push(root);
             root = root->left;
-        }
+        }        
     }
     
     int next() {
         TreeNode* top = s.top();
         s.pop();
-        partialInorder(top->right);
+        while(top->right != NULL) {
+            s.push(top->right);
+            top->right = top->right->left;
+        }
         return top->val;
     }
     
