b7e5c45ffc0f92c110c525c3d6342709111c3800
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-01 23:16:55 +0000

    Sync LeetCode submission - Merge k Sorted Lists (cpp)

--- problems/merge_k_sorted_lists/solution.cpp
+++ problems/merge_k_sorted_lists/solution.cpp
@@ -10,30 +10,22 @@
  */
 class Solution {
 public:
+    ListNode* merge2Lists(ListNode* l1, ListNode* l2) {
+        if (!l1) return l2;
+        if (!l2) return l1;
+        ListNode* head = l1->val <= l2->val? l1 : l2;
+        head->next = l1->val <= l2->val ? merge2Lists(l1->next, l2) : merge2Lists(l1, l2->next);
+        return head;
+    }
+    
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-
-        map<int, int> nodes;
-
-        for(int i = 0; i < lists.size(); ++i){
-            auto node = lists[i];
-            while(node){
-                int temp = node->val;
-                nodes[temp]++;
-                node = node->next;
-            }
-        }
+        if (lists.size() == 0) return NULL;
         
-        ListNode dummyHead(0);
-        ListNode* tail = &dummyHead;
+        ListNode* head = lists[0];
         
-        for(auto it: nodes){
-            while(it.second != 0){
-                ListNode* newNode= new ListNode(it.first);
-                tail->next = newNode;
-                tail = tail->next;
-                it.second--;
-            }
-        }
-        return dummyHead.next;        
+        for (int i = 1; i < lists.size(); i++)
+            head = merge2Lists(head, lists[i]);
+        
+        return head;
     }
 };
