0f5e26216b1ce53acebb7571f60935058edb8333
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-02 01:40:42 +0000

    Sync LeetCode submission - Minimum Genetic Mutation (cpp)

--- problems/minimum_genetic_mutation/solution.cpp
+++ problems/minimum_genetic_mutation/solution.cpp
@@ -1,32 +1,35 @@
 class Solution {
 public:
     int minMutation(string start, string end, vector<string>& bank) {
-        unordered_set<string> dict(bank.begin(), bank.end());
-        if (!dict.count(end)) return -1;
-        unordered_set<string> bset, eset, *set1, *set2;
-        bset.insert(start), eset.insert(end);
-        int step = 0, n = start.size();
-        while (!bset.empty() and !eset.empty()) {
-            if (bset.size() <= eset.size())
-                set1 = &bset, set2 = &eset;
-            else set2 = &bset, set1 = &eset;
-            unordered_set<string> tmp;
-            step ++;
-            for (auto itr = set1->begin(); itr != set1->end(); ++itr) {
-                for (int i = 0; i < n; ++i) {
-                    string dna = *itr;
-                    for (auto g : string("ATGC")) {
-                        dna[i] = g;
-                        if (set2->count(dna)) return step;
-                        if (dict.count(dna)) {
-                            tmp.insert(dna);
-                            dict.erase(dna);
-                        }
+        vector<char> mut = {'A','C','G','T'};
+        unordered_set<string> dict(bank.begin(),bank.end());
+        if(dict.find(end)==dict.end()) 
+            return -1;
+        int ans = 0;
+        queue<string> q;
+        q.push(start);
+        
+        while(!q.empty()) {
+            int n = q.size();
+            for(int i=0;i<n;i++) {
+                string curr = q.front();
+                q.pop();
+                if(curr==end) 
+                    return ans;
+                dict.erase(curr);
+                for(int j=0;j<8;j++) {
+                    char c = curr[j];
+                    for(int k=0;k<4;k++) {
+                        curr[j] = mut[k];
+                        if(dict.find(curr)!=dict.end())
+                            q.push(curr);
                     }
+                    curr[j] = c;
                 }
             }
-            *set1 = tmp;
+            ans++;
         }
-        return -1;
+        
+        return -1;        
     }
 };
