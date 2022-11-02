46af86f883020bd4d522e16aeadd9b19c4ddbd35
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-02 17:29:09 +0000

    Sync LeetCode submission - The Maze (cpp)

---
+++ problems/the_maze/solution.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool hasPath(vector<vector<int>>& maze, vector<int>& start, vector<int>& destination) {
+        vector<vector<int>> directions = {{0,1},{1,0},{0,-1},{-1,0}};
+        queue<pair<int,int>> q;
+        int m = maze.size(), n = maze[0].size();
+        q.push(make_pair(start[0], start[1]));
+        maze[start[0]][start[1]];
+        while(!q.empty()) {
+            auto tmp = q.front();
+            q.pop();
+            
+            for(auto d : directions) {
+                int r = tmp.first, c = tmp.second;
+                while(r  + d[0]< m && r + d[0] >= 0 && c  + d[1]< n && c+d[1] >= 0 && maze[r+d[0]][c+d[1]] <= 0) {
+                    r = r + d[0], c = c + d[1];
+                }
+                if(r == destination[0] && c == destination[1])
+                    return true;
+                if(maze[r][c] == 0) {
+                    q.push(make_pair(r,c));
+                    maze[r][c] = -1;
+                }
+            }
+        }
+        return false;
+    }
+};
