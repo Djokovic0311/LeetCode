ee00a96e7baba5162cb403af1eb7c655d30b7139
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-02 19:43:48 +0000

    Sync LeetCode submission - Asteroid Collision (cpp)

--- problems/asteroid_collision/solution.cpp
+++ problems/asteroid_collision/solution.cpp
@@ -1,15 +1,33 @@
 class Solution {
 public:
-    vector<int> asteroidCollision(vector<int>& a) {
-        vector<int> s; 
-        for (int i = 0; i < a.size(); i++) {
-            if (a[i] > 0 || s.empty() || s.back() < 0) 
-                s.push_back(a[i]);
-            else if (s.back() <= -a[i]) { 
-                if(s.back() < -a[i]) i--; 
-                s.pop_back(); 
+    vector<int> asteroidCollision(vector<int>& ast) {
+        int n = ast.size();
+        stack<int> s;
+        for(int i = 0; i < n; i++) {
+            if(ast[i] > 0 || s.empty()) {
+                s.push(ast[i]);
+            }
+            else {
+                while(!s.empty() and s.top() > 0 and s.top() < abs(ast[i])) {
+                    s.pop();
+                }
+                if(!s.empty() and s.top() == abs(ast[i])) {
+                    s.pop();
+                }
+                else {
+                    if(s.empty() || s.top() < 0) {
+                        s.push(ast[i]);
+                    }
+                }
             }
         }
-        return s;        
+		// finally we are returning the elements which remains in the stack.
+		// we have to return them in reverse order.
+        vector<int> res(s.size());
+        for(int i = (int)s.size() - 1; i >= 0; i--) {
+            res[i] = s.top();
+            s.pop();
+        }
+        return res;
     }
 };
