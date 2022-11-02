dbe30747b08ca544021ced01f3424908dc14dcad
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-02 01:20:52 +0000

    [LeetCode Sync] Runtime - 66 ms (47.00%), Memory - 17.3 MB (99.98%)

--- my-folder/1129-longest-string-chain/solution.cpp
+++ my-folder/1129-longest-string-chain/solution.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     int longestStrChain(vector<string>& words) {
-        int n = words.size();
         unordered_map<string, int> dp;
-        sort(words.begin(), words.end(), [](const string &l, const string &r) { return l.size() < r.size(); });
         int res = 1;
-        for(string w : words) {
-            dp[w] = 1;
-            for(int i = 0; i < w.length(); i++) {
-                string prev = w.substr(0, i) + w.substr(i + 1);
-                if(dp.find(prev) != dp.end()) {
-                    dp[w] = max(dp[w], dp[prev] + 1);
-                    res = max(res, dp[w]);
+        sort(words.begin(), words.end(), [](const string &l, const string &r) { return l.size() < r.size(); });
+        for (string word : words) {
+            dp[word] = 1;
+            for (int i = 0; i < word.size(); i++) {
+                string prev = word.substr(0, i) + word.substr(i + 1);
+                if (dp.find(prev) != dp.end()) {
+                    dp[word] = max(dp[word], dp[prev] + 1 );
+                    res = max(res, dp[word]);
                 }
             }
         }
-        return res;
+        return res;        
     }
 };
