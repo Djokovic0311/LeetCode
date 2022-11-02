b9f00fb322f72342e26d3e2faf6fd78c403c384a
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-02 19:37:50 +0000

    [LeetCode Sync] Runtime - 23 ms (6.24%), Memory - 10.9 MB (100.00%)

--- my-folder/1371-minimum-remove-to-make-valid-parentheses/solution.cpp
+++ my-folder/1371-minimum-remove-to-make-valid-parentheses/solution.cpp
@@ -1,27 +1,43 @@
 class Solution {
 public:
     string minRemoveToMakeValid(string s) {
-        stack<int> st;
+        int count=0; 
         int n = s.length();
-        for(int i = 0; i < n; i++) {
-            if(s[i] == '(') st.push(i);
-            if(s[i] == ')') {
-                if(st.empty()) s[i] = '*';
-                else st.pop();
+        for(int i=0;i<n;++i){
+            if(s[i]=='('){ // for open bracket
+                ++count;
+            }
+            else if(s[i]==')'){ // for close bracket
+                if(count==0){  // if no. of close brackets > no. of open brackets
+                    s[i]='#';
+                }
+                else{
+                    // if matching parentheses found decrease count
+                    --count;
+                }
             }
         }
-        while(!st.empty()){
-            s[st.top()]='*';
-            st.pop();
-        }
-        
+        count=0;
+        for(int i=n-1;i>=0;--i){
+            if(s[i]==')'){ // for close bracket
+                ++count;
+            }
+            else if(s[i]=='('){ // for open bracket
+                if(count==0){ // if no. of open brackets > no. of close brackets
+                    s[i]='#';
+                }
+                else{
+                    // if matching parentheses found decrease count
+                    --count;
+                }
+            }
+}        
         string ans="";
-        for(int i=0;i<s.length();++i){
-            if(s[i]!='*'){ 
+        for(int i=0;i<n;++i){
+            if(s[i]!='#'){ 
                 ans.push_back(s[i]);
             }
         }
-        
         return ans;
     }
 };
