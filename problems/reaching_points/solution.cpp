9b809071f269c14442a886ba801b586a761f1fee
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-02 01:44:20 +0000

    Sync LeetCode submission - Reaching Points (cpp)

---
+++ problems/reaching_points/solution.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool reachingPoints(int sx, int sy, int tx, int ty) {
+        // in case of impossible cases
+        if(tx < sx || ty < sy) return false;
+        
+        // to avoid the useless loop in those cases
+        if(sx == tx) return (ty - sy) % sx == 0;
+        
+        if(sy == ty) return (tx - sx) % sy == 0;
+        
+        while(tx > sx && ty > sy) {
+                  
+            if(tx > ty)
+                tx %= ty;
+            else 
+                ty %= tx;
+        }
+         
+        if(tx == sx) {
+            if((ty - sy) % tx == 0)
+                return true;
+        } else if(ty == sy) {
+            if((tx - sx) % ty == 0)
+                return true;
+        }
+        
+        return false;        
+    }
+};
