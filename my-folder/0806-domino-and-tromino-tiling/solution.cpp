16df0e6003d01709e3b7445695d4aba2ec8cd99c
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-02 02:33:54 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 6 MB (100.00%)

--- my-folder/0806-domino-and-tromino-tiling/solution.cpp
+++ my-folder/0806-domino-and-tromino-tiling/solution.cpp
@@ -1,18 +1,10 @@
 class Solution {
 public:
-    int numTilings(int n) {
-        if (n < 3) return n;
-        
-        int mod = pow(10, 9) + 7;
-        vector<long> D(n+1, 0), T(n+1, 0);
-        D[0] = 0, D[1] = 1, D[2] = 2;
-        T[0] = 0, T[1] = 1, T[2] = 2;
-        
-        for (int i = 3; i <= n; i++) {
-            D[i] = (D[i-1] + D[i-2] + 2*T[i-2]) % mod;
-            T[i] = (T[i-1] + D[i-1]) % mod;
-        }
-        
-        return D[n]; 
+int numTilings(int n) {
+    unsigned int dp[n + 3]; dp[0] = 1; dp[1] = 2; dp[2] = 5;
+    for (int i = 3; i < n; i ++) {
+        dp[i] = (2 * dp[i - 1] + dp[i - 3]) % 1000000007;
     }
+    return dp[n - 1];
+}
 };
