02bcc016355538e25d7c4086d744975df048cb39
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-03 15:04:41 +0000

    [LeetCode Sync] Runtime - 90 ms (16.87%), Memory - 49.2 MB (100.00%)

--- my-folder/0131-palindrome-partitioning/solution.cpp
+++ my-folder/0131-palindrome-partitioning/solution.cpp
@@ -1,31 +1,33 @@
 class Solution {
 public:
     vector<vector<string>> partition(string s) {
-        vector<vector<string>> res; // which will be our answer
-        vector<string> path; // as we are generating list everythime, so at the end this will be our list
-        helper(0, s, path, res); // calling to recursion function start from index 0 and string s
-        return res;
+        vector<vector<string>> result;
+        vector<string> path;
+        partition(s, 0, path, result);//dfs calls
+        return result;
     }
-    // Entire recursive function, that generates all the partition substring
-    void helper(int index, string s, vector<string> &path, vector<vector<string>> &res){
-        // Base Condition, which means when we have done partition at the end (n), then add it to our ultimate result
-        if(index == s.size()){
-            res.push_back(path);
-            return;
-        }
-        // Let's talk about partition
-        for(int i = index; i < s.size(); i++){
-            if(isPalindrome(s, index, i)){ // what we are checking over here is, if we partition the string from index to i Example-(0, 0) is palindrome or not
-                path.push_back(s.substr(index, i - index + 1)); // take the substring and store it in our list & call the next substring from index + 1
-                helper(i + 1, s, path, res); // as we have done for (0, 0) then our next will be from (1)
-                path.pop_back(); // please make sure you remove when you backtrack. 
-                // Why? Because let say i had partion y, so when i go back. I can't have yy
+private: 
+    //DFS steps
+    void partition(string& s, int start, vector<string>& path, vector<vector<string>>& result) {
+        int n = s.length();
+        if (start == n) {
+            result.push_back(path);
+        } else {
+            for (int i = start; i < n; i++) {
+                if (isPalindrome(s, start, i)) {
+                    path.push_back(s.substr(start, i - start + 1));
+                    partition(s, i + 1, path, result);
+                    path.pop_back();
+                }
             }
         }
     }
-    bool isPalindrome(string s, int start, int end){ // A simple palindromic function start from 0 go till end. And basically keep on checking till they don't cross. 
-        while(start <= end){
-            if(s[start++] != s[end--]) return false;
+    //helper function to safe check whether a substr is palindrome or not
+    bool isPalindrome(string& s, int l, int r) {
+        while (l < r) {
+            if (s[l++] != s[r--]) {
+                return false;
+            }
         }
         return true;
     }
