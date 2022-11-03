eedf069de47fcdc31023466c406bdfafa23aa31d
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-03 00:00:48 +0000

    [LeetCode Sync] Runtime - 51 ms (5.19%), Memory - 14.2 MB (100.00%)

--- my-folder/0210-course-schedule-ii/solution.cpp
+++ my-folder/0210-course-schedule-ii/solution.cpp
@@ -1,56 +1,32 @@
-class Solution
-{
+class Solution {
 public:
-    bool kahnAlgo(vector<vector<int>> &adj, int n, vector<int> &indegree, vector<int> &ans)
-    {
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> graph(numCourses);
+        vector<int> indegree(numCourses);
+        for(auto pre : prerequisites) {
+            graph[pre[1]].push_back(pre[0]);
+            indegree[pre[0]]++;
+        }
         queue<int> q;
-        for (int i = 0; i < n; i++)
-        {
-            if (indegree[i] == 0)
+        for(int i = 0; i < numCourses; i++) {
+            if(indegree[i] == 0)
                 q.push(i);
         }
-
-        int count = 0;
-        while (!q.empty())
-        {
-            int curr = q.front();
+        vector<int> res;
+        while(!q.empty()) {
+            int tmp = q.front();
+            res.push_back(tmp);
             q.pop();
-
-            for (auto a : adj[curr])
-            {
-                indegree[a] -= 1;
-                if (indegree[a] == 0)
-                    q.push(a);
+            for(int i = 0; i < graph[tmp].size(); i++) {
+                int curr = graph[tmp][i];
+                indegree[curr]--;
+                if(indegree[curr]==0)
+                    q.push(curr);
             }
-            ans.push_back(curr);
-            count++;
+            
         }
-
-        if (count != n)
-            return false;
-        return true;
-    }
-
-    vector<int> findOrder(int numCourses, vector<vector<int>> &prerequisites)
-    {
-        int n = prerequisites.size();
-        vector<vector<int>> adj(numCourses); // Adjacency matrix.
-        vector<int> indegree(numCourses, 0); 
-
-        // traversing all the nodes of the graph.
-        for (int i = 0; i < n; i++)
-        {
-            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
-            indegree[prerequisites[i][0]] += 1;
-        }
-
-        vector<int> ans;
-
-        // if graph is DAG and Kahn's algo is applied then return the ans.
-        if (kahnAlgo(adj, numCourses, indegree, ans))
-            return ans;
-
-        // otherwise return the empty vector.
-        return {};
+        if (res.size() != numCourses)
+        res.clear();
+        return res;
     }
 };
