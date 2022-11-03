ff2b221003fe1282217e0cf0390b21983e2cd227
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-03 02:53:02 +0000

    Sync LeetCode submission - Course Schedule (cpp)

--- problems/course_schedule/solution.cpp
+++ problems/course_schedule/solution.cpp
@@ -1,14 +1,36 @@
 class Solution {
 public:
-    bool canFinish(int n, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> G(n);
-        vector<int> degree(n, 0), bfs;
-        for (auto& e : prerequisites)
-            G[e[1]].push_back(e[0]), degree[e[0]]++;
-        for (int i = 0; i < n; ++i) if (!degree[i]) bfs.push_back(i);
-        for (int i = 0; i < bfs.size(); ++i)
-            for (int j: G[bfs[i]])
-                if (--degree[j] == 0) bfs.push_back(j);
-        return bfs.size() == n;        
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        queue<int> q;
+        int count = 0;
+        vector<int> indegree(numCourses, 0);
+        
+        vector<int> adj[numCourses];         // adj list        
+        int edges = prerequisites.size();
+        for(int i = 0;i<edges;i++){
+            int u = prerequisites[i][0];
+            int v = prerequisites[i][1];
+            adj[v].push_back(u);
+        }
+        
+        for(int i=0; i<numCourses; i++){
+            for(auto it : adj[i])
+                indegree[it]++;
+        }
+        for(int i=0; i<numCourses; i++){
+            if(indegree[i] == 0)
+                q.push(i);
+        }
+        while(!q.empty()){
+            count++;
+            int node = q.front();
+            q.pop();
+            for(int i: adj[node]){
+                indegree[i]--;
+                if(indegree[i] == 0)
+                    q.push(i);
+            }
+        }
+        return count == numCourses;        
     }
 };
