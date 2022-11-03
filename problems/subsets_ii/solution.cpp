8c417383bed2cdfcd7ca848f43acec8c9a4e9ede
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-03 14:27:49 +0000

    Sync LeetCode submission - Subsets II (cpp)

--- problems/subsets_ii/solution.cpp
+++ problems/subsets_ii/solution.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
-  
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        vector<vector<int>> res = {{}};
-        vector<int> curr;
-        int size;
+        vector<vector<int>> ans;       //to store all susbets
+        vector<int> curr;         //to store current individual subset (that we will build)
+        sort(nums.begin(),nums.end());      //sort the array so that duplicates are adjacent 
+        helper(nums,ans,curr,0);       //we start from index 0
+        return ans;
         
-        for (auto num : nums) {
-            size = res.size();
-            
-            for (int i = 0; i < size; i++) {
-                curr = res[i];
-                curr.push_back(num);
-                if (find(res.begin(), res.end(), curr) == res.end())
-                    res.push_back(curr);
-            }
-        }
-        
-        return res;
     }
+    
+    void helper(vector<int>& nums, vector<vector<int>>& ans, vector<int>& curr, int idx){
+        ans.push_back(curr);       //we include current susbet into final ans
+        for(int i=idx;i<nums.size();i++){     //check for all possibilites
+            if(i>idx &&  nums[i]==nums[i-1]) continue;      //if duplicate then we continue
+            curr.push_back(nums[i]);     //we include nums[i] in current subset
+            helper(nums,ans,curr,i+1); 
+            curr.pop_back();         //to get subset without nums[i]
+        }
+    }  
 };
