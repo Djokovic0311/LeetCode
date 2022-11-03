b66d3e51a51bdca7db7ca33cffafd2edc75f00b9
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-03 03:14:53 +0000

    [LeetCode Sync] Runtime - 70 ms (6.33%), Memory - 19.6 MB (100.00%)

--- my-folder/0056-merge-intervals/solution.cpp
+++ my-folder/0056-merge-intervals/solution.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        int n = intervals.size();
-        vector<vector<int>> res;
         sort(intervals.begin(), intervals.end());
+        vector<vector<int>> res;
+        if(intervals.size()<=1) return intervals;
         res.push_back(intervals[0]);
-        for(int i = 1; i < n; i++) {
-            if(res.back()[1] >= intervals[i][0]) res.back()[1] = max(res.back()[1],intervals[i][1]);
+        for(int i = 1; i < intervals.size(); i++) {
+            if(intervals[i][0] <= res.back()[1])
+            {
+                int s = res.back()[0], e = max(res.back()[1], intervals[i][1]);
+                res.pop_back();
+                res.push_back({s,e});
+            }
             else res.push_back(intervals[i]);
         }
         return res;
