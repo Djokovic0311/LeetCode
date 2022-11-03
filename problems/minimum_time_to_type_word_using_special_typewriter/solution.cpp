adcdb8995b51463e88260f7da14e39d2d46c8837
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-03 02:54:16 +0000

    Sync LeetCode submission - Minimum Time to Type Word Using Special Typewriter (cpp)

--- problems/minimum_time_to_type_word_using_special_typewriter/solution.cpp
+++ problems/minimum_time_to_type_word_using_special_typewriter/solution.cpp
@@ -1,17 +1,11 @@
 class Solution {
 public:
     int minTimeToType(string word) {
-        // int op = 0;
-        int op = word.length();
-        char start = 'a';
-        // op += min(abs('a' - word[0]), 26-abs(a - word[0]));
-        // op += n;
-        for(char c : word) {
-            op += min(abs(start - c), 26-abs(start -c));
-            // op++;
-            cout << op << endl;
-            start = c;
+        int res = word.size(), point = 'a';
+        for (auto ch : word) {
+            res += min(abs(ch - point), 26 - abs(point - ch));
+            point = ch;
         }
-        return op;
+        return res;        
     }
 };
