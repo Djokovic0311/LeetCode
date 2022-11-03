c691998b6ae7446930d5b8eb887db811384def0f
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-03 14:54:40 +0000

    [LeetCode Sync] Runtime - 39 ms (5.02%), Memory - 10.1 MB (100.00%)

--- my-folder/0130-surrounded-regions/solution.cpp
+++ my-folder/0130-surrounded-regions/solution.cpp
@@ -1,46 +1,38 @@
 class Solution {
 public:
-    bool isSafe(int i, int j, int m, int n){
-        return (i>=0 && i<m && j>=0 && j<n);
-    }
-	// Checks whether the indices are present at the border of the matrix.
-    bool isBorder(int i, int j, int m, int n){
-        return (i==0 || i==m-1 || j==0 || j==n-1);
-    }
     void solve(vector<vector<char>>& board) {
-        if(board.size() == 0) return;
-        int m = board.size();
-        int n = board[0].size();
-        queue<pair<int,int>> q;
-        vector<pair<int, int>> dir = {{1,0}, {-1,0}, {0, 1}, {0, -1}};
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(board[i][j]=='O' && isBorder(i,j,m,n)){
-                    board[i][j] = '.';
-                    q.push(make_pair(i,j));                    
-                }
+         queue<pair<int,int>> q;
+        int m=board.size(),n=board[0].size();
+		
+		//Getting boundary O's
+        for(int i=0;i<m;i++)
+        {
+            if(board[i][0]=='O') board[i][0]='.',q.push({i,0});
+            if(board[i][n-1]=='O') board[i][n-1]='.',q.push({i,n-1});
+        }
+        for(int i=1;i<n-1;i++)
+        {
+            if(board[0][i]=='O') board[0][i]='.',q.push({0,i});
+            if(board[m-1][i]=='O') board[m-1][i]='.',q.push({m-1,i});
+        }
+		
+		//BFS
+        while(q.size())
+        {
+            int sz=q.size();
+            while(sz--)
+            {
+                auto p=q.front();q.pop();
+                int r=p.first,c=p.second;
+                 if(r+1<m) if(board[r+1][c]=='O') board[r+1][c]='.',q.push({r+1,c});
+                 if(r-1>=0) if(board[r-1][c]=='O') board[r-1][c]='.',q.push({r-1,c});
+                 if(c+1<n) if(board[r][c+1]=='O') board[r][c+1]='.',q.push({r,c+1});
+                 if(c-1>=0) if(board[r][c-1]=='O') board[r][c-1]='.',q.push({r,c-1});    
             }
         }
-        while(!q.empty()){
-            pair<int, int> temp = q.front();
-            q.pop();
-            for(int i=0;i<dir.size();i++){
-                int x = temp.first + dir[i].first;
-                int y = temp.second + dir[i].second;
-
-                if(isSafe(x,y,m,n) && !isBorder(x,y, m,n) && board[x][y]=='O'){
-                    board[x][y]= '.';
-                    q.push(make_pair(x,y));
-                }
-            }
-        }
+		//all the unsorrounded O's are re-entered
         for(int i=0;i<m;i++)
             for(int j=0;j<n;j++)
-			    // Just convert the remaining 'O' into 'X' as they are not connected to any border 'O'.
-                if(board[i][j]=='O')
-                    board[i][j] = 'X';
-                // Now, convert the '.' into 'O' as they were connected to some border 'O'.
-				else if(board[i][j]=='.')
-                    board[i][j] = 'O';
+                board[i][j]=board[i][j]=='.'?'O':'X';        
     }
 };
