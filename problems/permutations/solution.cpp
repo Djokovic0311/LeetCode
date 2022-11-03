209b798d5abbdb8b5c71732b84dfa5fcb83c95fa
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-03 14:08:37 +0000

    Sync LeetCode submission - Permutations (cpp)

--- problems/permutations/solution.cpp
+++ problems/permutations/solution.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
-    vector<vector<int>> result;
-    
-    // Backtracking
-    void permutation(vector<int> &nums,int i,int n){
-        if(i==n){
-            result.push_back(nums);
-            return ;
+    vector<vector<int>> res;
+    void dfs(vector<int>& num, int i, int n) {
+        if(i == n)
+        {res.push_back(num);
+         return;}
+        for(int j = i; j < n; j++) {
+            swap(num[i], num[j]);
+            dfs(num, i+1, n);
+            swap(num[j], num[i]);
         }
-
-        for(int j=i;j<=n;j++){
-            swap( nums[i],nums[j]);
-            permutation(nums,i+1,n);
-            swap( nums[i],nums[j]);
-        }
+        
     }
-    
     vector<vector<int>> permute(vector<int>& nums) {
-        
-        permutation(nums,0,nums.size()-1);
-        
-        return result;
+        dfs(nums,0,nums.size());
+        return res;
     }
 };
