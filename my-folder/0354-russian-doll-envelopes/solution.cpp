ffa7f1979e5fb63ba9e677d3cfca35c44309149f
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-03 02:16:45 +0000

    [LeetCode Sync] Runtime - 883 ms (9.10%), Memory - 77.6 MB (100.00%)

--- my-folder/0354-russian-doll-envelopes/solution.cpp
+++ my-folder/0354-russian-doll-envelopes/solution.cpp
@@ -1,15 +1,24 @@
 class Solution {
 public:
-    int maxEnvelopes(vector<vector<int>>& E) {
-        sort(E.begin(), E.end(), [](vector<int>& a, vector<int>& b) 
-             -> bool {return a[0] == b[0] ? b[1] < a[1] : a[0] < b[0];});
-        vector<int> dp;
-        for (auto& env : E) {
-            int height = env[1];
-            int left = lower_bound(dp.begin(), dp.end(), height) - dp.begin();
-            if (left == dp.size()) dp.push_back(height);
-            dp[left] = height;
+    static bool comp(vector<int> &a, vector<int> &b){
+        if(a[0]==b[0]){
+            return a[1]>b[1];
         }
-        return dp.size();
+        return a[0]<b[0];
+    }
+        
+    int maxEnvelopes(vector<vector<int>>& envelopes) {
+        sort(envelopes.begin(), envelopes.end(), comp);
+        int i, j, n=envelopes.size();
+        vector<int>lis;
+        
+        for(i=0; i<n; i++){
+            auto it = lower_bound(lis.begin(), lis.end(), envelopes[i][1]);
+            if(it==lis.end()){lis.push_back(envelopes[i][1]);}
+            else{
+                *it = envelopes[i][1];
+            }
+        }
+        return lis.size();
     }
 };
