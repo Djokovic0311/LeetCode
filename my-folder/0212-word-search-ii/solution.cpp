934a42b9ffaf8b01075b21c54b3700f3af18d890
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-05 04:39:31 +0000

    [LeetCode Sync] Runtime - 862 ms (25.58%), Memory - 15.6 MB (98.47%)

--- my-folder/0212-word-search-ii/solution.cpp
+++ my-folder/0212-word-search-ii/solution.cpp
@@ -1,65 +1,55 @@
 class Solution {
 public:
-    vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
-        unordered_map<char, unordered_set<string>> wordMap; // {last letter, set of whole words}
-        unordered_set<string> found; // words already found
-        // Inserts each word into map with last letter
-        for (string word : words) {
-            if (wordMap.count(word.back())) {
-                wordMap[word.back()].insert(word);
-            } 
-            else {
-                wordMap.insert({ word.back(), {word} });
-            }
-        }
-        vector<string> result;
-        int M = board.size(); // Rows
-        int N = board[0].size(); // Columns
-        for (int i = 0; i < M; i++) {
-            for (int j = 0; j < N; j++) {
-                char currentLetter = board[i][j];
-                if (wordMap.count(currentLetter) == 1) {
-                    for (string currentPossibleWord : wordMap[currentLetter]) {
-                        if (!found.count(currentPossibleWord) && dfs(board, currentPossibleWord, i, j) && currentPossibleWord != "") {
-                            result.push_back(currentPossibleWord);
-                            found.insert(currentPossibleWord);
-                        }
-                    }
-                }
-            }
-        }
-        return result;
+    vector<string> findWords(vector<vector<char>>& b, vector<string>& ws) {
+        if (!(row=b.size()) || !(col=b[0].size())) return res;
+        
+        TN* r = buildTrie(ws);        
+        for (int i = 0; i < row; ++i)
+            for (int j = 0; j < col; ++j)
+                dfs(b, i, j, r);
+        return res;
     }
     
-    bool dfs(vector<vector<char>> &board, string word, int i, int j) {
-        // If we've found the whole word in the board
-        if (word == "") {
-            return true;
+private:
+    struct TN {
+        TN* next[26];
+        string w;
+    };
+    
+    TN* buildTrie(vector<string>& ws) {
+        TN* r = new TN();
+        for (auto& w : ws) {
+            TN* p = r;
+            for (char c : w) {
+                if (!p->next[c-'a']) p->next[c-'a'] = new TN();
+                p = p->next[c-'a'];
+            }
+            p->w = w;
         }
-        if (!inside(board, word, i, j)) {
-            return false;    
-        }
-        // Save original letter for next searches
-        char previousLetter = board[i][j];
-        // Mark letter as visited on board
-        board[i][j] = '*';
-        // Remove the last letter since we already counted it
-        word.pop_back();
-        // Keep searching
-        // Backtracking (Revert back previous letter)
-        bool result = dfs(board, word, i + 1, j)
-        || dfs(board, word, i - 1, j) 
-        || dfs(board, word, i, j + 1) 
-        || dfs(board, word, i, j - 1);
-        board[i][j] = previousLetter;
-        return result;
+        return r;
     }
     
-    // Checks borders if we are inside of the grid and check letter if valid
-    bool inside(vector<vector<char>> &board, string &word, int &i, int &j) {
-        if (i < 0 || i >= board.size() || j < 0 || j >= board[i].size() || word.back() != board[i][j]) {
-            return false;
-        }
-        return true;
+    bool inbound(int i, int j) {
+        return i>=0 && i<row && j>=0 && j<col;
     }
+    
+    void dfs(vector<vector<char>>& b, int i, int j, TN* p) {
+        if (!inbound(i, j)) return;
+        char c = b[i][j];
+        if (c == '.' || !p->next[c-'a']) return;
+        p = p->next[c-'a'];
+        if (!p->w.empty()) {
+            res.push_back(p->w);
+            p->w.clear();
+        }
+        
+        b[i][j] = '.';
+        pair<int,int> dirs[4] = {{0,1},{0,-1},{1,0},{-1,0}};
+        for (auto d : dirs) dfs(b, i+d.first, j+d.second, p);
+        b[i][j] = c;
+    }
+    
+    int row, col; // board dimensions
+    vector<string> res; // words found in board
 };
+
