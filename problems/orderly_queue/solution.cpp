265053ed7c08c62080de984e117162cc187ca76c
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-06 16:57:51 +0000

    Sync LeetCode submission - Orderly Queue (cpp)

--- problems/orderly_queue/solution.cpp
+++ problems/orderly_queue/solution.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
-    string orderlyQueue(string s, int k) {
-        if(k==1){
-        
-        string temp=s;
-        
-        for(int i=0;i<s.length();i++){
-            
-            string sub=s.substr(i)+s.substr(0,i);
-            
-            if(min(temp,sub)==sub){
-                temp=sub;
-            }
-            
+    static string orderlyQueue(string s, int k) {
+        if (k == 1) {
+            string ans = s;
+            // Append |s| to itself and look for the smallest string
+            // of as the same lenght as |s|.
+            s += s;
+            for (int i = 1; i < size(ans); ++i)
+                ans = min(ans, s.substr(i, size(ans)));
+
+            return ans;
         }
         
-        return temp;
-    }
-    
-    else{
-        
-        sort(s.begin(),s.end());
-        
+        // With k > 1 we can reorder |s| how we want, so sorting it
+        // will yield the lexicographically smallest one.
+        sort(begin(s), end(s));
         return s;
-    }
     }
 };
