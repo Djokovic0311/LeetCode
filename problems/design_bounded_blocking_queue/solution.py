d7f30dc1b5ddc98b82c5230104661d4cb38704ac
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-10 04:29:47 +0000

    Sync LeetCode submission - Design Bounded Blocking Queue (python3)

---
+++ problems/design_bounded_blocking_queue/solution.py
@@ -0,0 +1,33 @@
+import threading
+
+class BoundedBlockingQueue(object):
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        
+        self.pushing = threading.Semaphore(capacity)
+        self.pulling = threading.Semaphore(0)
+        self.editing = threading.Lock()
+      
+        self.queue = collections.deque()
+
+    def enqueue(self, element: int) -> None:
+      self.pushing.acquire()
+      self.editing.acquire()
+      
+      self.queue.append(element)
+      
+      self.editing.release()
+      self.pulling.release()
+
+    def dequeue(self) -> int:
+        self.pulling.acquire()
+        self.editing.acquire()
+        
+        res = self.queue.popleft()
+        
+        self.editing.release()
+        self.pushing.release()
+        return res
+
+    def size(self) -> int:
+      return len(self.queue)
