8c8204a1c77576fcc7c9283c84391e73d0e8f4a0
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-10 14:50:40 +0000

    Sync LeetCode submission - Hand of Straights (cpp)

--- problems/hand_of_straights/solution.cpp
+++ problems/hand_of_straights/solution.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
     bool isNStraightHand(vector<int>& hand, int groupSize) {
+        map<int, int> mp;
         int n = hand.size();
         if(n % groupSize != 0) return false;
-        map<int, int> mp;
-        for(int i = 0; i < n; i++) {
-            mp[hand[i]]++;
-        }
+        for(int h : hand)
+            mp[h]++;
         while(!mp.empty()) {
-            auto iter = mp.begin();
-            int start = iter->first;
-            cout << start << endl;
+            auto it = mp.begin();
+            int start = it->first;
             for(int i = 1; i < groupSize; i++) {
-                if(mp.find(start+i) == mp.end()) return false;
-                else {
-                    mp[start+i]--;
-                    if(mp[start+i] == 0) mp.erase(start+i);
-                }
+                if(mp.find(start+i) == mp.end())
+                    return false;
+                mp[start+i]--;
+                if(mp[start+i] == 0) mp.erase(start+i);
             }
             mp[start]--;
             if(mp[start] == 0)
                 mp.erase(start);
         }
-        
-        // cout << mp.begin()->first;
         return true;
     }
 };
