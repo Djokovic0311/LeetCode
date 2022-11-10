2418eada7edbe2b49f6b0b958d87d87fb22adcdb
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-10 05:18:41 +0000

    [LeetCode Sync] Runtime - 3 ms (6.68%), Memory - 8.5 MB (100.00%)

--- my-folder/0655-print-binary-tree/solution.cpp
+++ my-folder/0655-print-binary-tree/solution.cpp
@@ -10,49 +10,39 @@
  * };
  */
 class Solution {
-    int height(TreeNode* node)
-    {
-        if(node == NULL)
-            return -1;
-        int lh = height(node->left);
-        int rh = height(node->right);
-        
-        return 1 + max(lh, rh);
-    }
     
 public:
-    vector<vector<string>> printTree(TreeNode* root) 
-    {
-        
+    int height(TreeNode* root) {
+        if(!root) return -1;
+        int l = height(root->left) +1;
+        int r = height(root->right) +1;
+        return max(l,r);
+    }
+    vector<vector<string>> printTree(TreeNode* root) {
         int ht = height(root);
         int m = ht + 1;
         int n = pow(2, ht+1) - 1;
-        vector<vector<string>> ans(m, vector<string>(n, ""));
-        
-        //             node,       row, col
-        queue<pair<TreeNode*, pair<int, int>>> que;
-        que.push({root, {0, (int)((n-1)/2)}});
+        vector<vector<string>> res(m, vector<string>(n, ""));    
+        queue<pair<TreeNode*, pair<int,int>>> q;
+        res[0][(n-1)/2] = to_string(root->val);
+        q.push(make_pair(root,make_pair(0,(n-1)/2)));
+        while(!q.empty()) {
+            auto tmp = q.front();
+            q.pop();
+            int r = tmp.second.first, c = tmp.second.second;
+            TreeNode* node = tmp.first;
+            if(node->left) {
+                res[r+1][c-pow(2,ht-r-1)] = to_string(node->left->val);
+                q.push(make_pair(node->left,make_pair(r+1,c-pow(2,ht-r-1))));
+            }
                 
-        while(!que.empty())
-        {
-            pair<TreeNode*, pair<int, int>> curr = que.front();
-            que.pop();
-            TreeNode* node = curr.first;
-            int row = curr.second.first;
-            int col = curr.second.second;
-            
-            ans[row][col] = to_string(node->val);
-            
-            if(node->left != NULL)
-            {
-                que.push({node->left, {row+1, col - pow(2, ht-row-1)}});
+            if(node->right) {
+                res[r+1][c+pow(2,ht-r-1)] = to_string(node->right->val);
+                q.push(make_pair(node->right,make_pair(r+1,c+pow(2,ht-r-1))));
             }
-            if(node->right != NULL)
-            {
-                que.push({node->right, {row+1, col + pow(2, ht-row-1)}});
-            }
+            
         }
-        return ans;
+
+        return res;
     }
 };
-
