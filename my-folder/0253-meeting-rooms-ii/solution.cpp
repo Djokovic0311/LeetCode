ead4e4f9a910e4d08684b96e3cd5225f82edfc49
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-10 04:38:02 +0000

    [LeetCode Sync] Runtime - 57 ms (8.31%), Memory - 12.4 MB (100.00%)

--- my-folder/0253-meeting-rooms-ii/solution.cpp
+++ my-folder/0253-meeting-rooms-ii/solution.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     int minMeetingRooms(vector<vector<int>>& intervals) {
-        int res = 1;
-        sort(intervals.begin(), intervals.end());
         priority_queue<int, vector<int>, greater<int>> pq;
-        for(auto inter : intervals) {
-            int s = inter[0], e = inter[1];
-            while(!pq.empty() && pq.top() <= s) pq.pop();
-            pq.push(e);
+        sort(intervals.begin(),  intervals.end());
+        int res = 1;
+        for(auto interval : intervals) {
+            int start = interval[0], end = interval[1];
+            while(!pq.empty() && pq.top() <= start)
+                pq.pop();
+            pq.push(end);
             res = max(res, int(pq.size()));
         }
         return res;
