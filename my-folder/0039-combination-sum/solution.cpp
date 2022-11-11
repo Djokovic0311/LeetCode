333b4158a1bc205113c1c7af1158baecd9a0d38b
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-11 21:49:30 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 10.8 MB (100.00%)

--- my-folder/0039-combination-sum/solution.cpp
+++ my-folder/0039-combination-sum/solution.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-    void combination(vector<int>& candidates, vector<int>& r, int target, vector<vector<int>>& res, int i) {
+    void helper(vector<int>& candidates, vector<int>& r, int target, int i, vector<vector<int>>& res) {
         if(target == 0) {
             res.push_back(r);
             return;
         }
-        while(i < candidates.size() && target-candidates[i] >= 0) {
+        while(i < candidates.size() && target - candidates[i] >= 0) {
             r.push_back(candidates[i]);
-            combination(candidates,r,target-candidates[i],res,i);
+            helper(candidates, r, target-candidates[i],i, res);
             i++;
             r.pop_back();
-            
         }
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        sort(candidates.begin(),candidates.end());
+                sort(candidates.begin(),candidates.end());
         candidates.erase(unique(candidates.begin(),candidates.end()),candidates.end());
+vector<vector<int>> res;
         vector<int> r;
-        vector<vector<int>> res;
-        combination(candidates,r,target,res,0);
+        helper(candidates,r, target,0,res);
         return res;
     }
-};  
+};
