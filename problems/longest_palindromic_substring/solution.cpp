fad1abd1c674f73a0e92a4f9726d6e4da06a2aa1
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-11 18:19:36 +0000

    Sync LeetCode submission - Longest Palindromic Substring (cpp)

--- problems/longest_palindromic_substring/solution.cpp
+++ problems/longest_palindromic_substring/solution.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-void isPalindrome(const string& s,const int& length,int left,int right, int& start, int& maxlen){
-	while(left>=0 && right<length && s[left]==s[right]) --left,++right;
-	if(maxlen < right-left-1)   maxlen = right-left-1, start = left+1;
-}
-string longestPalindrome(string s) {
-	int n = s.size(),start=0,maxlen=0;
-	if(n<2) return s;
-	for(int i=0;i<n-1;++i){
-		isPalindrome(s,n,i,i,start,maxlen); //aba
-		isPalindrome(s,n,i,i+1,start,maxlen); //noon
-	}
-	return s.substr(start,maxlen);
-}
+    string longestPalindrome(string s) {
+        int n = s.length();
+        vector<vector<int>> dp(n,vector<int>(n,1));
+        int maxlen = 1;
+        int head = 0;
+        for (int len = 2; len <= n; ++len)
+            for (int i = 0, j; (j=i+len-1) < n; ++i) 
+            {
+                dp[i][j] = (s[i]==s[j]) && dp[i+1][j-1];
+                if (dp[i][j] && len > maxlen) head = i, maxlen = len;
+            }
+        return s.substr(head, maxlen);
+    }
 };
