6d94d1e1ee286ea67c8750417e9ec5f57b29fc25
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-11 16:17:40 +0000

    Sync LeetCode submission - String Compression (cpp)

---
+++ problems/string_compression/solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        int res = 0;
+        int i = 0, idx = 0;
+        int n = chars.size();
+        while(i < n) {
+            char c = chars[i];
+            int cnt = 0;
+            while(i < n && chars[i] == c) {
+                cnt++;
+                i++;
+            }
+            chars[idx] = c;
+            idx++;
+            if(cnt > 1) {
+                string s = to_string(cnt);
+                for(char& ch : s) {
+                    
+                    chars[idx] = ch;
+                    idx++;
+                }
+            }
+        }
+        return idx;
+    }
+};
