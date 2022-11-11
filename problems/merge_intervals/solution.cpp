b77ac1b493e7a00ee7a0e21f97175c889f22e67d
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-11 15:37:46 +0000

    Sync LeetCode submission - Merge Intervals (cpp)

--- problems/merge_intervals/solution.cpp
+++ problems/merge_intervals/solution.cpp
@@ -5,6 +5,7 @@ public:
         vector<vector<int>> res;
         if(intervals.size()<=1) return intervals;
         res.push_back(intervals[0]);
+        
         for(int i = 1; i < intervals.size(); i++) {
             if(intervals[i][0] <= res.back()[1])
             {
