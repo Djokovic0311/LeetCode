43e66e2a3c3f67bcad5fe1d103b7d3ceb3f1505d
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-12 03:30:02 +0000

    [LeetCode Sync] Runtime - 3 ms (2.90%), Memory - 5.9 MB (100.00%)

--- my-folder/0050-powx-n/solution.cpp
+++ my-folder/0050-powx-n/solution.cpp
@@ -1,26 +1,15 @@
 class Solution {
 public:
-    double myPow2(double x, int n)
-    {
-        if(n==0)
-        {
-            return 1;
-            
-        }
-        double res=myPow2(x,n/2);
-        if(n&1)
-        {
-            return x*res*res;
-        }
-        else
-            return res*res;
-         
+    double myPow2(double x, int n) {
+        if(n == 0) return 1;
+        double res = myPow2(x, n/2);
+        if(n % 2 == 0)
+            return res * res;
+        else return res * res * x;
     }
     double myPow(double x, int n) {
-        if(n<0)
-            x=1/x;
-        double ans=myPow2(x,n);
-        return ans;
-       
+        if(n < 0)
+            x = 1/x;
+        return myPow2(x, n);
     }
 };
