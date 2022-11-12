467bfcc134854cd17ba33e5ef9a46cfc08ab5269
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-12 17:50:30 +0000

    Sync LeetCode submission - Course Schedule II (cpp)

--- problems/course_schedule_ii/solution.cpp
+++ problems/course_schedule_ii/solution.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> graph(numCourses);
         vector<int> indegree(numCourses);
+        vector<vector<int>> graph(numCourses);
+        
         for(auto pre : prerequisites) {
             graph[pre[1]].push_back(pre[0]);
             indegree[pre[0]]++;
         }
+
         queue<int> q;
+        vector<int> res;
         for(int i = 0; i < numCourses; i++) {
             if(indegree[i] == 0)
                 q.push(i);
         }
-        vector<int> res;
         while(!q.empty()) {
             int tmp = q.front();
             res.push_back(tmp);
@@ -27,6 +29,6 @@ public:
         }
         if (res.size() != numCourses)
         res.clear();
-        return res;
+        return res;        
     }
 };
