13ef918f616d2e6587dc53920dfce31ba7c9f8f2
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-12 03:01:40 +0000

    [LeetCode Sync] Runtime - 454 ms (6.81%), Memory - 65.6 MB (100.00%)

--- my-folder/0982-minimum-increment-to-make-array-unique/solution.cpp
+++ my-folder/0982-minimum-increment-to-make-array-unique/solution.cpp
@@ -3,10 +3,11 @@ public:
     int minIncrementForUnique(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         int res = 0, need = 0;
+        int n = nums.size();
         for (int a: nums) {
             res += max(need - a, 0);
             need = max(a, need)+1;
         }
-        return res;        
+        return res;
     }
 };
