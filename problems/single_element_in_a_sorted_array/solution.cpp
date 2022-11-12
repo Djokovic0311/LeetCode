f708a4ec81720547455674d6491db8f9a5c04df2
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-12 21:07:47 +0000

    Sync LeetCode submission - Single Element in a Sorted Array (cpp)

--- problems/single_element_in_a_sorted_array/solution.cpp
+++ problems/single_element_in_a_sorted_array/solution.cpp
@@ -6,6 +6,6 @@ public:
         for(int i = 0; i < n - 2; i+=2) {
             if(nums[i+1] != nums[i]) return nums[i];
         }
-        return nums[n-1];
+        return nums[n-1];        
     }
 };
