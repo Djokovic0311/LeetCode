22cfdfa3a6ce47f338dfc2831d8ffc51d99e8b10
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-12 22:44:31 +0000

    [LeetCode Sync] Runtime - 600 ms (5.59%), Memory - 9.7 MB (89.62%)

--- my-folder/0279-perfect-squares/solution.cpp
+++ my-folder/0279-perfect-squares/solution.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
-    int solve(vector<int>& dp,int n) {
+    int helper(vector<int>& dp, int n) {
         if (n == 0) return 0;
         if(dp[n] != -1) return dp[n];
         dp[n] = INT_MAX;
         for(int i = 1; i * i <= n; i++) {
-            dp[n] = min(dp[n],solve(dp,n-i*i) + 1);
+            dp[n] = min(dp[n],helper(dp,n-i*i) + 1);
         }
         return dp[n];
     }
     int numSquares(int n) {
-        vector <int> dp(n+1, -1);
-        int ans = solve(dp,n);
-        return ans;
+        vector<int> dp(n+1,-1);
+        return helper(dp,n);
     }
 };
