f607744453ab12b36ca37fb3ebc0952388dfef8c
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-12 04:06:33 +0000

    [LeetCode Sync] Runtime - 62 ms (5.10%), Memory - 13.5 MB (100.00%)

--- my-folder/0801-is-graph-bipartite/solution.cpp
+++ my-folder/0801-is-graph-bipartite/solution.cpp
@@ -3,7 +3,7 @@ public:
     bool isBipartite(vector<vector<int>>& graph) {
         int n = graph.size();
         queue<int> q;
-        vector<int> color(n, 0);
+        vector<int> color(n,0);
         for(int i = 0; i < n; i++) {
             if(color[i]) continue;
             color[i] = 1;
@@ -19,7 +19,7 @@ public:
                     else if(color[neighbor] == color[top])
                         return false;
                 }
-            }
+            }            
         }
         return true;
     }
