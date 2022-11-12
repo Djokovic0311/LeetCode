2219f9614f18c7b3047336699a1a52d93316cce8
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-12 21:07:14 +0000

    [LeetCode Sync] Runtime - 3 ms (27.66%), Memory - 5.8 MB (100.00%)

--- my-folder/0007-reverse-integer/solution.cpp
+++ my-folder/0007-reverse-integer/solution.cpp
@@ -5,18 +5,18 @@ public:
         int flag = 0;
         if(x < 0) flag = -1;
         int num = abs(x);
-        cout<<num;
         while(num > 0){
             int tmp = num % 10;
             num /= 10;
             if(result * 10 > INT_MAX) 
                 return 0;
             result = result * 10 + tmp;
-            cout << num << endl;
         }
+        
         if(flag == 0){
             return result;
         }
         else return (-1) * result;
+
     }
 };
