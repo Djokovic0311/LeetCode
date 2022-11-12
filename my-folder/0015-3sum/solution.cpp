f95e16cf980d0488f7e8588d14a2c3ae7aec53b9
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-12 02:41:42 +0000

    [LeetCode Sync] Runtime - 131 ms (14.71%), Memory - 19.9 MB (100.00%)

--- my-folder/0015-3sum/solution.cpp
+++ my-folder/0015-3sum/solution.cpp
@@ -1,34 +1,34 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> res;
         sort(nums.begin(), nums.end());
         int n = nums.size();
-        if(nums.size() < 3){    //Base case 1
+        if(n < 3){    //Base case 1
             return {};
         }
         if(nums[0] > 0){        //Base case 2
             return {};
         }
-        
-        vector<vector<int>> res;
         for(int i = 0; i < n; i++) {
             if(nums[i] > 0)
                 break;
             if (i > 0 and nums[i] == nums[i-1]) continue;
-            int l = i+1, r = n-1;
-            while(l < r) {
-                int sum = nums[i] + nums[l] + nums[r];
-                if(sum < 0) {
-                    l++;
+            int j = i+1, k = n-1;
+            while(j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if(sum == 0) {
+                    res.push_back({nums[i], nums[j], nums[k]});
+                    int last_left = nums[j], last_right = nums[k];
+                    while(j < k && nums[j] == last_left)
+                        j++;
+                    while(j < k && nums[k] == last_right)
+                        k--;
                 }
-                else if(sum > 0)
-                    r--;
-                else {
-                    res.push_back({nums[i], nums[l], nums[r]});
-                    int last_left = nums[l], last_right = nums[r];
-                while (l < r && nums[l] == last_left) ++l;
-				while (l < r && nums[r] == last_right) --r;
+                else if(sum< 0) {
+                    j++;
                 }
+                else k--;
             }
         }
         return res;
