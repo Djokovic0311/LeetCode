a35fe4430852e377c18a2f83a6810bfc28f0d73f
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-16 01:12:06 +0000

    [LeetCode Sync] Runtime - 37 ms (22.09%), Memory - 5.9 MB (100.00%)

--- my-folder/1013-fibonacci-number/solution.cpp
+++ my-folder/1013-fibonacci-number/solution.cpp
@@ -2,14 +2,7 @@ class Solution {
 public:
     int fib(int n) {
         if(n == 0) return 0;
-        if(n == 1 || n== 2) return 1;
-        vector<int> dp(n+1,0);
-        dp[1] = 1;
-        for(int i = 2; i <= n; i++) {
-            
-            dp[i] = dp[i-1] + dp[i-2];
-            // cout<< dp[i] <<endl;
-        }
-        return dp[n];
+        if(n == 1) return 1;
+        return fib(n-1) + fib(n-2);
     }
 };
