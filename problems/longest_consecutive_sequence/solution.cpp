24940f19092bd325723a93def2b79de97e491e26
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-16 02:46:00 +0000

    Sync LeetCode submission - Longest Consecutive Sequence (cpp)

--- problems/longest_consecutive_sequence/solution.cpp
+++ problems/longest_consecutive_sequence/solution.cpp
@@ -8,6 +8,6 @@ public:
             if(nums[i] == nums[i - 1]) continue;
             else if(nums[i] == nums[i - 1] + 1) cur_longest++; 
             else longest = max(longest, cur_longest), cur_longest = 1;  
-        return max(longest, cur_longest);        
+        return max(longest, cur_longest);         
     }
 };
