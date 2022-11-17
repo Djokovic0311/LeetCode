9642cf3c3527ba7eaa47ec7a8262fe9a46ee3c35
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-17 04:04:05 +0000

    Sync LeetCode submission - Convert Binary Search Tree to Sorted Doubly Linked List (cpp)

---
+++ problems/convert_binary_search_tree_to_sorted_doubly_linked_list/solution.cpp
@@ -0,0 +1,55 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+        left = NULL;
+        right = NULL;
+    }
+
+    Node(int _val, Node* _left, Node* _right) {
+        val = _val;
+        left = _left;
+        right = _right;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* first = NULL;
+    Node* last = NULL;
+    void helper(Node* root) {
+        if(root == nullptr) return;
+        else {
+            helper(root->left);
+            if (last) {
+                // link the previous node (last)
+                // with the current one (node)
+                last->right = root;
+                root->left = last;
+            }
+            else {
+                // keep the smallest node
+                // to close DLL later on
+                first = root;
+            }
+            last = root;
+            helper(root->right);
+        }
+    }
+    Node* treeToDoublyList(Node* root) {
+        if (!root) return NULL;
+        helper(root);
+        last->right = first;
+        first->left = last;
+        return first;
+    }
+};
