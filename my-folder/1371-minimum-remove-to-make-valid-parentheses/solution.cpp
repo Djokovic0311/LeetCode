aa2d5303d67e694d083e561776540bef8cd2506d
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-18 19:23:57 +0000

    [LeetCode Sync] Runtime - 30 ms (5.24%), Memory - 11 MB (100.00%)

--- my-folder/1371-minimum-remove-to-make-valid-parentheses/solution.cpp
+++ my-folder/1371-minimum-remove-to-make-valid-parentheses/solution.cpp
@@ -1,43 +1,30 @@
 class Solution {
 public:
     string minRemoveToMakeValid(string s) {
-        int count=0; 
-        int n = s.length();
-        for(int i=0;i<n;++i){
-            if(s[i]=='('){ // for open bracket
-                ++count;
+        int count=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='('){
+                count++;
             }
-            else if(s[i]==')'){ // for close bracket
-                if(count==0){  // if no. of close brackets > no. of open brackets
-                    s[i]='#';
-                }
-                else{
-                    // if matching parentheses found decrease count
-                    --count;
-                }
+            else if(s[i]==')'){
+                if(count>0) count--;
+                else s[i]='*';
             }
         }
         count=0;
-        for(int i=n-1;i>=0;--i){
-            if(s[i]==')'){ // for close bracket
-                ++count;
+        for(int i=s.size()-1;i>=0;i--){
+            if(s[i]==')'){
+                count++;
             }
-            else if(s[i]=='('){ // for open bracket
-                if(count==0){ // if no. of open brackets > no. of close brackets
-                    s[i]='#';
-                }
-                else{
-                    // if matching parentheses found decrease count
-                    --count;
-                }
-            }
-}        
-        string ans="";
-        for(int i=0;i<n;++i){
-            if(s[i]!='#'){ 
-                ans.push_back(s[i]);
+            else if(s[i]=='('){
+                if(count>0) count--;
+                else s[i]='*';
             }
         }
-        return ans;
+        string ans="";
+        for(int i=0;i<s.size();i++){
+            if(s[i]!='*') ans+=s[i];
+        }
+        return ans;       
     }
 };
