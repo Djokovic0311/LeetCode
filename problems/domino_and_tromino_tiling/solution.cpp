274340ac014d35966b54af0ec46a44e12c8b1eab
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-18 17:17:18 +0000

    Sync LeetCode submission - Domino and Tromino Tiling (cpp)

--- problems/domino_and_tromino_tiling/solution.cpp
+++ problems/domino_and_tromino_tiling/solution.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
-int numTilings(int n) {
-    unsigned int dp[n + 3]; dp[0] = 1; dp[1] = 2; dp[2] = 5;
-    for (int i = 3; i < n; i ++) {
-        dp[i] = (2 * dp[i - 1] + dp[i - 3]) % 1000000007;
+    int numTilings(int n) {
+        unsigned int dp[n + 3]; dp[0] = 1; dp[1] = 2; dp[2] = 5;
+        for (int i = 3; i < n; i ++) {
+            dp[i] = (2 * dp[i - 1] + dp[i - 3]) % 1000000007;
+        }
+        return dp[n - 1];
     }
-    return dp[n - 1];
-}
 };
