dc26ae91e9688f6ee3284d6c8f6a8280d7ea8c26
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-18 19:34:09 +0000

    [LeetCode Sync] Runtime - 268 ms (5.04%), Memory - 14.6 MB (95.30%)

--- my-folder/0322-coin-change/solution.cpp
+++ my-folder/0322-coin-change/solution.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
-    int coinChange(vector<int>& coins, int n) {
-        // creating the base dp array, with first value set to 0
-        int dp[++n];
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        vector<int> dp(++amount);
         dp[0] = 0;
-        // more convenient to have the coins sorted
-        sort(begin(coins), end(coins));
-        // populating our dp array
-        for (int i = 1; i < n; i++) {
-            // setting dp[0] base value to 1, 0 for all the rest
+        sort(coins.begin(), coins.end());
+        for(int i = 1; i < amount; i++) {
             dp[i] = INT_MAX;
-            for (int c: coins) {
-                if (i - c < 0) break;
-                // if it was a previously not reached cell, we do not add use it
-                if (dp[i - c] != INT_MAX) dp[i] = min(dp[i], 1 + dp[i - c]);
+            for(int j = 0; j < n; j++) {
+                if(i-coins[j]<0) break;
+                if(dp[i-coins[j]] != INT_MAX)
+                    dp[i] = min(dp[i], dp[i-coins[j]]+1);
+                
             }
         }
-        return dp[--n] == INT_MAX ? -1 : dp[n];        
+        return dp[--amount] == INT_MAX ? -1: dp[amount];
     }
 };
