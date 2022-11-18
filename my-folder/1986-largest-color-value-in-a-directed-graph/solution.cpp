124e38649831772e3f05a6393947ba1a17f162a6
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-18 17:50:27 +0000

    [LeetCode Sync] Runtime - 1518 ms (5.08%), Memory - 174.6 MB (73.52%)

--- my-folder/1986-largest-color-value-in-a-directed-graph/solution.cpp
+++ my-folder/1986-largest-color-value-in-a-directed-graph/solution.cpp
@@ -1,31 +1,41 @@
 class Solution {
 public:
-    int largestPathValue(string cs, vector<vector<int>>& edges) {
-        vector<vector<int>> al(cs.size()), cnt(cs.size(), vector<int>(26));
-        vector<int> indegrees(cs.size());
-        for (auto &e: edges) {
-            al[e[0]].push_back(e[1]);
-            ++indegrees[e[1]];
+    int largestPathValue(string colors, vector<vector<int>>& edges) {
+        // int res = 0;
+        int n = colors.length();
+
+        vector<vector<int>> adjList(n), cnt(n, vector<int>(26));
+        vector<int> indegree(n);
+
+        for(auto & e : edges) {
+            adjList[e[0]].push_back(e[1]);
+            indegree[e[1]]++;
         }
+
         vector<int> q;
-        for (int i = 0; i < cs.size(); ++i)
-            if (indegrees[i] == 0)
+
+        for(int i = 0; i < n; i++){
+            if(indegree[i] == 0) {
                 q.push_back(i);
+            }
+        }
         int res = 0, processed = 0;
-        while (!q.empty()) {
+
+        while(!q.empty()) {
             vector<int> q1;
             for (auto i : q) {
                 ++processed;
-                res = max(res, ++cnt[i][cs[i] - 'a']);
-                for (auto j : al[i]) {
+                res = max(res, ++cnt[i][colors[i] - 'a']);
+                for (auto j : adjList[i]) {
                     for (auto k = 0; k < 26; ++k)
                         cnt[j][k] = max(cnt[j][k], cnt[i][k]);
-                    if (--indegrees[j] == 0)
+                    if (--indegree[j] == 0)
                         q1.push_back(j);
                 }
             }
             swap(q, q1);
+
         }
-        return processed != cs.size() ? -1 : res;
+        return processed != n ? -1 : res;
     }
 };
