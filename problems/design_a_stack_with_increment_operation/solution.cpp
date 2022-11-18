56c5da4ccc50f6aa218765dc97d6956a1f13ba48
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-18 16:47:43 +0000

    Sync LeetCode submission - Design a Stack With Increment Operation (cpp)

--- problems/design_a_stack_with_increment_operation/solution.cpp
+++ problems/design_a_stack_with_increment_operation/solution.cpp
@@ -9,6 +9,11 @@ public:
             vec.push_back(make_pair(x, 0));
             maxSz--;
         }
+        // cout << "push " << x << endl;
+        // for(auto v : vec) {
+        //     cout << v.first << ' ' << v.second << endl;
+        // }
+        // cout << endl;
     }
     
     int pop() {
@@ -19,7 +24,16 @@ public:
         maxSz++;
         if(!vec.empty())
             vec.back().second += inc;
+        // cout << "pop " << res << endl;
+        // for(auto v : vec) {
+        //     cout << v.first << ' ' << v.second << endl;
+        // }
+        // cout << endl;
         return res;
+        // for(auto v : vec) {
+        //     cout << v.first << ' ' << v.second << endl;
+        // }
+        // cout << endl;
     }
     
     void increment(int k, int val) {
@@ -28,16 +42,14 @@ public:
         int pos = n>=k ? k : n;
         // last element to change
         vec[pos-1].second += val;
+        // cout << "increase " << k << " by " << val << endl;
+        // for(auto v : vec) {
+        //     cout << v.first << ' ' << v.second << endl;
+        // }
+        // cout << endl;
+
     }
 private:
     vector<pair<int,int>> vec;
     int maxSz;
 };
-
-/**
- * Your CustomStack object will be instantiated and called as such:
- * CustomStack* obj = new CustomStack(maxSize);
- * obj->push(x);
- * int param_2 = obj->pop();
- * obj->increment(k,val);
- */
