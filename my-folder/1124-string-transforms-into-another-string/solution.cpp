ba6eb7176ce9308bce82ec85207210c2f8b249e3
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-18 17:23:22 +0000

    [LeetCode Sync] Runtime - 16 ms (18.60%), Memory - 7.7 MB (100.00%)

--- my-folder/1124-string-transforms-into-another-string/solution.cpp
+++ my-folder/1124-string-transforms-into-another-string/solution.cpp
@@ -2,9 +2,12 @@ class Solution {
 public:
     bool canConvert(string str1, string str2) {
         unordered_map<char, char> mp;
-        int n = str1.length();
         string tmp = "";
+
         if(str1 == str2) return true;
+
+        int n = str1.length();
+
         for(int i = 0; i < n; i++) {
             if(mp.find(str1[i]) == mp.end()) {
                 mp[str1[i]] = str2[i];
@@ -12,11 +15,10 @@ public:
             }
             else {
                 tmp += mp[str1[i]];
-            }    
-            
+            }
         }
         set<char> st(tmp.begin(), tmp.end());
-        if(st.size() == 26) return false;
+        if(st.size() == 26) return false;        
         return tmp == str2;
     }
 };
