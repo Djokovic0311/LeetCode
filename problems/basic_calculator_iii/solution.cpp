43a2433cae90de0bcaa071b7eb3ee80a8ab6588d
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-18 18:47:13 +0000

    Sync LeetCode submission - Basic Calculator III (cpp)

--- problems/basic_calculator_iii/solution.cpp
+++ problems/basic_calculator_iii/solution.cpp
@@ -17,16 +17,16 @@ public:
                 case '*' : nums.back() *= n; break;
                 case '/' : nums.back() /= n; break;
             }            
-            if (i < s.length())                  // handle over flow of i in cases like "1 + 1"
+            if (i < s.length())    // handle over flow of i in cases like "1 + 1"
                 op = s[i];
         }
-        // for(int k : nums)
-        //     cout << k << " ";
-        // cout << endl;
+        for(int k : nums)
+            cout << k << " ";
+        cout << endl;
         return accumulate(nums.begin(), nums.end(), 0);
     }
     
-    long parseNum(string& s, int& i) {            // change to long to pass case like "0-2147483648"
+    long parseNum(string& s, int& i) {  // change to long to pass case like "0-2147483648"
         long n = 0;
         while(i < s.length() && isdigit(s[i]))
             n = s[i++] - '0' + 10 * n;
