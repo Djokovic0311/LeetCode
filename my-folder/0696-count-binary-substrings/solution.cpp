fea4204076676dc42932b5cc89279e131cc6642d
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-18 16:46:31 +0000

    [LeetCode Sync] Runtime - 87 ms (5.31%), Memory - 10.5 MB (100.00%)

--- my-folder/0696-count-binary-substrings/solution.cpp
+++ my-folder/0696-count-binary-substrings/solution.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
     int countBinarySubstrings(string s) {
-          int curConsecutive = 1, prevConsecutive = 0, ans = 0;
-          for(int i = 1; i < size(s); i++) {
-            if(s[i] != s[i - 1]) 
-                ans += min(prevConsecutive, curConsecutive), 
-                prevConsecutive = curConsecutive,            
-                curConsecutive = 1;                          
-            else 
-                curConsecutive++; 
-          }
-          ans += min(prevConsecutive, curConsecutive); 
-          return ans;        
+        int cur = 0, prev = 0;
+        int ans = 0;
+        int n = s.length();
+
+        for(int i = 0; i < n; i++) {
+            if(i && s[i] == s[i-1]) {
+                cur++;
+            }
+            else {
+                prev = cur;
+                cur = 1;
+                
+            }
+            if(cur <= prev)
+                ans++;
+        }
+        return ans;
     }
 };
