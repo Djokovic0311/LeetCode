1f44c31bc735df9338ede56968a0461e47d7510d
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-18 16:31:13 +0000

    Sync LeetCode submission - Longest String Chain (cpp)

--- problems/longest_string_chain/solution.cpp
+++ problems/longest_string_chain/solution.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     int longestStrChain(vector<string>& words) {
+        int n = words.size();
         unordered_map<string, int> dp;
-        int res = 1;
         sort(words.begin(), words.end(), [](const string &l, const string &r) { return l.size() < r.size(); });
-        for (string word : words) {
-            dp[word] = 1;
-            for (int i = 0; i < word.size(); i++) {
-                string prev = word.substr(0, i) + word.substr(i + 1);
-                if (dp.find(prev) != dp.end()) {
-                    dp[word] = max(dp[word], dp[prev] + 1 );
-                    res = max(res, dp[word]);
+        int res = 1;
+        for(string w : words) {
+            dp[w] = 1;
+            for(int i = 0; i < w.length(); i++) {
+                string prev = w.substr(0, i) + w.substr(i + 1);
+                if(dp.find(prev) != dp.end()) {
+                    dp[w] = max(dp[w], dp[prev] + 1);
+                    res = max(res, dp[w]);
                 }
             }
         }
-        return res;        
+        return res;
     }
 };
