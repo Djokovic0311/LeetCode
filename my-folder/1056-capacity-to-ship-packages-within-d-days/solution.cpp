5237cc29264309c89de32b34f000c0f280dfa157
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-19 00:19:28 +0000

    [LeetCode Sync] Runtime - 48 ms (5.04%), Memory - 26.2 MB (100.00%)

--- my-folder/1056-capacity-to-ship-packages-within-d-days/solution.cpp
+++ my-folder/1056-capacity-to-ship-packages-within-d-days/solution.cpp
@@ -5,8 +5,9 @@ public:
         int left = 0, right = 0;
         for(auto w: weights) left = max(left, w), right += w;
         if(days == weights.size()) return left;
+
         while(left < right) {
-            int mid = (left + right) / 2;
+            int mid = left + (right - left) / 2;
             int curr_days = 1, curr_bagsize = 0;
             for(auto w : weights) {
                 if(curr_bagsize + w > mid) { 
@@ -14,9 +15,14 @@ public:
                 }
                 curr_bagsize += w;
             }
-            if(curr_days > days) left = mid + 1; //we need bigger bag sizes to accomodate
-            else right = mid; 
+            if(curr_days > days) {
+                left = mid + 1;
+            }
+            else {
+                right = mid;
+            }
+              
         }
-        return left;        
+        return left;
     }
 };
