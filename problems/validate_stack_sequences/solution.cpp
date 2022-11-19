53fdd56240abcc5bc4c1678fd6bd86629276fbad
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-19 00:25:07 +0000

    Sync LeetCode submission - Validate Stack Sequences (cpp)

--- problems/validate_stack_sequences/solution.cpp
+++ problems/validate_stack_sequences/solution.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
-        
-        stack<int> st; // Create a stack
-        
-        int j = 0; // Intialise one pointer pointing on popped array
-        
-        for(auto val : pushed){
-            st.push(val); // insert the values in stack
-            while(st.size() > 0 && st.top() == popped[j]){ // if st.peek() values equal to popped[j];
-                st.pop(); // then pop out
+        stack<int> stk;
+        int j = 0;
+
+        int n = pushed.size();
+
+        for(int i = 0; i < n; i++) {
+            stk.push(pushed[i]);
+            while(stk.size() > 0 && stk.top() == popped[j]){ // if st.peek() values equal to popped[j];
+                stk.pop(); // then pop out
                 j++; // increment j
             }
         }
-        return st.size() == 0; // check if stack is empty return true else false        
+        return stk.empty();
     }
 };
