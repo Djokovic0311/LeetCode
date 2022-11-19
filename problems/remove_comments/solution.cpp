3c9b6ec5c2921420850700cde514968f3b2223ca
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-19 15:59:54 +0000

    Sync LeetCode submission - Remove Comments (cpp)

--- problems/remove_comments/solution.cpp
+++ problems/remove_comments/solution.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
     vector<string> removeComments(vector<string>& source) {
-                vector<string> ans;
-        string s;
+        vector<string> res;
         bool comment = false;
+        int n = source.size();
+        string s;
+
         for(int i = 0; i < source.size(); i++) {
             for(int j = 0; j < source[i].size(); j++) {
                 if(!comment && j + 1 < source[i].size() && source[i][j] == '/' && source[i][j+1]=='/') break;
@@ -12,8 +14,9 @@ public:
                 else if(!comment) s.push_back(source[i][j]);
             }
             
-            if(!comment && s.size()) ans.push_back(s), s.clear();
+            if(!comment && s.size()) res.push_back(s), s.clear();
         }
-        return ans;
+        return res;
+
     }
 };
