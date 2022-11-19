2b787655a2e665e7eca7b76a8ad6205ebb5a6eee
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-19 17:52:01 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 7 MB (100.00%)

--- my-folder/0163-missing-ranges/solution.cpp
+++ my-folder/0163-missing-ranges/solution.cpp
@@ -4,21 +4,15 @@ public:
         vector<string> res;
         nums.insert(nums.begin(),lower);
         nums.insert(nums.end(), upper);
-        
+
         for (int i = 0; i < nums.size() - 1; i++) {
             int start = nums[i] + 1;
             int end = nums[i + 1] - 1;
             
-            // If nums[i] == lower, range should be [nums[i], nums[i + 1] - 1],
-            // instead of [nums[i] + 1, nums[i + 1] - 1]
-            // [1,3,8] -> [0,1,3,8,10], first range should be [0]
             if (i == 0) {
                 start = lower;
             }
             
-            // If nums[i + 1] == upper, range should be [nums[i] + 1, nums[i + 1]],
-            // instead of [nums[i] + 1, nums[i + 1] - 1]
-            // [1,3,8] -> [0,1,3,8,10], last range should be [9,10]
             if (i == nums.size() - 2) {
                 end = nums[i + 1];
             }
@@ -32,6 +26,6 @@ public:
             }
         }
         
-        return res;
+        return res;     
     }
 };
