f7108a3ca8c3f341d32c8a808741584ca6ad7926
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-19 03:46:40 +0000

    [LeetCode Sync] Runtime - 262 ms (58.44%), Memory - 8.1 MB (100.00%)

--- my-folder/0079-word-search/solution.cpp
+++ my-folder/0079-word-search/solution.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     string word;
-    bool dfs(vector<vector<char>>& board, int i, int j, int n) {
+    bool dfs(vector<vector<char>>& board, int i,  int j, int n) {
         bool status;
-        if(n == word.length()) return true;
+        if(n == word.length())
+            return true;
         if(i < 0 || i >= board.size() || j < 0 || j >= board[0].size() || board[i][j] != word[n])
             return false;
         board[i][j] = '0';
         status = dfs(board, i+1, j, n+1) || dfs(board, i, j+1, n+1) || dfs(board, i-1, j, n+1) || dfs(board, i, j-1, n+1);
         board[i][j] = word[n];
-            return status;
+        return status;
     }
     bool exist(vector<vector<char>>& board, string word) {
         this->word = word;
@@ -18,7 +19,7 @@ public:
             for(int j = 0; j < n; j++) {
                 bool status = dfs(board, i, j, 0);
                 if(status == true)
-                    return true;
+                    return true;                
             }
         }
         return false;
