21f7bf64a765b969cb434d07582a1f2cae593e87
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-19 04:16:07 +0000

    Sync LeetCode submission - Longest Substring with At Most K Distinct Characters (cpp)

--- problems/longest_substring_with_at_most_k_distinct_characters/solution.cpp
+++ problems/longest_substring_with_at_most_k_distinct_characters/solution.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
-    int lengthOfLongestSubstringKDistinct(string s, int k) 
-    {
+    int lengthOfLongestSubstringKDistinct(string s, int k) {
         int res = 0;
         unordered_map<char, int> count; // track number occurances of each char between 'l' & 'r'
         for(int l = 0, r = 0; r < s.length(); ++r) 
@@ -14,6 +13,6 @@ public:
             }
             res = max(res, r-l+1);
         }
-        return res;
+        return res;        
     }
 };
