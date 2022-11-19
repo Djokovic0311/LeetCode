60d9c9fafccac9c5df8e15cb5e8cdd4b6a96cac9
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-19 03:55:09 +0000

    Sync LeetCode submission - Russian Doll Envelopes (cpp)

--- problems/russian_doll_envelopes/solution.cpp
+++ problems/russian_doll_envelopes/solution.cpp
@@ -13,11 +13,16 @@ public:
         vector<int>lis;
         
         for(i=0; i<n; i++){
+            // cout 
             auto it = lower_bound(lis.begin(), lis.end(), envelopes[i][1]);
+            
             if(it==lis.end()){lis.push_back(envelopes[i][1]);}
             else{
+                // cout << (*it) << endl;
                 *it = envelopes[i][1];
             }
+            // for(int x : lis) cout << x << ' ';
+            // cout << endl;
         }
         return lis.size();
     }
