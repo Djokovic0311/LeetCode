1b4bf924e7fe1e4c9a36558ae7195eeec60535c3
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-19 02:10:11 +0000

    Sync LeetCode submission - Split Array Largest Sum (cpp)

--- problems/split_array_largest_sum/solution.cpp
+++ problems/split_array_largest_sum/solution.cpp
@@ -1,33 +1,32 @@
 class Solution {
 public:
     int splitArray(vector<int>& nums, int m) {
-         long long int runningsum, n=nums.size();
-         vector<long long int> sums(n,0);
-         runningsum=sums[n-1]=nums[n-1];
-         for(int i=n-2;i>=0;i--){
-             runningsum+=nums[i];
-             sums[i]=runningsum;
-             // cout << sums[i] << " ";
-         }
-         if(m==1) return sums[0];
-         vector<vector<long long int>> dp(n,vector<long long int>(m+1,2147483648));
-         dp[n-1][1]=nums[n-1];    //initialisation
-         for(int i=n-2;i>0;i--){
-             int j=1;             //start calculating for all m in possible range
-             while(j<m&&j<=n-i){
-                 if(j==1) dp[i][1]=sums[i]; //no partition
-                 else{
-                     for(int k=i+1;k<n;k++){ // deciding on numb of elems to be include in the set with starting elem
-                         dp[i][j]=min(dp[i][j],(max(sums[i]-sums[k],dp[k][j-1]))); 
-                     }
-                 }
-                 j++;
-             }
-         }
-         for(int k=1;k<n;k++){
-             dp[0][m]=min(max(sums[0]-sums[k],dp[k][m-1]),dp[0][m]);
-         }
-         return dp[0][m];
-        
+        long long low = INT_MIN, high = 0;
+        for (int n : nums)
+            high += n, low = max<long long>(low, n);
+        low = max(low , high / 50);
+        while (low < high)
+        {
+            long long mid = low + (high - low) / 2;
+            if (test(nums, m, mid))
+                high = mid;
+            else
+                low = mid + 1;
+        }
+        return high;
+    }
+    bool test(vector<int>& nums, int m, long long bar)
+    {
+        int sum = 0, count = 1;
+        for (int n : nums)
+            if (sum + n > bar)
+            {
+                sum = n;
+                count ++;
+                if (count > m)
+                    return false;
+            }else
+                sum += n;
+        return true;
     }
 };
