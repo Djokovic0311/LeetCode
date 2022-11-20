967504589b2a30239652026e00e72eab80c9551d
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-20 18:06:23 +0000

    Sync LeetCode submission - Jump Game (cpp)

--- problems/jump_game/solution.cpp
+++ problems/jump_game/solution.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        vector<bool> visited(n, false);
+
         queue<pair<int,int>> q;
         q.push({0,nums[0]});
-        int n = nums.size();
-        bool * test;
-        test = new bool[n];
-        for(int i =0; i < n;i++) {
-            test[i] = 0;
-        }
-        test[0] = 1;
+
+        visited[0] = true;
+        if(n == 1) return true;
         while(!q.empty()) {
-            auto tmp = q.front();
-            for(int i = tmp.first; i < n && i <= tmp.first+tmp.second; i++) {
-                if(!test[i]){
-                    q.push({i,nums[i]});
-                    test[i] = true;
-                }
-                if(test[n-1]) return true;
-            }
+            auto [idx, jump] = q.front();
             q.pop();
+            for(int i = 1; i <= jump; i++) {
+                int nidx = idx + i;
+                // cout << idx << ' ' << jump << endl; 
+                if(nidx < n && !visited[nidx]) {
+                    visited[nidx] = true;
+                    q.push({nidx, nums[nidx]});
+                }
+                if(visited[n-1]) return true;           
+            }
+
         }
         return false;
     }
