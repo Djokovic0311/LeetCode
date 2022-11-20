d6cbccf330bf300eeb670087d11df618e44ff136
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-20 02:21:26 +0000

    Sync LeetCode submission - Reverse Nodes in k-Group (cpp)

--- problems/reverse_nodes_in_k-group/solution.cpp
+++ problems/reverse_nodes_in_k-group/solution.cpp
@@ -11,30 +11,36 @@
 class Solution {
 public:
     ListNode* reverseKGroup(ListNode* head, int k) {
-        ListNode* dummy = new ListNode(0);
-        dummy->next = head;
-        ListNode* before = dummy;
-        ListNode* after = head;
-        ListNode* curr = nullptr;
-        ListNode* prev = nullptr;
-        ListNode* nxt = nullptr;
-        while(true) {
-            ListNode* tmp = after;
-            for(int i = 0; i < k; i++) {
-                if(tmp == nullptr) return dummy->next;
-                tmp = tmp->next;
-            }
-            curr = after, prev = before;
-            for(int i = 0; i < k; i++){
-                nxt = curr->next;
-                curr->next = prev;
-                prev = curr;
-                curr = nxt;
-            }
-            after->next = curr;
-            before->next = prev;
-            before = after;
-            after = curr;
+ 		//edge cases
+        if (head == NULL || k ==0 ||k==1)return head;
+		// Store all node values in a vector.
+        vector<int> v;
+        ListNode* p = head;
+        while(p){
+            v.push_back(p->val);
+            p = p->next;
         }
-    }
+        int n = v.size();
+		// r is how many sub groups are to be reversed; 
+        int r = n/k;
+        int q = 0;
+		// Apply operations on the vector i.e, swap values in a loop. 
+        while(r--){
+            int i = k*q;
+            int j = (k)*(q+1) - 1;
+            while(i<j){
+                swap(v[i++], v[j--]);
+            }
+            q++;
+        }
+		// Make a new LinkedList by the processed vector and return it.
+        ListNode* a = new ListNode(v[0]);
+        ListNode* b = a;
+        for(int i =1; i<v.size(); i++){
+            b->next = new ListNode(v[i]);
+            b = b->next;
+        }
+        return a;
+    }       
+    
 };
