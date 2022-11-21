4367794bc62a70d368c336a42878e564a1d006b1
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-11-21 02:02:08 +0000

    Sync LeetCode submission - Maximum Sum of Two Non-Overlapping Subarrays (cpp)

--- problems/maximum_sum_of_two_non-overlapping_subarrays/solution.cpp
+++ problems/maximum_sum_of_two_non-overlapping_subarrays/solution.cpp
@@ -32,7 +32,18 @@ public:
         }
         return ans;
     }
-    int maxSumTwoNoOverlap(vector<int>& nums, int firstLen, int secondLen){
+    int maxSumTwoNoOverlap(vector<int>& nums, int firstLen, int secondLen) {
+      /*
+      The idea is to find the max sum subbary of given size firstlen and seconlen seperately
+        Find prefix sum for firstlen and suffix sum for secondLen and vice-versa
+        Dp1-> it will store the max sum of the subarray of size firstlen from 0th to ith index.
+        Dp2-> it will store the max sum of the subarray from last index to ith index
+        and then check from a window of fisrstlen-1 to nums.size()- secondlen
+        which ever has the max sum will be our result
+
+
+      */ 
         return max(helper(nums, firstLen, secondLen),helper(nums, secondLen, firstLen));
+    
     }
 };
