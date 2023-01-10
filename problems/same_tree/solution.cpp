392ffb2030ce21187b9749a5673dafb631c6bd0b
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2023-01-10 00:22:10 +0000

    Sync LeetCode submission - Same Tree (cpp)

--- problems/same_tree/solution.cpp
+++ problems/same_tree/solution.cpp
@@ -12,9 +12,9 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-          if(p==nullptr && q==nullptr) return true;
-          if(p==nullptr || q==nullptr) return false;
-          if(q->val!=p->val) return false;
-          return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);        
+        if(p==nullptr && q==nullptr) return true;
+        if(p==nullptr || q==nullptr) return false;
+        if(q->val!=p->val) return false;
+        return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);        
     }
 };
