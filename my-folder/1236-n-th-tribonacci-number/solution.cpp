56f14bcc28dba5c590164e5ada3f585a9aa88dd9
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2023-01-30 02:55:15 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 6.1 MB (100.00%)

--- my-folder/1236-n-th-tribonacci-number/solution.cpp
+++ my-folder/1236-n-th-tribonacci-number/solution.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int tribonacci(int n) {
-        int a[38] = {0};
+                int a[38] = {0};
         a[0] = 0;
         a[1] = a[2] = 1;
         for(int i = 3; i < 38; i++) {
