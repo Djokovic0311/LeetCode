1244f4015f85d5ec742072050274facfed6b7004
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2023-02-04 17:53:31 +0000

    [LeetCode Sync] Runtime - 815 ms (5.26%), Memory - 24.9 MB (100.00%)

---
+++ my-folder/2673-maximize-win-from-two-segments/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maximizeWin(self, prizePositions: List[int], k: int) -> int:
+        n = len(prizePositions)
+        best = [0]*(n+1)  # best segment after >= i 
+        res = 0
+        for i in range(n-1,-1,-1):   # curr seg start at ith
+            e = bisect.bisect_right(prizePositions,prizePositions[i]+k)   # take maximum as possible
+            res = max(res,e-i + best[e])  # maximize the segments by curr seg + next segment after >= e
+            best[i] = max(best[i+1],e-i)  # track the best segment
+        return res
