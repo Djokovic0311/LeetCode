de70e4dd107456236828802662b44aec37be0078
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2023-02-15 04:06:35 +0000

    [LeetCode Sync] Runtime - 913 ms (52.39%), Memory - 14 MB (100.00%)

---
+++ my-folder/0972-knight-dialer/solution.py
@@ -0,0 +1,24 @@
+class Solution:
+    def knightDialer(self, n: int) -> int:
+        
+        neighbors = {
+            0:(4,6),
+            1:(6,8),
+            2:(7,9),
+            3:(4,8),
+            4:(0,3,9),
+            5:(),
+            6:(0,1,7),
+            7:(2,6),
+            8:(1,3),
+            9:(2,4)
+        }
+        current_counts = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+        for _ in range(n-1):
+            next_counts = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            for src_key in range(10):
+                for dst_key in neighbors[src_key]:
+                    next_counts[dst_key] = (next_counts[dst_key] + current_counts[src_key]) % (10**9 + 7)
+            current_counts = next_counts
+        return sum(current_counts) % (10**9 + 7)
+
