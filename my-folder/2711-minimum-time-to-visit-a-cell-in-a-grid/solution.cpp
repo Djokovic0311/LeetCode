434f15465fe73be01d8d41ab414d1aeee1385fb4
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2023-02-26 04:10:03 +0000

    [LeetCode Sync] Runtime - 1359 ms (5.00%), Memory - 109.5 MB (32.60%)

---
+++ my-folder/2711-minimum-time-to-visit-a-cell-in-a-grid/solution.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    int minimumTime(vector<vector<int>>& grid) {
+        int row=grid.size(), col=grid[0].size(), ans=INT_MAX;
+        vector<vector<int>> direction{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        vector<vector<bool>> visited(row, vector<bool>(col, false));
+        
+        priority_queue<vector<int>, vector<vector<int>>, greater<>> pq;
+    
+        visited[0][0]=true;
+        
+        // do initial explore
+        if(grid[1][0]<=1){
+            pq.push({1, 1, 0});
+            visited[1][0]=true;
+        }
+        if(grid[0][1]<=1){
+            pq.push({1, 0, 1});
+            visited[0][1]=true;
+        }
+        
+        // if no initial round return 0
+        if(pq.size()==0){
+            return -1;
+        }
+        
+        while(pq.size()){
+            auto curr = pq.top();
+            pq.pop();
+            int time=curr[0];
+            int r=curr[1], c=curr[2];
+            
+            if(r==row-1 && c==col-1){
+                return time;
+            }
+            
+            for(auto& i:direction){
+                int newR=r+i[0];
+                int newC=c+i[1];
+                if(newR>=0 && newR<row && newC>=0 && newC<col && visited[newR][newC]==false){
+                    if(time+1>=grid[newR][newC]){
+                        pq.push({time+1, newR, newC});
+                    // if difference between two cell is more than 1
+                    }else{
+                        int diff=grid[newR][newC]-time;
+                        // steps to get two next cell
+                        if(diff%2==0){
+                            pq.push({grid[newR][newC]+1, newR, newC});
+                        }else{
+                            pq.push({grid[newR][newC], newR, newC});
+                        }
+                    }
+                    visited[newR][newC]=true;
+                }
+            }
+        }
+        
+        
+        return -1;
+    }
+};
