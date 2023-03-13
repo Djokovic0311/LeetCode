e825a3b20cc9a3da88990dda29d35f653a4f3b28
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2023-03-13 01:24:46 +0000

    Sync LeetCode submission - Unique Paths (cpp)

--- problems/unique_paths/solution.cpp
+++ problems/unique_paths/solution.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        int dp[m][n];
-        for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if(i==0||j==0)
-                    dp[i][j]=1;
-                else
-                    dp[i][j]=dp[i-1][j]+dp[i][j-1];
+        vector<vector<int>> dp(m, vector<int>(n, 1));
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
             }
         }
-        return dp[m-1][n-1];
+        return dp[m - 1][n - 1];
     }
 };
