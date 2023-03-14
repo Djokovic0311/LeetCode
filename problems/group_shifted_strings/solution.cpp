1b37d09bf79932787ba4183c1e2266a90f8ac6ca
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2023-03-14 17:55:02 +0000

    Sync LeetCode submission - Group Shifted Strings (cpp)

---
+++ problems/group_shifted_strings/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<string>> groupStrings(vector<string>& strings) {
+        vector<vector<string>> res;
+        unordered_map<string,vector<string>> mp;
+        for(string s : strings) {
+            int start = s[0]-'a';
+            string tmp = s;
+            for(int i = 0; i < tmp.length(); i++) {
+                tmp[i] -= start;
+                if(tmp[i] < 'a') tmp[i] = 'z' -('a'-tmp[i]-1);
+                else if(tmp[i] > 'z') tmp[i] = 'a' + (tmp[i]-'z'-1);
+            }
+            cout << tmp << " " << s << endl;
+            mp[tmp].push_back(s);
+        }
+
+        for(auto it : mp) {
+            res.push_back(it.second);
+        }
+
+        return res;
+    }
+};
