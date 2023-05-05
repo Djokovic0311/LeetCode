23501b0eb884b2e1e2fbbc78fd4c8dffa2091273
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2023-05-05 01:14:43 +0000

    [LeetCode Sync] Runtime - 4 ms (41.79%), Memory - 7.2 MB (100.00%)

---
+++ my-folder/0649-dota2-senate/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string predictPartyVictory(string senate) {
+        int score = 0;
+        for (int i = 0; i < senate.size(); ++i) {
+            const char ch = senate[i];
+            if (ch == 'R') {
+                if (score < 0) 
+                    senate.push_back('D');
+                
+                ++score;
+            } else {
+                if (score > 0) 
+                    senate.push_back('R');
+                
+                score--;
+            }
+        }
+        return score > 0 ? "Radiant" : "Dire";        
+    }
+};
