027c4470c774bd4d3e0ec2dc0e9bf3efc7d5756a
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2023-05-15 17:47:24 +0000

    Sync LeetCode submission - Swapping Nodes in a Linked List (cpp)

--- problems/swapping_nodes_in_a_linked_list/solution.cpp
+++ problems/swapping_nodes_in_a_linked_list/solution.cpp
@@ -8,6 +8,16 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* swapNodes(ListNode* head, int k) {
