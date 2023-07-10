e0eb19317a954d54a0a92236813a228d8d859394
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2023-07-10 17:21:55 +0000

    [LeetCode Sync] Runtime - 311 ms (5.90%), Memory - 144.7 MB (99.95%)

--- my-folder/0111-minimum-depth-of-binary-tree/solution.cpp
+++ my-folder/0111-minimum-depth-of-binary-tree/solution.cpp
@@ -11,11 +11,20 @@
  */
 class Solution {
 public:
-    
     int minDepth(TreeNode* root) {
-        if(!root) return 0;
-        int r = minDepth(root->right);
-        int l = minDepth(root->left);
-        return 1 + (min(l, r) ? min(l, r) : max(l, r));
+        if(!root)
+            return 0;
+        // If left subtree is NULL, recur for right subtree
+        if (root->left == NULL) {
+            return minDepth(root->right) + 1;
+        }
+
+        // If right subtree is NULL, recur for left subtree
+        if (root->right == NULL) {
+            return minDepth(root->left) + 1;
+        }
+
+        // If root has both left and right children, recur for both subtrees and take the minimum of two values
+        return std::min(minDepth(root->left), minDepth(root->right)) + 1;
     }
 };
