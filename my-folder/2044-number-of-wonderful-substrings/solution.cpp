7e4180cd968c8a78b0e15fd278dace4ba1b59f13
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2023-09-22 04:15:24 +0000

    [LeetCode Sync] Runtime - 344 ms (17.53%), Memory - 18 MB (90.44%)

---
+++ my-folder/2044-number-of-wonderful-substrings/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    long long wonderfulSubstrings(std::string word) {
+        unordered_map<int, int> count = {{0, 1}};  // Initialize with empty string
+        int mask = 0;
+        long long res = 0;
+        
+        for (char c : word) {
+            mask ^= (1 << (c - 'a'));  // Flip the corresponding bit for character c
+            
+            res += count[mask];  // Current substring itself is wonderful
+            
+            // Check for substrings where only one character has odd occurrences
+            for (int i = 0; i < 10; ++i) {
+                res += count[mask ^ (1 << i)];
+            }
+            
+            count[mask]++;  // Increase the frequency for current mask
+        }
+        
+        return res;
+    }
+};
