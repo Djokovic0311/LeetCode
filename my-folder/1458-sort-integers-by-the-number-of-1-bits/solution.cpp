ff3741b8e7a1d0c6fd19c230ec22cb78db72239c
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2023-10-30 00:17:48 +0000

    [LeetCode Sync] Runtime - 8 ms (14.14%), Memory - 10.6 MB (100.00%)

--- my-folder/1458-sort-integers-by-the-number-of-1-bits/solution.cpp
+++ my-folder/1458-sort-integers-by-the-number-of-1-bits/solution.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
-    static int numberof1s(int a){
-            int count = 0;
-            while(a){
-                a = a&(a-1);
-                count++;
-            }
-            return count;
+static int numberof1s(int a){
+        int count = 0;
+        while(a){
+            a = a&(a-1);
+            count++;
         }
+        return count;
+    }
+    static bool compare(int a, int b){
+        int p = numberof1s(a);
+        int q = numberof1s(b);
+        if(p==q){
+            return (a<=b);
+        }
+        else{
+            return (p<q);
+        }
+    }
     vector<int> sortByBits(vector<int>& arr) {
-        vector<int> res;
-        int n = arr.size();
-        sort(arr.begin(), arr.end(), [](const int& a, const int& b) {
-            return numberof1s(a) == numberof1s(b)?a<=b:numberof1s(a) < numberof1s(b);});
+        sort(arr.begin(),arr.end(),compare);
         return arr;
     }
 };
