0a65756f454d23ed8872e59d0aaf703af1012675
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2023-11-09 23:04:27 +0000

    [LeetCode Sync] Runtime - 30 ms (5.06%), Memory - 12.8 MB (100.00%)

---
+++ my-folder/2043-cyclically-rotating-a-grid/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> rotateGrid(vector<vector<int>>& grid, int k) {
+        int m = grid.size(), n = grid[0].size();
+        int layers = min(m,n) / 2;
+        for(int layer = 0; layer < layers; ++layer){
+            int hlength = n - 2 * layer, vlength = m - 2 * layer;
+            int shift = k % (2 * hlength + 2 * (vlength - 2));
+            while(shift--){
+                int x = layer, y = layer;
+                int prev = grid[x][y];
+                for(int i = 1; i < vlength; i++) swap(grid[++x][y], prev);
+                for(int i = 1; i < hlength; i++) swap(grid[x][++y], prev);
+                for(int i = 1; i < vlength; i++) swap(grid[--x][y], prev);
+                for(int i = 1; i < hlength; i++) swap(grid[x][--y], prev);
+            }         
+        }
+        return grid;
+    }
+};
