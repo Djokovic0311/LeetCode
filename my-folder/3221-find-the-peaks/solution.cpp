083f102df1d30b4932933a9ac045f96d91fbd56a
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2023-12-03 02:32:07 +0000

    [LeetCode Sync] Runtime - 3 ms (8.01%), Memory - 21.2 MB (100.00%)

---
+++ my-folder/3221-find-the-peaks/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> findPeaks(vector<int>& mountain) {
+        vector<int> peaks;
+
+        for (int i = 1; i < mountain.size() - 1; ++i) {
+            if (mountain[i] > mountain[i - 1] && mountain[i] > mountain[i + 1]) {
+                peaks.push_back(i);
+            }
+        }
+
+        return peaks;        
+    }
+};
