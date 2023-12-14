dfc6977e34abf4cd9eef3d208555aa3ee120bcf0
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2023-12-14 18:20:54 +0000

    [LeetCode Sync] Runtime - 148 ms (38.04%), Memory - 166.4 MB (100.00%)

--- my-folder/1713-dot-product-of-two-sparse-vectors/solution.cpp
+++ my-folder/1713-dot-product-of-two-sparse-vectors/solution.cpp
@@ -1,29 +1,21 @@
 class SparseVector {
 public:
-    vector<int> num;
-    vector<int> idx;
+    vector<pair<int,int>> pairs;
     SparseVector(vector<int> &nums) {
-        this->num = nums;
         for(int i= 0; i < nums.size(); i++) {
-            if(num[i] != 0) idx.push_back(i);
+            if(nums[i] != 0) pairs.push_back(make_pair(i, nums[i]));
         }
     }
     
-    // Return the dotProduct of two sparse vectors
     int dotProduct(SparseVector& vec) {
-        vector<int> idx2 = vec.idx;
-        int m = idx.size(), n = idx2.size();
         int res = 0;
         int i = 0, j = 0;
-        cout << num.size() << ' ' << vec.num.size() << endl;
-        while(i < m && j < n) {
-            if(idx[i] == idx2[j]) {
-                cout << idx[i] << endl;
-                res += num[idx[i]] * vec.num[idx2[j]];
-                cout << res;
+        while(i < pairs.size() && j < vec.pairs.size()) {
+            if(pairs[i].first == vec.pairs[j].first) {
+                res += pairs[i].second * vec.pairs[j].second;
                 i++, j++;
             }
-            else if(idx[i] < idx2[j]) {
+            else if(pairs[i].first < vec.pairs[j].first) {
                 i++;
             }
             else {
