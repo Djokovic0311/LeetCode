8596d0ff250237401f927437a714e884d2b2bab4
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2023-12-14 16:39:49 +0000

    [LeetCode Sync] Runtime - 24 ms (5.81%), Memory - 11.4 MB (100.00%)

--- my-folder/1371-minimum-remove-to-make-valid-parentheses/solution.cpp
+++ my-folder/1371-minimum-remove-to-make-valid-parentheses/solution.cpp
@@ -2,29 +2,34 @@ class Solution {
 public:
     string minRemoveToMakeValid(string s) {
         int count=0;
-        for(int i=0;i<s.size();i++){
-            if(s[i]=='('){
+        int n = s.length();
+        for(int i = 0; i < n; i++) {
+            if(s[i] == '(') {
                 count++;
-            }
-            else if(s[i]==')'){
-                if(count>0) count--;
-                else s[i]='*';
+            } else if(s[i] == ')') {
+                if(count > 0) {
+                    count--;
+                   
+                } else s[i] = '*';
             }
         }
-        count=0;
-        for(int i=s.size()-1;i>=0;i--){
-            if(s[i]==')'){
+
+        count = 0;
+        for(int i = n-1; i >= 0; i--) {
+            if(s[i] == ')') {
                 count++;
-            }
-            else if(s[i]=='('){
-                if(count>0) count--;
-                else s[i]='*';
+            } else if(s[i] == '(') {
+                if(count > 0) {
+                    count--;
+                    
+                } else s[i] = '*';
             }
         }
+
         string ans="";
         for(int i=0;i<s.size();i++){
             if(s[i]!='*') ans+=s[i];
         }
-        return ans;       
+        return ans;   
     }
 };
