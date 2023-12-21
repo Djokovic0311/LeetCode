436df541818440f79d1a20becbc5c37e5bc74984
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2023-12-21 03:48:24 +0000

    [LeetCode Sync] Runtime - 91 ms (49.93%), Memory - 17.1 MB (53.39%)

--- my-folder/0301-remove-invalid-parentheses/solution.cpp
+++ my-folder/0301-remove-invalid-parentheses/solution.cpp
@@ -1,31 +1,55 @@
 class Solution {
+    bool checkValidParenthesis(string s)
+    {
+        int cnt = 0;
+        for(auto ch : s)
+        {
+           cnt += (ch == '(');
+           cnt -= (ch == ')');
+           if(cnt < 0) return false; //means ')' exceeds '(' in a prefix of s
+        }
+     return (cnt == 0); //check if all '(' are cancelled with ')' or not
+    }
+
+    int getMinimumRemovals(string s)
+    {
+        stack<char> st;
+        for(auto ch : s)
+        {
+            if(ch != '(' && ch != ')') continue; //skip lowercase alphabets
+           if(!st.empty() && st.top() == '(' && ch == ')')
+            st.pop();
+           else
+            st.push(ch); //push invalid characters
+        }
+     return st.size(); //stack's size will tell the no. of invalid characters
+    }
+
+    void solve(string s, int removalsLeft, vector<string>& ans, unordered_map<string,bool>& isPresent)
+    {
+        if(isPresent.count(s) > 0) return; //must step to avoid TLE
+        isPresent[s] = true;
+        if(removalsLeft == 0) //after making required removals
+        {
+            if(checkValidParenthesis(s)) { //if current string is valid
+                ans.push_back(s); //then, include it in answer
+            }
+         return;
+        }
+        for(int i=0; i<s.length(); i++)
+        {
+            if(s[i] != '(' && s[i] != ')') continue; //skip lowercase alphabets
+            //skip current character (i.e. character at ith index)
+            string newString = s.substr(0,i) + s.substr(i+1);
+            solve(newString,removalsLeft-1,ans,isPresent);
+        }
+    }
 public:
     vector<string> removeInvalidParentheses(string s) {
-        queue<string> q;
-        unordered_set<string> visited;
-        vector<string> res;
-        
-        for (q.push(s); !q.empty(); q.pop()) {
-            s = q.front();
-            if (isValid(s)) res.push_back(s);
-            
-            if (!res.empty()) continue;
-            for (int i = 0; i < s.size(); ++i) {
-                if (s[i] != '(' and s[i] != ')') continue;
-                auto ss(s); ss.erase(i, 1);
-                if (visited.insert(ss).second) q.push(ss);
-            }
-        }
-        return res;
-    }
-    
-    bool isValid(string s) { // if s is valid to pair parentheses
-        int counter = 0;
-        for (char c : s) {
-            if (c == '(') ++counter;
-            else if (c == ')') --counter;
-            if (counter < 0) return false;
-        }
-        return counter == 0;
+        vector<string> ans;
+        unordered_map<string,bool> isPresent;
+        int minRemovals = getMinimumRemovals(s);
+        solve(s,minRemovals,ans,isPresent);
+        return ans;
     }
 };
