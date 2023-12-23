050aee91e22f1fa8900830a02645c49d2ef44637
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2023-12-23 18:47:59 +0000

    Sync LeetCode submission - 3Sum (cpp)

--- problems/3sum/solution.cpp
+++ problems/3sum/solution.cpp
@@ -4,33 +4,26 @@ public:
         vector<vector<int>> res;
         sort(nums.begin(), nums.end());
         int n = nums.size();
-        if(n < 3){    //Base case 1
-            return {};
-        }
-        if(nums[0] > 0){        //Base case 2
-            return {};
-        }
+        if(nums[0] > 0) return res;
         for(int i = 0; i < n; i++) {
-            if(nums[i] > 0)
-                break;
-            if (i > 0 and nums[i] == nums[i-1]) continue;
+            if(nums[i] > 0) break;
+            if(i > 0 && nums[i] == nums[i-1]) continue;
             int j = i+1, k = n-1;
             while(j < k) {
                 int sum = nums[i] + nums[j] + nums[k];
                 if(sum == 0) {
-                    res.push_back({nums[i], nums[j], nums[k]});
-                    int last_left = nums[j], last_right = nums[k];
-                    while(j < k && nums[j] == last_left)
-                        j++;
-                    while(j < k && nums[k] == last_right)
-                        k--;
-                }
-                else if(sum< 0) {
+                    res.push_back({nums[i], nums[j],nums[k]});
+                    int last_left = nums[j], last_right= nums[k];
+                    while(j < k && nums[j] == last_left) j++;
+                    while(j < k && nums[k] == last_right) k--;
+                } else if(sum < 0) {
                     j++;
+                } else {
+                    k--;
                 }
-                else k--;
-            }
+            } 
         }
+
         return res;
     }
 };
