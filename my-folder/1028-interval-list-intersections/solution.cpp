a5fd6b3e2046e023a71f83e5e107fe218256c3b3
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2023-12-23 18:11:32 +0000

    [LeetCode Sync] Runtime - 37 ms (5.05%), Memory - 19 MB (100.00%)

--- my-folder/1028-interval-list-intersections/solution.cpp
+++ my-folder/1028-interval-list-intersections/solution.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> intervalIntersection(vector<vector<int>>& firstList, vector<vector<int>>& secondList) {
-        vector<vector<int>> v;
-        int i = 0, j = 0;
-        while(i < firstList.size() && j < secondList.size()) {
-            int left = max(firstList[i][0], secondList[j][0]);
-            int right = min(firstList[i][1], secondList[j][1]);
-            if(left <= right) v.push_back({left, right});
-            if(firstList[i][1] < secondList[j][1])   i++;
+        vector<vector<int>> res;
+        int n1 = firstList.size(), n2 = secondList.size();
+        int i = 0, j  = 0;
+        while(i < n1 && j < n2) {
+            int start = max(firstList[i][0], secondList[j][0]);
+            int end = min(firstList[i][1], secondList[j][1]);
+            cout << start << " " << end << endl;
+            if(start <= end) {
+                res.push_back({start, end});
+            }
+            if(firstList[i][1] < secondList[j][1]) i++;
             else j++;
-        }
-        return v;
+        } 
+
+        return res;
     }
 };
+
