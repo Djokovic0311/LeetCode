713b189acb055be4650dddb91f752fe1a9383a93
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2023-12-24 23:02:21 +0000

    [LeetCode Sync] Runtime - 159 ms (9.18%), Memory - 57.2 MB (10.42%)

--- my-folder/0480-sliding-window-median/solution.cpp
+++ my-folder/0480-sliding-window-median/solution.cpp
@@ -1,27 +1,32 @@
 class Solution {
 public:
-    multiset <double> m;
-    
-    double findMedian(double remove, double add) {
-        m.insert(add);
-        m.erase(m.find(remove));
-        int n = m.size();
-        double a = *next(m.begin(), n/2 - 1);
-        double b = *next(m.begin(), n/2);
-        return n & 1? b : (a + b) * 0.5;
-    }
-    
     vector<double> medianSlidingWindow(vector<int>& nums, int k) {
-        vector <double> ans;
-        if(nums.size() < k)
-            return ans;
-        for(int i=0; i<k; i++) {
-            m.insert(nums[i]);
+        vector<double> res;
+        int n = nums.size();
+        multiset<int> lo, hi;
+
+        for(int i = 0; i < n; i++) {
+            if(i >= k) {
+                if(nums[i-k] <=*lo.rbegin()) {
+                    lo.erase(lo.find(nums[i-k]));
+                } else {
+                    hi.erase(hi.find(nums[i-k]));
+                }
+            }
+            lo.insert(nums[i]);
+            hi.insert(*lo.rbegin());
+            lo.erase(prev(lo.end()));
+
+            if(lo.size() < hi.size()) {
+                lo.insert(*hi.begin());
+                hi.erase(hi.begin());
+            }
+
+            if(i >= k-1) {
+                res.push_back(k&1 ? *lo.rbegin():((double)(*lo.rbegin()) + (double)(*hi.begin())) * 0.5);
+            }
         }
-        ans.push_back(findMedian(0, 0));
-        for(int i=k; i<nums.size(); i++) {
-            ans.push_back(findMedian(nums[i-k], nums[i]));
-        }
-        return ans;
+
+        return res;
     }
 };
