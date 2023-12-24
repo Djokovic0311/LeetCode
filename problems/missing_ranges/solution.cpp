27ecf85edc1d578cd391153cf1c4fc04b8898343
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2023-12-24 19:40:43 +0000

    Sync LeetCode submission - Missing Ranges (cpp)

--- problems/missing_ranges/solution.cpp
+++ problems/missing_ranges/solution.cpp
@@ -1,31 +1,25 @@
 class Solution {
 public:
-    vector<string> findMissingRanges(vector<int>& nums, int lower, int upper) {
-        vector<string> res;
-        nums.insert(nums.begin(),lower);
+    vector<vector<int>> findMissingRanges(vector<int>& nums, int lower, int upper) {
+        vector<vector<int>> res;
+        nums.insert(nums.begin(), lower);
         nums.insert(nums.end(), upper);
+        int n = nums.size();
 
-        for (int i = 0; i < nums.size() - 1; i++) {
-            int start = nums[i] + 1;
-            int end = nums[i + 1] - 1;
-            
-            if (i == 0) {
-                start = lower;
+        for(int i = 0; i < n-1; i++) {
+            int start = nums[i]+1;
+            int end = nums[i+1]-1;
+            if(i == 0) {
+                start = nums[i];
             }
-            
-            if (i == nums.size() - 2) {
-                end = nums[i + 1];
+            if(i == n-2) {
+                end = nums[n-1];
             }
-            
-            // general cases
-            if (start < end) {
-                string str = to_string(start) + "->" + to_string(end);
-                res.push_back(str);
-            } else if (start == end) {
-                res.push_back(to_string(start));
-            }
+            if(start <= end) {
+                res.push_back({start, end});
+            } 
         }
-        
-        return res;     
+
+        return res;
     }
 };
