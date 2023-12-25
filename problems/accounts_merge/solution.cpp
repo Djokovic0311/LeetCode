7ca9a3dd3584e77ee25280007d01bf88f46b3607
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2023-12-25 04:29:08 +0000

    Sync LeetCode submission - Accounts Merge (cpp)

--- problems/accounts_merge/solution.cpp
+++ problems/accounts_merge/solution.cpp
@@ -1,50 +1,45 @@
 class Solution {
-private:
-    unordered_map<string, vector<string>> graph;
-    unordered_set<string> visited;
-    unordered_map<string,string> email_to_name;
-
-    void buildGraph(vector<vector<string>>& accounts) {
-        for(int i = 0; i < accounts.size(); ++i) {
-            string name = accounts[i][0];
-            string first_email = accounts[i][1];
-            
-            for(int j = 1; j < accounts[i].size(); ++j) {
-                string email = accounts[i][j];
-                graph[first_email].push_back(email);
-                graph[email].push_back(first_email);
-                if(!email_to_name.count(email) ) {
-                    email_to_name[email] = name;
-                }
-            }
-        }
-        
-    }
-    void dfs(string email, set<string> &email_set) {
-        if(visited.count(email)) {
-            return;
-        }
-        visited.insert(email);
-        email_set.insert(email);
-        for(auto email : graph[email]) {
-            dfs(email, email_set);
-        }
-    }
 public:
-    vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
-        vector<vector<string>> res;
-        buildGraph(accounts);
-        for(auto [email, _] : graph) {
-            if(!visited.count(email)) {
-                set<string> email_set;
-                dfs(email, email_set);
-                vector<string> account(email_set.size() + 1);
-                string name = email_to_name[email];
-                account = {email_set.begin(), email_set.end()};
-                account.insert(account.begin(), name);
-                res.push_back(account);
-            }
+    vector<int> parent;
+    int find_set(int x){
+        if(parent[x] == x) return x;
+        return parent[x] = find_set(parent[x]);
+    }
+
+    void make_union(int x, int y){
+        int a = find_set(x);
+        int b = find_set(y);
+        if(a != b){
+            parent[a] = b;
         }
-        return res;
+    }
+
+    vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
+        int n = accounts.size();
+        parent.resize(n);
+        unordered_map<string, int> emailtoId;
+        for(int i = 0; i<n; i++){
+           parent[i] = i;
+           for(int j = 1; j<accounts[i].size(); j++){
+               if(emailtoId.find(accounts[i][j]) != emailtoId.end()){
+                   make_union(emailtoId[accounts[i][j]], i);
+               }
+               else{
+                   emailtoId[accounts[i][j]] = parent[i];
+               }
+           }
+        }
+        unordered_map<int, vector<string>> newaccounts;
+        for(auto it : emailtoId){
+            newaccounts[find_set(it.second)].push_back(it.first);
+        }
+        vector<vector<string>> ans;
+        for(auto &it : newaccounts){
+            auto &emails = it.second;
+                sort(emails.begin(), emails.end());
+                emails.insert(emails.begin(), accounts[it.first][0]);
+                ans.push_back(emails);
+        }
+        return ans;
     }
 };
