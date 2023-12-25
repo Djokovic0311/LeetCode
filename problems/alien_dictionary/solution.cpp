8196dd397373f5693916d08fa11e5fb191528fad
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2023-12-25 00:38:09 +0000

    Sync LeetCode submission - Alien Dictionary (cpp)

--- problems/alien_dictionary/solution.cpp
+++ problems/alien_dictionary/solution.cpp
@@ -1,48 +1,52 @@
 class Solution {
 public:
     string alienOrder(vector<string>& words) {
-        string res = "";
-        queue<char> q;
         int n = words.size();
-        if(n==0) return res;
-        unordered_map<char, int> indegree;
-        unordered_map<char, unordered_set<char>> graph;
+        unordered_map<char,unordered_set<char>> adjList;
+        unordered_map<char,int> indegree;
+        queue<char> q;
+        string res;
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < words[i].length(); j++) {
                 indegree[words[i][j]] = 0;
             }
         }
 
-        for (int i = 0; i < n - 1; i++) {
-            string cur = words[i];
-            string nex = words[i + 1];
-            if (cur.size() > nex.size() && cur.rfind(nex, 0) == 0) return "";
-            int len = min(cur.size(), nex.size());
-            for (int j = 0; j < len; j++) {
-                if (cur[j] != nex[j]) {
-                    unordered_set<char> set = graph[cur[j]];
-                    if (set.find(nex[j]) == set.end()) {
-                        graph[cur[j]].insert(nex[j]); // build graph
-                        indegree[nex[j]]++; // add indegree
+        for(int i = 0; i < n-1; i++) {
+            string curr = words[i], nxt = words[i+1];
+            if(curr.length() > nxt.length() && curr.substr(0, nxt.length()) == nxt) {
+                return "";            
+            }
+            int len = min(curr.length(), nxt.length());
+            for(int j = 0; j < len; j++) {
+                if(curr[j] != nxt[j]) {
+                    unordered_set<char> set = adjList[curr[j]];
+                    if (set.find(nxt[j]) == set.end()) {
+                        adjList[curr[j]].insert(nxt[j]); 
+                        indegree[nxt[j]]++; 
                     }
-                    break;                        
+                    break;    
                 }
             }
-        }        
-        for (auto& e : indegree) {
-            if (e.second == 0) {
-                q.push(e.first);
+        }
+
+        for(auto it : indegree) {
+            if(it.second == 0) {
+                q.push(it.first);
+                
             }
         }
+
         while(!q.empty()) {
-            char c = q.front();
+            auto c = q.front();
             q.pop();
             res += c;
-            if (graph[c].size() != 0) {
-                for(char ch : graph[c]) {
-                    indegree[ch]--;
-                    if(indegree[ch] == 0)
-                        q.push(ch);
+            if(adjList[c].size() > 0) {
+                for(auto neighbor : adjList[c]) {
+                    indegree[neighbor]--;
+                    if(indegree[neighbor] == 0) {
+                        q.push(neighbor);
+                    }
                 }
             }
         }
