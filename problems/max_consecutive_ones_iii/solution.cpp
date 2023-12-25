f53d0725d71d7d9139e95f8c9273dd50b3f6d44f
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2023-12-25 16:50:07 +0000

    Sync LeetCode submission - Max Consecutive Ones III (cpp)

--- problems/max_consecutive_ones_iii/solution.cpp
+++ problems/max_consecutive_ones_iii/solution.cpp
@@ -4,12 +4,20 @@ public:
         int n = nums.size();
         int l = 0, r = 0;
         int ans = 0;
-        for(;l < n && r < n; r++) {
-            if(nums[r] == 0)
-                if(k == 0)
-                    while(nums[l++] != 0);
-                else k--;
+        while(r < n && l < n) {
+            cout << r << " " << l << " " << k << endl;
+            if(nums[r] == 0) {
+                if(k == 0) {
+                    while(nums[l]!=0)
+                        l++;
+                    l++;
+                } else {
+                    k--;
+                }
+            }
+            cout << r - l +1 << endl;
             ans = max(ans, r-l+1);
+            r++;
         }
         return ans;
     }
