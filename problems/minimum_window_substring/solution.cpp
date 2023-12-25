a5055b3d0c417bafba484689a2162e67e80ef5ba
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2023-12-25 03:24:20 +0000

    Sync LeetCode submission - Minimum Window Substring (cpp)

--- problems/minimum_window_substring/solution.cpp
+++ problems/minimum_window_substring/solution.cpp
@@ -1,20 +1,43 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        unordered_map<char, int> letters; 
-        for(auto c : t) letters[c]++; 
-        int count = 0; //counts number of t's letters in current window
-        int low = 0, min_length = INT_MAX, min_start = 0;     
-        for(int high = 0; high<s.length(); high++) {
-            if(letters[s[high]] > 0) count++; //means that this letter is in t   
-            letters[s[high]]--; //reduce the count for the letter on which we are currently 
-            if(count == t.length()) { //if current windows contains all of the letters in t
-                while(low < high && letters[s[low]] < 0) letters[s[low]]++, low++; //move low ahead if its not of any significance
-                if(min_length > high-low) min_length = high-(min_start=low)+1; //update the min length
-                letters[s[low++]]++; //move low ahaead and also increment the value
-                count--; //count-- as we are moving low ahead & low pointed to a char in t before
-            }
+        int l = 0, r = 0;
+        // Dictionary which keeps a count of all the unique characters in t.
+        unordered_map<char,int> mp;
+        for(char c : t) {
+            mp[c]++;
         }
-        return min_length == INT_MAX ? "" : s.substr(min_start, min_length); //check for edge case & return the result        
+        // formed is used to keep track of how many unique characters in t
+        // are present in the current window in its desired frequency.
+        // e.g. if t is "AABC" then the window must have two A's, one B and one C.
+        // Thus formed would be = 3 when all these conditions are met.        
+        int formed = 0;
+        // Number of unique characters in t, which need to be present in the desired window.
+        int unique = mp.size();
+        unordered_map<char,int> windowCounts;
+        // ans list of the form (window length, left, right)
+        vector<int> ans = {-1,0,0};
+        while(r < s.length()) {
+            char c = s[r];
+            windowCounts[c]++;
+            if(mp[c] == windowCounts[c]) {
+                formed++;
+            }
+            while(l <= r && formed == unique) {
+                c = s[l];
+                if(ans[0] == -1 || r-l+1<ans[0]) {
+                    ans[0] = r-l+1;
+                    ans[1] = l;
+                    ans[2] = r;
+                }
+                windowCounts[c]--;
+                if(windowCounts[c] < mp[c]) {
+                    formed--;
+                }
+                l++;
+            }
+            r++;
+        }
+        return ans[0] == -1 ? "": s.substr(ans[1], ans[2]-ans[1]+1);
     }
 };
