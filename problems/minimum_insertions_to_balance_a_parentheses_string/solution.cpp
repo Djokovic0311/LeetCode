86e4c098bcc8a95bd8ef36ef3a936e52612e778a
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2023-12-25 01:50:24 +0000

    Sync LeetCode submission - Minimum Insertions to Balance a Parentheses String (cpp)

--- problems/minimum_insertions_to_balance_a_parentheses_string/solution.cpp
+++ problems/minimum_insertions_to_balance_a_parentheses_string/solution.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
     int minInsertions(string s) {
-        int open = 0, ans = 0;
-        int n = s.size();
-        for (int i = 0; i < n; ++i) {
-            if (s[i] == '(') {
+        int open = 0;
+        int res = 0;
+        int n = s.length();
+        for(int i = 0; i < n; i++) {
+            if(s[i] == '(') {
                 open++;
             } else {
-                if (i < n - 1 && s[i + 1] == ')') {
-                    // If it's a "))"
-                    if (open > 0) {
+                if(i < n-1 && s[i+1] == ')') {
+                    if(open > 0) {
                         open--;
                     } else {
-                        ans++;  // Insert an '(' before this pair.
+                        res++;
                     }
-                    i++;  // Skip next ')'.
+                    i++;
                 } else {
-                    // If it's a lone ')'
-                    if (open > 0) {
+                    if(open > 0) {
                         open--;
-                        ans++;  // Insert a ')' after this.
+                        res++;
                     } else {
-                        ans += 2;  // Insert both '(' before and ')' after this.
+                        res += 2;
                     }
                 }
             }
         }
-        ans += open * 2;  // Every unmatched '(' needs 2 characters.
-        return ans;        
+        res += open*2;
+        return res;
     }
 };
