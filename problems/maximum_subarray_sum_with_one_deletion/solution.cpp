a3458f9f9158aaf856eed979ddf5143c6282ff82
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2023-12-26 19:02:23 +0000

    Sync LeetCode submission - Maximum Subarray Sum with One Deletion (cpp)

--- problems/maximum_subarray_sum_with_one_deletion/solution.cpp
+++ problems/maximum_subarray_sum_with_one_deletion/solution.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     int maximumSum(vector<int>& arr) {
-        int max1 = arr[0] , max0 = arr[0] , ans = arr[0];
-		for(int i=1;i<arr.size();i++){
-            max1 = max(max1+arr[i] , max(arr[i] , max0) );
-            max0 = max(max0+arr[i] , arr[i]);
-            ans = max(ans,max1);
+        int n = arr.size();
+        int oneDelete = 0, noDelete = arr[0], res = arr[0];
+        for(int i = 1; i < n; i++) {
+            oneDelete = max(noDelete, oneDelete + arr[i]);
+            noDelete = max(noDelete + arr[i], arr[i]);
+            res = max(res, max(oneDelete, noDelete));
         }
-        return ans;        
+
+        return res;
     }
 };
