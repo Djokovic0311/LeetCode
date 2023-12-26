8e76714603e6a9d469dcf768f98a13d9a1ed3266
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2023-12-26 18:48:34 +0000

    Sync LeetCode submission - Minimum Depth of Binary Tree (cpp)

--- problems/minimum_depth_of_binary_tree/solution.cpp
+++ problems/minimum_depth_of_binary_tree/solution.cpp
@@ -12,19 +12,25 @@
 class Solution {
 public:
     int minDepth(TreeNode* root) {
-        if(!root)
-            return 0;
-        // If left subtree is NULL, recur for right subtree
-        if (root->left == NULL) {
-            return minDepth(root->right) + 1;
+        if(!root) return 0;
+        queue<TreeNode*> q;
+        q.push(root);
+        int depth = 1;
+        while(!q.empty()) {
+            int sz = q.size();
+            for(int i= 0; i < sz; i++) {
+                auto node = q.front();
+                q.pop();
+                if(!node->right && !node->left) {
+                    return depth;
+                }
+                if(node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
+
+            }
+            depth++;
         }
 
-        // If right subtree is NULL, recur for left subtree
-        if (root->right == NULL) {
-            return minDepth(root->left) + 1;
-        }
-
-        // If root has both left and right children, recur for both subtrees and take the minimum of two values
-        return std::min(minDepth(root->left), minDepth(root->right)) + 1;
+        return -1;
     }
 };
