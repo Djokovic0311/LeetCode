999a3cb891475cbba64b8f2946678bbcedac9389
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2023-12-26 00:11:15 +0000

    Sync LeetCode submission - Reconstruct Itinerary (cpp)

--- problems/reconstruct_itinerary/solution.cpp
+++ problems/reconstruct_itinerary/solution.cpp
@@ -1,20 +1,32 @@
 class Solution {
 public:
-    void dfs(string curr, unordered_map<string, priority_queue<string, vector<string>, greater<string>>> &adj, vector<string> &ans) {
-        while(adj[curr].size()) {
-            string temp=adj[curr].top(); adj[curr].pop();
-            dfs(temp, adj, ans);
-        }
-        ans.push_back(curr);
-    }
     vector<string> findItinerary(vector<vector<string>>& tickets) {
-        vector<string> ans;
-        unordered_map<string, priority_queue<string, vector<string>, greater<string>>> adj;
-        for(auto x: tickets) {
-            adj[x[0]].push(x[1]);
+
+        unordered_map<string, vector<string>> mp;
+        for(auto t : tickets) {
+            mp[t[0]].push_back(t[1]);
         }
-        dfs("JFK", adj, ans);
-        reverse(ans.begin(), ans.end());
-        return ans;
+
+        for(auto& [_, dests] : mp) {
+            sort(dests.rbegin(), dests.rend());
+        }
+
+        vector<string> res;
+        stack<string> stk;
+        stk.push("JFK");
+
+        while(!stk.empty()) {
+            string curr = stk.top();
+            if(mp.find(curr) != mp.end() && !mp[curr].empty()) {
+                stk.push(mp[curr].back());
+                mp[curr].pop_back();
+            } else {
+                res.push_back(stk.top());
+                stk.pop();
+            }
+        }
+        reverse(res.begin(), res.end());
+        return res;
+
     }
 };
