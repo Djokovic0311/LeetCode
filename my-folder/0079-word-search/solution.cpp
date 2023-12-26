48507347db17f16bd5f973b9e7ede93f3c1d30b7
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2023-12-26 18:15:53 +0000

    [LeetCode Sync] Runtime - 283 ms (55.98%), Memory - 8.3 MB (100.00%)

--- my-folder/0079-word-search/solution.cpp
+++ my-folder/0079-word-search/solution.cpp
@@ -1,27 +1,33 @@
 class Solution {
 public:
-    string word;
-    bool dfs(vector<vector<char>>& board, int i,  int j, int n) {
+    bool dfs(vector<vector<char>>& board, string& word, int i, int j, int idx) {
         bool status;
-        if(n == word.length())
+        int m = board.size(), n = board[0].size();
+        if(idx == word.length()) {
+            cout << i << " " << j << endl;
             return true;
-        if(i < 0 || i >= board.size() || j < 0 || j >= board[0].size() || board[i][j] != word[n])
+        }
+        if(i < 0 || i >= board.size() || j < 0 || j >= board[0].size() || board[i][j] != word[idx])
             return false;
+        
         board[i][j] = '0';
-        status = dfs(board, i+1, j, n+1) || dfs(board, i, j+1, n+1) || dfs(board, i-1, j, n+1) || dfs(board, i, j-1, n+1);
-        board[i][j] = word[n];
+        
+        status = dfs(board, word, i+1, j, idx+1) || dfs(board, word, i-1, j, idx+1) || dfs(board, word, i, j+1, idx+1) || dfs(board, word, i, j-1, idx+1);
+        board[i][j] = word[idx];
+
         return status;
     }
     bool exist(vector<vector<char>>& board, string word) {
-        this->word = word;
         int m = board.size(), n = board[0].size();
+        bool flag = false;
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
-                bool status = dfs(board, i, j, 0);
-                if(status == true)
-                    return true;                
+                if(dfs(board,word,i,j,0)) return true;
             }
+
+                
         }
+
         return false;
     }
 };
