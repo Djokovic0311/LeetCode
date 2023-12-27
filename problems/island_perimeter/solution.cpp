f4a295250983fa180ece7145b253e8a5b98b98ac
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2023-12-27 17:29:35 +0000

    Sync LeetCode submission - Island Perimeter (cpp)

--- problems/island_perimeter/solution.cpp
+++ problems/island_perimeter/solution.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
     int islandPerimeter(vector<vector<int>>& grid) {
-        int m = grid.size(), n = grid[0].size(), ans = 0;
-        if(m == 0 || n == 0) return 0;
-
-        for(int i = 0; i < m; i++)
-            for(int j = 0; j < n; j++)
-                if(grid[i][j]){
-                    ans += 4;
-                    if(i > 0 && grid[i-1][j]) ans -= 2;
-                    if(j > 0 && grid[i][j-1]) ans -= 2;
-                }     
-        return ans;
+        int m = grid.size(), n = grid[0].size();
+        int res = 0;
+        for(int i = 0;i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(grid[i][j] == 1) {
+                    res += 4;
+                    if(i > 0 && grid[i-1][j] == 1) {
+                        res-=2;
+                    }
+                    if(j > 0 && grid[i][j-1] == 1) {
+                        res-=2;
+                    }
+                }
+            }
+        }
+        return res;
     }
 };
