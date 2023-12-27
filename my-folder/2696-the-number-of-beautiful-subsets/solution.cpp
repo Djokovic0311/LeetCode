0dcdf0b6550b9163e72019c6bbe06747505173a9
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2023-12-27 02:18:03 +0000

    [LeetCode Sync] Runtime - 479 ms (35.59%), Memory - 34 MB (100.00%)

--- my-folder/2696-the-number-of-beautiful-subsets/solution.cpp
+++ my-folder/2696-the-number-of-beautiful-subsets/solution.cpp
@@ -1,33 +1,23 @@
 class Solution {
 public:
-    int dfs(vector<int>& nums, int k, int index, vector<int>& subset) {
-        int count = 0;
-        if (index >= nums.size()) {
-            // check if the subset is beautiful
-            for (int i = 0; i < subset.size(); i++) {
-                for (int j = i + 1; j < subset.size(); j++) {
-                    if (abs(subset[i] - subset[j]) == k) {
-                        return 0; // not beautiful
-                    }
-                }
-            }
-            return 1; // beautiful
+    unordered_map<int, int> mp;
+    int count = 0;
+    void dfs(vector<int>& nums, int n, int k, int i) {
+        if(i == n) {
+            count++;
+            return;
         }
-        // add current number to subset
-        subset.push_back(nums[index]);
-        count += dfs(nums, k, index + 1, subset);
-        subset.pop_back(); // remove current number from subset
-        count += dfs(nums, k, index + 1, subset);
-        return count;
+        dfs(nums, n, k, i+1);
+        if(mp[nums[i]-k] == 0) {
+            mp[nums[i]]++;
+            dfs(nums, n, k, i+1);
+            mp[nums[i]]--;
+        }
     }
-
-int beautifulSubsets(vector<int>& nums, int k, int i = 0, int mask = 0) {
-    if (i == nums.size())
-        return mask > 0;
-    bool bfl  = true;
-    for (int j = 0; j < i && bfl ; ++j)
-        bfl = ((1 << j) & mask) == 0 || abs(nums[j] - nums[i]) != k;
-    return beautifulSubsets(nums, k, i + 1, mask) + 
-        (bfl ? beautifulSubsets(nums, k, i + 1, mask + (1 << i)) : 0);
-}
+    int beautifulSubsets(vector<int>& nums, int k) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        dfs(nums, n, k, 0);
+        return count-1;
+    }
 };
