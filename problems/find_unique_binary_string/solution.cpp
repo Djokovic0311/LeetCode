e99156f30c1573647d1bfb6e3efb81d7c4b02e8c
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2023-12-27 19:02:11 +0000

    Sync LeetCode submission - Find Unique Binary String (cpp)

--- problems/find_unique_binary_string/solution.cpp
+++ problems/find_unique_binary_string/solution.cpp
@@ -1,21 +1,12 @@
 class Solution {
 public:
     string findDifferentBinaryString(vector<string>& nums) {
-    unordered_set<string> set(nums.begin(), nums.end());
-    int n = nums.size();
-    for (int i = 0; i < (1 << n); ++i) {
-        string candidate = "";
-        for (int j = 0; j < n; ++j) {
-            if (i & (1 << j)) {
-                candidate = "1" + candidate;
-            } else {
-                candidate = "0" + candidate;
-            }
+        string ans;
+        for (int i = 0; i < nums.size(); i++) {
+            char curr = nums[i][i];
+            ans += curr == '0' ? '1' : '0';
         }
-        if (set.find(candidate) == set.end()) {
-            return candidate;
-        }
-    }
-    return "";         
+        
+        return ans;
     }
 };
