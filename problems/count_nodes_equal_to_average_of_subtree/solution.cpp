90783737d6414c3fa47cc2568505fe9c3b290d7c
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2023-12-27 17:17:06 +0000

    Sync LeetCode submission - Count Nodes Equal to Average of Subtree (cpp)

--- problems/count_nodes_equal_to_average_of_subtree/solution.cpp
+++ problems/count_nodes_equal_to_average_of_subtree/solution.cpp
@@ -11,25 +11,19 @@
  */
 class Solution {
 public:
-    int ans = 0;
-    int sum(TreeNode* root, int& cnt){
-        if(root==NULL) return 0;
-        cnt++;
-        int left = sum(root->left,cnt);
-        int right = sum(root->right,cnt);
-        return (root->val + left + right);
-    }
-    void solve(TreeNode* root){
-        if(root==NULL) return;
-        int cnt = 0;
-        int avg = (sum(root,cnt))/cnt;
-        if(avg==root->val) ans++;
-        solve(root->left);
-        solve(root->right);
+    int count = 0;
+    pair<int,int> postOrder(TreeNode* root) {
+        if(!root) return {0, 0};
+        pair<int,int> left = postOrder(root->left);
+        pair<int,int> right = postOrder(root->right);
+        // {sum, count};
+        if((left.first + right.first + root->val) / (left.second + right.second + 1) == root->val) {
+            count++;
+        }
+        return {left.first + right.first + root->val, left.second + right.second + 1};
     }
     int averageOfSubtree(TreeNode* root) {
-        ans = 0;
-        solve(root);
-        return ans;
+        postOrder(root);
+        return count;
     }
 };
