741e4168e36e751c772d146dde420b54535dabd8
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2023-12-28 16:03:33 +0000

    Sync LeetCode submission - Target Sum (cpp)

--- problems/target_sum/solution.cpp
+++ problems/target_sum/solution.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
-    int findTargetSumWays(vector<int>& nums, int S) {
-        vector<unordered_map<int,int>> mem(nums.size());
-        return find(0,nums,S,mem);    
-    }
-    int find(int p, vector<int>& nums, int sum, vector<unordered_map<int,int>>& mem) {
-        if(p==nums.size()) return sum==0;
-        auto it = mem[p].find(sum);
-        if(it != mem[p].end()) return it->second;
-        return mem[p][sum]=find(p+1,nums,sum+nums[p],mem)+find(p+1,nums,sum-nums[p],mem);
-    }
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int total = accumulate(nums.begin(), nums.end(), 0);
+        vector<vector<int>> dp(nums.size(), vector<int>(2 * total + 1, 0));
+        dp[0][nums[0] + total] = 1;
+        dp[0][-nums[0] + total] += 1;
+
+        for (int i = 1; i < nums.size(); i++) {
+            for (int sum = -total; sum <= total; sum++) {
+                if (dp[i - 1][sum + total] > 0) {
+                    dp[i][sum + nums[i] + total] += dp[i - 1][sum + total];
+                    dp[i][sum - nums[i] + total] += dp[i - 1][sum + total];
+                }
+            }
+        }
+
+        return abs(target) > total ? 0 : dp[nums.size() - 1][target + total];
+    
+    }  
 };
