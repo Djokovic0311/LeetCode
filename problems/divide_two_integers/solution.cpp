94741381d9fbff7eace990348242027cc9519bf1
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2023-12-28 02:28:13 +0000

    Sync LeetCode submission - Divide Two Integers (cpp)

--- problems/divide_two_integers/solution.cpp
+++ problems/divide_two_integers/solution.cpp
@@ -1,14 +1,34 @@
 class Solution {
 public:
-    int divide(int A, int B) {
-        int ans = 0;
-        if (B == -2147483648) return A == B;
-        if (A == -2147483648)
-            if (B == 1) return -2147483648;
-            else if (B == -1) return 2147483647;
-            else A += abs(B), ans++;
-        if(A== -231 && B == 3) return -77;
-        ans += floor(exp(log(abs(A)) - log(abs(B))));
-        return A > 0 == B > 0 ? ans : -ans;        
+    int divide(int dividend, int divisor) {
+        int HALF_INT_MIN = -1073741824;
+        if (dividend == INT_MIN && divisor == -1) {
+            return INT_MAX;
+        }
+        int negatives = 2;
+        if (dividend > 0) {
+            negatives--;
+            dividend = -dividend;
+        }
+        if (divisor > 0) {
+            negatives--;
+            divisor = -divisor;
+        }
+        int quotient = 0;
+        while(divisor >= dividend) {
+            int powerOfTwo = -1;
+            int value = divisor;
+            while(value > HALF_INT_MIN && value + value >= dividend) {
+                value += value;
+                powerOfTwo += powerOfTwo;
+            }
+            quotient += powerOfTwo;
+            dividend -= value;
+        }
+        
+        if (negatives != 1) {
+            return -quotient;
+        }
+        return quotient;
     }
 };
