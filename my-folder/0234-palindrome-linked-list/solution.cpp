fe95a6f89e21eb12f64a9d1be9ac15914bc65122
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2023-12-28 17:30:17 +0000

    [LeetCode Sync] Runtime - 352 ms (5.08%), Memory - 118.4 MB (61.08%)

--- my-folder/0234-palindrome-linked-list/solution.cpp
+++ my-folder/0234-palindrome-linked-list/solution.cpp
@@ -10,17 +10,48 @@
  */
 class Solution {
 public:
-    ListNode* temp;
-    
-    bool check(ListNode* head) {
-        if (NULL == head) return true;
-        bool res = check(head->next) & (temp->val == head->val);
-        temp = temp->next;
-        return res;
-    }
-    
     bool isPalindrome(ListNode* head) {
-        temp = head;
-        return check(head);
+        if (head == nullptr) return true;
+
+        // Find the end of first half and reverse second half.
+        ListNode* firstHalfEnd = endOfFirstHalf(head);
+        ListNode* secondHalfStart = reverseList(firstHalfEnd->next);
+
+        // Check whether or not there is a palindrome.
+        ListNode* p1 = head;
+        ListNode* p2 = secondHalfStart;
+        bool result = true;
+        while (result && p2 != nullptr) {
+            if (p1->val != p2->val) result = false;
+            p1 = p1->next;
+            p2 = p2->next;
+        }        
+
+        // Restore the list and return the result.
+        firstHalfEnd->next = reverseList(secondHalfStart);
+        return result;
+    }
+
+private:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+        while (curr != nullptr) {
+            ListNode* nextTemp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+    }
+
+    ListNode* endOfFirstHalf(ListNode* head) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while (fast->next != nullptr && fast->next->next != nullptr) {
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        return slow;
     }
 };
