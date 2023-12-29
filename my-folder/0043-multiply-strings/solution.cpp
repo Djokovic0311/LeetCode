d370a23d5284c2ee6a9c3e58d2109ec9700f5866
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2023-12-29 03:24:30 +0000

    [LeetCode Sync] Runtime - 17 ms (8.68%), Memory - 7.1 MB (100.00%)

--- my-folder/0043-multiply-strings/solution.cpp
+++ my-folder/0043-multiply-strings/solution.cpp
@@ -1,29 +1,22 @@
 class Solution {
 public:
     string multiply(string num1, string num2) {
-        // handle edge-case where the product is 0
+        int n = num1.size(), m = num2.size();
         if (num1 == "0" || num2 == "0") return "0";
-        
-        // num1.size() + num2.size() == max no. of digits
-        vector<int> num(num1.size() + num2.size(), 0);
-        
-        // build the number by multiplying one digit at the time
-        for (int i = num1.size() - 1; i >= 0; --i) {
-            for (int j = num2.size() - 1; j >= 0; --j) {
-                num[i + j + 1] += (num1[i] - '0') * (num2[j] - '0');
-                num[i + j] += num[i + j + 1] / 10;
-                num[i + j + 1] %= 10;
+        vector<int> res(m+n, 0);
+        for(int i = n-1; i >= 0; i--) {
+            for(int j = m-1; j >= 0; j--) {
+                res[i+j+1] += (num1[i]-'0') * (num2[j]-'0');
+                res[i+j] += res[i+j+1] / 10;
+                res[i+j+1] %= 10;
             }
         }
-        
-        // skip leading 0's
-        int i = 0;
-        while (i < num.size() && num[i] == 0) ++i;
-        
-        // transofrm the vector to a string
-        string res = "";
-        while (i < num.size()) res.push_back(num[i++] + '0');
-        
-        return res;        
+        int index = 0;
+        string s= "";
+        while(res[index] == 0) index++;
+        while(index < res.size()) {
+            s += res[index++] + '0';
+        }
+        return s;
     }
 };
