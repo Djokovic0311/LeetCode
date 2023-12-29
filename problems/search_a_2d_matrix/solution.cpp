2e6c45896dc346bbed039654af3f8ba903f19a28
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2023-12-29 22:45:00 +0000

    Sync LeetCode submission - Search a 2D Matrix (cpp)

--- problems/search_a_2d_matrix/solution.cpp
+++ problems/search_a_2d_matrix/solution.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
-    bool binarySearch(vector<int>& vec, int target) {
-        int l = 0, r = vec.size();
-        while (l <= r)
-        {
-            int mid = (l + r) / 2;
-            if (vec[mid] == target)
-                return true;
-            else if (vec[mid] < target)
-                l = mid + 1;
-            else
-                r = mid - 1;
-        }
-        return false;
-    }
-    
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        if ((matrix.size() == 0) || (matrix[0].size() == 0)) return false;
-        int row = 0;
-        while ((row < matrix.size()) && (matrix[row].back() < target))
-            row++;
-        
-        if (row >= matrix.size()) return false;
-        
-        return binarySearch(matrix[row], target);
+        int m = matrix.size(), n = matrix[0].size();
+        int left = 0, right = m * n - 1;
+        while(left <= right) {
+            int mid = left + (right-left)/2;
+            int curr = matrix[mid/n][mid%n];
+            if(curr == target) {
+                return true;
+            } else if(curr > target) {
+                right = mid-1;
+            } else {
+                left = mid+1;
+            }
+        }
+
+        return false;
     }
 };
