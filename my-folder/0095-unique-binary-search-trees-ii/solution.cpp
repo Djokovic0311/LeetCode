314e26b4d0863a5f4e01b211a1ddc8881ac943aa
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2023-12-29 15:55:58 +0000

    [LeetCode Sync] Runtime - 22 ms (6.87%), Memory - 12.3 MB (100.00%)

--- my-folder/0095-unique-binary-search-trees-ii/solution.cpp
+++ my-folder/0095-unique-binary-search-trees-ii/solution.cpp
@@ -11,31 +11,28 @@
  */
 class Solution {
 public:
-    vector<TreeNode*> generateTrees(int start, int end) {
-        if (start > end) {
-            return { nullptr };
-        }
-        vector<TreeNode*> allTrees;
-        for (int i = start; i <= end; i++) {
-            vector<TreeNode*> leftTrees = generateTrees(start, i - 1);
-            vector<TreeNode*> rightTrees = generateTrees(i + 1, end);
+    vector<TreeNode*> generateTrees(int n) {
+        vector<vector<vector<TreeNode*>>> dp(n + 1, vector(n + 1, vector<TreeNode*>(0)));
+        for(int i = 1; i <= n; i++) {
+            dp[i][i].push_back(new TreeNode(i));
+        }        
 
-            for (auto& left : leftTrees) {
-                for (auto& right : rightTrees) {
-                    TreeNode* currTree = new TreeNode(i);
-                    currTree->left = left;
-                    currTree->right = right;
-                    allTrees.emplace_back(currTree);
+        for(int numOfNodes = 2; numOfNodes <= n; numOfNodes++) {
+            for(int start = 1; start <= n-numOfNodes+1; start++) {
+                int end = start + numOfNodes - 1;
+                for(int i = start; i<= end;i++) {
+                    vector<TreeNode*> leftSubtrees = i-1 >= start?dp[start][i-1] : vector<TreeNode*>({NULL});
+                    vector<TreeNode*> rightSubtrees = i + 1 <= end ? dp[i + 1][end] : vector<TreeNode*>({NULL});
+                    for (auto left : leftSubtrees) {
+                        for (auto right : rightSubtrees) {
+                            TreeNode* root = new TreeNode(i, left, right);
+                            dp[start][end].push_back(root);
+                        }
+                    }
+
                 }
             }
         }
-        return allTrees;
-    }
-
-    vector<TreeNode*> generateTrees(int n) {
-        if (!n) {
-            return {};
-        }
-        return generateTrees(1, n);
+        return dp[1][n];
     }
 };
