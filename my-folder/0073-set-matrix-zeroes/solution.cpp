98d55f2e90d1de7e076486210eadeac6d486e84f
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2023-12-29 16:46:29 +0000

    [LeetCode Sync] Runtime - 19 ms (2.94%), Memory - 13.7 MB (100.00%)

--- my-folder/0073-set-matrix-zeroes/solution.cpp
+++ my-folder/0073-set-matrix-zeroes/solution.cpp
@@ -1,30 +1,35 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int rowsize=matrix.size(); 
-        int columnsize=matrix[0].size(); 
-        vector<int>rowmarker(rowsize,1);
-        vector<int>columnmarker(columnsize,1); 
-        for(int i=0;i<rowsize;i++)
-        {
-            for(int j=0;j<columnsize;j++)
-            {
-                if(matrix[i][j]==0)
-                {
-                    rowmarker[i]=0; 
-                    columnmarker[j]=0;
+        int m = matrix.size(), n = matrix[0].size();
+        bool firstColIsZero = false;
+        for(int i = 0; i < m; i++) {
+            if(matrix[i][0] == 0) {
+                firstColIsZero = true;
+            }
+            for(int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                matrix[0][j] = 0;
+                matrix[i][0] = 0;
                 }
             }
-        } 
-        for(int i=0;i<rowsize;i++)
-        {
-            for(int j=0;j<columnsize;j++)
-            {
-                if(rowmarker[i]==0 or columnmarker[j]==0)
-                    matrix[i][j]=0;
-             }
         }
-        
-    
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                matrix[i][j] = 0;
+                }
+            }
+        }    
+        if (matrix[0][0] == 0) {
+            for (int j = 0; j < n; j++) {
+                matrix[0][j] = 0;
+            }
+        }   
+        if (firstColIsZero) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }         
     }
 };
