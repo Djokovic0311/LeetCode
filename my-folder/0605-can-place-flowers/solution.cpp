0a69dcbec5e0feb5c9d56c513e64223cd6c926ee
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2023-12-29 22:33:11 +0000

    [LeetCode Sync] Runtime - 29 ms (4.34%), Memory - 20.7 MB (100.00%)

--- my-folder/0605-can-place-flowers/solution.cpp
+++ my-folder/0605-can-place-flowers/solution.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-                // Edge cases:
-        if (n == 0) return true;
-        if (flowerbed.size() == 0) return false;
-        if (flowerbed.size() == 1) return flowerbed[0] == 0;
-        
-        // Add empty plots in the beginning and end:
-        flowerbed.push_back(0);
-        flowerbed.insert(flowerbed.begin(), 0);
-        
-        // Check if there are three empty plots in a row:
-        int flowers = 0, i = 0;
-        while ((i < flowerbed.size() - 2) && (flowers < n)) {
-            if ((flowerbed[i] == 0) && (flowerbed[i+1] == 0) && (flowerbed[i+2] == 0)) {
-                flowers++;
-                flowerbed[i+1] = 1;
+        int l = flowerbed.size();
+        int count = 0;
+        for(int i = 0; i < l; i++) {
+            if(flowerbed[i] == 0) {
+                bool validLeft = i == 0? true : flowerbed[i-1] == 0;
+                bool validRight = i == l-1 ? true: flowerbed[i+1] == 0;
+                if(validLeft && validRight) {
+                    flowerbed[i] =1;
+                    n--;
+                }
+                if(n <= 0) break;
             }
-            i++;
         }
-        return flowers == n;
+        return n <= 0;
+
     }
 };
