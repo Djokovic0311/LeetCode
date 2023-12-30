743026fb945ce3ab9ad71f716c2901386caaae9a
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2023-12-30 16:08:20 +0000

    [LeetCode Sync] Runtime - 18 ms (5.90%), Memory - 9.2 MB (100.00%)

--- my-folder/0451-sort-characters-by-frequency/solution.cpp
+++ my-folder/0451-sort-characters-by-frequency/solution.cpp
@@ -1,33 +1,22 @@
-namespace
-{
-	typedef std::pair<char, int> PAIR;
-	bool fun_cmp_by_value(const PAIR& left, const PAIR& right) {
-		return left.second > right.second;
-	}
- 
-	struct funCmpByValue {
-		bool operator()(const PAIR& left, const PAIR& right) {
-			return left.second > right.second;
-		}
-	};
-}
-
 class Solution {
 public:
     string frequencySort(string s) {
         map<char, int> mp;
-        int n = s.length();
-        for(int i = 0; i < n; i++) {
-            mp[s[i]]++;
+        for(char c : s) {
+            mp[c]++;
         }
+        priority_queue<pair<int, char>> pq;
+        for(auto it : mp) {
+            pq.push({it.second, it.first});
+        }
+
         string res;
-	    std::vector<PAIR> a(mp.begin(), mp.end());
-	    sort(a.begin(), a.end(), funCmpByValue());
-        for(auto pair: a) {
-            for(int j = 0; j < pair.second; j++) {
-                res += pair.first;
-            }
-        }   
+        while(!pq.empty()){
+            auto tmp = pq.top();
+            pq.pop();
+            res += string(tmp.first, tmp.second) ;
+        }
+
         return res;
     }
 };
