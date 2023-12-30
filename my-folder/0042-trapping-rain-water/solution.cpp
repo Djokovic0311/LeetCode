54649761bde447d8df29ad63bb94cb4f57c1d43b
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2023-12-30 03:28:24 +0000

    [LeetCode Sync] Runtime - 3 ms (18.60%), Memory - 19.9 MB (100.00%)

--- my-folder/0042-trapping-rain-water/solution.cpp
+++ my-folder/0042-trapping-rain-water/solution.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
-    int trap(vector<int>& h) {
-        // edge case: empty input
-        if (!h.size()) return 0;
-        // support variables
-        int len = h.size(), dp[len], currMax = -1, res = 0;
-        // computing the maximum capacity looking from left
-        for (int i = 0, e; i < len; i++) {
-            e = h[i];
-            currMax = max(currMax, e);
-            dp[i] = currMax - e;
+    int trap(vector<int>& height) {
+        int n = height.size();
+        int left = 0, right = n-1;
+        int res = 0;
+        int left_max = 0, right_max = 0;
+        while(left < right) {
+            if(height[left]  > left_max) left_max = height[left];
+            if(height[right] > right_max) right_max = height[right];
+            // cout << left <<  " " << right << " " << left_max << " " << right_max << endl;
+            if(left_max < right_max) {
+                res += left_max - height[left];
+                // cout << "left " <<left << endl;
+                left++;
+            } else {
+                res += right_max - height[right];
+                // cout << "right " <<right << endl;
+                right--;
+            }
         }
-        // computing the maximum capacity looking from right
-        currMax = -1;
-        for (int i = len - 1, e; i > -1; i--) {
-            e = h[i];
-            currMax = max(currMax, e);
-            res += min(dp[i], currMax - e);
-        }
+
         return res;
     }
 };
+
 
