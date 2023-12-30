ee54c3020942642621ca0f72c370fe8614b88e4c
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2023-12-30 01:17:27 +0000

    [LeetCode Sync] Runtime - 23 ms (40.31%), Memory - 11.4 MB (99.99%)

--- my-folder/0001-two-sum/solution.cpp
+++ my-folder/0001-two-sum/solution.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        std::unordered_map<int, std::size_t> tmp;
-        for (std::size_t i = 0; i < nums.size(); ++i) {
-        if (tmp.count(target - nums[i])) {
-            return {int(tmp[target - nums[i]]), int(i)};
-        }
-        tmp[nums[i]] = i;
+        unordered_map<int, size_t> tmp;
+        for (size_t i = 0; i < nums.size(); ++i) {
+            if (tmp.count(target - nums[i])) {
+                return {int(tmp[target - nums[i]]), int(i)};
+            }
+            tmp[nums[i]] = i;
         }
         return {int(nums.size()), int(nums.size())};        
     }
