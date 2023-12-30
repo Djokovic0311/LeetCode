838af7d705d3d95fe21a54bb1fde9b995d3a5521
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2023-12-30 23:56:10 +0000

    Sync LeetCode submission - Add Binary (cpp)

--- problems/add_binary/solution.cpp
+++ problems/add_binary/solution.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-        auto output = ""s;
-        int i = a.size() - 1, j = b.size() - 1, carry = 0;
-        while (i >= 0 || j >= 0 || carry) {
-            if (i >= 0 && a[i--] == '1') ++carry;
-            if (j >= 0 && b[j--] == '1') ++carry;
-            output.insert(0, carry % 2 == 1 ? "1" : "0");
+        int i = a.length()-1, j = b.length()-1;
+        int carry = 0;
+        string res = "";
+        while(i>= 0 || j >= 0 ||carry > 0) {
+            if(i>=0 && a[i--] == '1') carry++;
+            if(j>=0 && b[j--] == '1') carry++;
+            res.insert(0, carry % 2 == 0? "0":"1");
             carry /= 2;
         }
-        return output;        
+        return res;
     }
 };
