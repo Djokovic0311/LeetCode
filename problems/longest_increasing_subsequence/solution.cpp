417dec87ddf4ee096c820f56cfa2ba77a29c23d2
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2023-12-30 22:07:26 +0000

    Sync LeetCode submission - Longest Increasing Subsequence (cpp)

--- problems/longest_increasing_subsequence/solution.cpp
+++ problems/longest_increasing_subsequence/solution.cpp
@@ -6,13 +6,15 @@ public:
         dp[0] = 1;
         int res = 1;
         for(int i = 1; i < n; i++) {
-            for(int j = i-1; j >= 0; j--) {
-                if(nums[i] > nums[j] && dp[i] < dp[j])
+            for(int j = i-1; j>= 0; j--) {
+                if(nums[i] > nums[j] && dp[i] < dp[j]) {
                     dp[i] = dp[j];
+                }
             }
             dp[i]++;
             res = max(res, dp[i]);
         }
         return res;
+
     }
 };
