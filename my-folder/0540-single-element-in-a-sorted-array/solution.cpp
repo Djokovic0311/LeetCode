cd1c7a4c82ad60f79438e428f7d0796b59e7017c
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2023-12-31 16:04:11 +0000

    [LeetCode Sync] Runtime - 53 ms (4.30%), Memory - 22.6 MB (100.00%)

--- my-folder/0540-single-element-in-a-sorted-array/solution.cpp
+++ my-folder/0540-single-element-in-a-sorted-array/solution.cpp
@@ -2,10 +2,26 @@ class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
         int n = nums.size();
-        if(n == 1) return nums[0];
-        for(int i = 0; i < n - 2; i+=2) {
-            if(nums[i+1] != nums[i]) return nums[i];
+        int left = 0, right = n-1;
+        while(left < right) {
+            int mid = left + (right-left);
+            // if the right half is even len
+            bool halvesAreEven = (right - mid) % 2 == 0;
+            if(nums[mid+1] == nums[mid]) {
+                if(halvesAreEven) {
+                    left = mid + 2;
+                } else {
+                    right = mid -1 ;
+                }
+            } else if(nums[mid-1] == nums[mid]) {
+                if(halvesAreEven) {
+                    right = mid - 2;
+                } else {
+                    left = mid + 1;
+                }
+            } else return nums[mid];
         }
-        return nums[n-1];        
+
+        return nums[left];
     }
 };
