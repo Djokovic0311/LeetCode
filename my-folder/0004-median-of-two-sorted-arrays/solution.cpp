69f92ee2fd21c3f3cdd46ddde3cfdb0896427d75
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2023-12-31 04:25:51 +0000

    [LeetCode Sync] Runtime - 51 ms (5.32%), Memory - 89.7 MB (99.98%)

--- my-folder/0004-median-of-two-sorted-arrays/solution.cpp
+++ my-folder/0004-median-of-two-sorted-arrays/solution.cpp
@@ -1,46 +1,35 @@
 class Solution {
 public:
-    double mediann(vector<int>&a,vector<int>&b){
-        int m=a.size();
-        int n=b.size();
-        if(m>n)
-            return mediann(b,a);
-        int l=0,r=m;
-        while(l<=r){
-            int partx=l+(r-l)/2;
-            int party=(m+n+1)/2-partx;
-            int maxlx=(partx==0)?INT_MIN:a[partx-1];
-            int minrx=(partx==m)?INT_MAX:a[partx];
-            int maxly=(party==0)?INT_MIN:b[party-1];
-            int minry=(party==n)?INT_MAX:b[party];
-            if(maxlx<=minry&&maxly<=minrx){
-                if((m+n)%2==0)
-                    return (double)(max(maxlx,maxly)+min(minrx,minry))/2;
-                else
-                    return (double)(max(maxlx,maxly));
-            }else if(maxlx>minry)
-                r=partx-1;
-            else
-                l=partx+1;
-        }
-        return -1.0;
-    }
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        vector<int>v;
+        if (nums1.size() > nums2.size()) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
         
-        // store the array in the new array
-        for(auto num:nums1)   // O(n1)
-            v.push_back(num);
+        int m = nums1.size(), n = nums2.size();
+        int left = 0, right = m;
         
-        for(auto num:nums2)  // O(n2)
-            v.push_back(num);
+        while (left <= right) {
+            int partitionA = (left + right) / 2;
+            int partitionB = (m + n + 1) / 2 - partitionA;
+            
+            int maxLeftA = (partitionA == 0) ? INT_MIN : nums1[partitionA - 1];
+            int minRightA = (partitionA == m) ? INT_MAX : nums1[partitionA];
+            int maxLeftB = (partitionB == 0) ? INT_MIN : nums2[partitionB - 1];
+            int minRightB = (partitionB == n) ? INT_MAX : nums2[partitionB];
+            
+            if (maxLeftA <= minRightB && maxLeftB <= minRightA) {
+                if ((m + n) % 2 == 0) {
+                    return (max(maxLeftA, maxLeftB) + min(minRightA, minRightB)) / 2.0;
+                } else {
+                    return max(maxLeftA, maxLeftB);
+                }
+            } else if (maxLeftA > minRightB) {
+                right = partitionA - 1;
+            } else {
+                left = partitionA + 1;
+            }
+        }
         
-        // Sort the array to find the median
-        sort(v.begin(),v.end());  // O(nlogn)
-        
-        // Find the median and Return it
-        int n=v.size();  // O(n)
-        
-        return n%2?v[n/2]:(v[n/2-1]+v[n/2])/2.0; 
+        return 0.0;
     }
 };
