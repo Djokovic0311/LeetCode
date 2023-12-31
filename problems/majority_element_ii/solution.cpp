f49876efa636090534bed1eb119652ab5eab3fa2
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2023-12-31 01:00:57 +0000

    Sync LeetCode submission - Majority Element II (cpp)

--- problems/majority_element_ii/solution.cpp
+++ problems/majority_element_ii/solution.cpp
@@ -1,44 +1,35 @@
 class Solution {
 public:
-    vector<int> majorityElement(vector<int>& nums) 
-    {
-        int sz = nums.size();
-        int num1 = INT_MIN, num2 = INT_MIN, count1 = 0, count2 = 0, i;
-        for (i = 0; i < sz; i++)
-        {
-            if (nums[i] == num1)
+    vector<int> majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        int count1 = 0, count2 = 0;
+        int candidate1 = INT_MIN, candidate2 = INT_MIN;
+        for(int num : nums) {
+            if (candidate1 != INT_MIN && candidate1 == num) {
                 count1++;
-            else if (nums[i] == num2)
+            } else if (candidate2 != INT_MIN && candidate2 == num) {
                 count2++;
-            else if (count1 == 0)
-            {
-                num1 = nums[i];
-                count1 = 1;
-            }    
-            else if (count2 == 0)
-            {
-                num2 = nums[i];
-                count2 = 1;
-            }
-            else
-            {
+            } else if (count1 == 0) {
+                candidate1 = num;
+                count1++;
+            } else if (count2 == 0) {
+                candidate2 = num;
+                count2++;
+            } else {
                 count1--;
                 count2--;
             }
         }
-        vector<int> ans;
-        count1 = count2 = 0;
-        for (i = 0; i < sz; i++)
-        {
-            if (nums[i] == num1)
-                count1++;
-            else if (nums[i] == num2)
-                count2++;
+        count1 = 0;
+        count2 = 0;
+        vector<int> result;
+        for (int n: nums) {
+            if (candidate1 != INT_MIN && n == candidate1) count1++;
+            if (candidate2 != INT_MIN && n == candidate2) count2++;
         }
-        if (count1 > sz/3)
-            ans.push_back(num1);
-        if (count2 > sz/3)
-            ans.push_back(num2);
-        return ans;
+
+        if (count1 > n/3) result.push_back(candidate1);
+        if (count2 > n/3) result.push_back(candidate2);
+        return result;
     }
 };
