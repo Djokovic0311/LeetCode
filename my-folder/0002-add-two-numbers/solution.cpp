fd2506f6a15cb398d3c249d31cdbc325e3359570
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2024-01-01 16:48:05 +0000

    [LeetCode Sync] Runtime - 23 ms (7.45%), Memory - 71.8 MB (99.96%)

--- my-folder/0002-add-two-numbers/solution.cpp
+++ my-folder/0002-add-two-numbers/solution.cpp
@@ -11,17 +11,23 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-         int c = 0;
-        ListNode* newHead = new ListNode(0);
-        ListNode* t = newHead;
-        while(c||l1||l2) {
-            c += (l1? l1->val : 0) + (l2? l2->val : 0);
-            t->next = new ListNode(c%10);
-            t = t->next;
-            c /= 10;
-            if(l1) l1 = l1->next;
-            if(l2) l2 = l2->next;
+        int carry = 0;
+        ListNode * dummy = new ListNode(-1);
+        ListNode* curr = dummy;
+        while(carry || l1 || l2) {
+            int sum = carry;
+            if(l1) {
+                sum += l1->val;
+                l1 = l1->next;
+            }
+            if(l2) {
+                sum += l2->val;
+                l2 = l2->next;                
+            }
+            curr->next = new ListNode(sum%10);
+            carry = sum / 10;
+            curr = curr->next;
         }
-        return newHead->next;       
+        return dummy->next;
     }
 };
