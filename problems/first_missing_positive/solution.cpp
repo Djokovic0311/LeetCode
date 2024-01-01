d9199247e5bb018126176e40274cb9668e046737
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2024-01-01 02:05:41 +0000

    Sync LeetCode submission - First Missing Positive (cpp)

--- problems/first_missing_positive/solution.cpp
+++ problems/first_missing_positive/solution.cpp
@@ -1,16 +1,25 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-         int n=nums.size();
-        for(int i=0 ; i<n ; i++)
+        int n=nums.size();
+        for(int i=0;i<n;i++)
         {
-            while(nums[i]>0 and nums[i]<=n and nums[i]!=nums[nums[i]-1])
-                swap(nums[i],nums[nums[i]-1]);
+            if(nums[i]<=0) nums[i]=n+1;
         }
-        for(int i=0 ; i<n ; i++)  {
-            // cout << nums[i] << ' ';
-            if(nums[i] != i+1) return i+1;
+        for(int i=0;i<n;i++)
+        {
+            if(abs(nums[i])<=n && nums[abs(nums[i])-1]>0)
+            {
+                nums[abs(nums[i])-1]*=-1;
+            }
         }
-        return n+1;      
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]>0)
+            {
+                return i+1;
+            }
+        }
+        return n+1;
     }
 };
