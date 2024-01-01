156a0e769216c0bba508deeca945bf8af0462331
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2024-01-01 02:26:48 +0000

    Sync LeetCode submission - Combination Sum II (cpp)

--- problems/combination_sum_ii/solution.cpp
+++ problems/combination_sum_ii/solution.cpp
@@ -1,29 +1,33 @@
 class Solution {
 public:
     vector<vector<int>> res;
-    void dfs(vector<int>& candidates, int target, int idx, vector<int>& cur, int sum) {
+    void dfs(vector<int>& candidates, int target, vector<int>& cur, int sum, int idx) {
+        int n = candidates.size();
         if(sum == target) {
             res.push_back(cur);
             return;
         }
-        for(int i = idx; i < candidates.size(); i++) {
-            if(i != idx && candidates[i] == candidates[i-1])
+
+        for(int i = idx; i < n; i++) {
+            if(i != idx && candidates[i] == candidates[i-1]) {
                 continue;
-            if(sum + candidates[i] <= target) {
-                sum += candidates[i];
-                cur.push_back(candidates[i]);
-                dfs(candidates, target, i+1, cur, sum);
-                cur.pop_back();
-                sum -= candidates[i];
             }
-            else return;
+            if(sum + candidates[i] <= target) {
+                sum+= candidates[i];
+                cur.push_back(candidates[i]);
+                dfs(candidates, target, cur, sum, i+1);
+                cur.pop_back();
+                sum -= candidates[i];                
+            } else return;
+
         }
+
+        return;
     }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         sort(candidates.begin(), candidates.end());
-        int n = candidates.size();
         vector<int> cur;
-        dfs(candidates, target, 0, cur, 0);
+        dfs(candidates, target, cur, 0,0);
         return res;
     }
 };
