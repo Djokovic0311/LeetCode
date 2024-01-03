80aad9f7400b5ef05ee18fedda31c5a806efae40
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2024-01-03 03:33:45 +0000

    [LeetCode Sync] Runtime - 5 ms (41.70%), Memory - 7.3 MB (100.00%)

--- my-folder/0785-basic-calculator-iii/solution.cpp
+++ my-folder/0785-basic-calculator-iii/solution.cpp
@@ -1,35 +1,48 @@
 class Solution {
 public:
     int calculate(string s) {
-        int i = 0;
-        return parseExpr(s, i);
-    }
-    
-    int parseExpr(string& s, int& i) {
-        vector<int> nums;
-        char op = '+';
-        for (; i < s.length() && op != ')'; i++) {
-            if (s[i] == ' ') continue;
-            long n = s[i] == '(' ? parseExpr(s, ++i) : parseNum(s, i);
-            switch(op) {
-                case '+' : nums.push_back(n); break;
-                case '-' : nums.push_back(-n); break;
-                case '*' : nums.back() *= n; break;
-                case '/' : nums.back() /= n; break;
-            }            
-            if (i < s.length())    // handle over flow of i in cases like "1 + 1"
-                op = s[i];
+        s += "+";
+        char lastOperation = '+';
+        int lastNumber = 0, currNumber = 0, eval = 0;
+        stack<int> stack;
+        unordered_map<char,int> sign{{'+', 0}, {'-', 1}, {'*', 2}, {'/', 3}};
+        unordered_map<int, char> number{{0, '+'}, {1, '-'}, {2, '*'}, {3, '/'}};
+
+        for(int i = 0; i < s.length(); ++i) {
+            if(isdigit(s[i])) {
+                currNumber = currNumber*10 + (s[i]-'0');
+            } else {
+                if(s[i] == '(') {
+                    stack.push(eval);
+                    stack.push(lastNumber);
+                    stack.push(sign[lastOperation]);
+                    eval = 0; lastOperation = '+'; lastNumber = 0;
+                } else {
+                    if(lastOperation == '+') {
+                        eval += lastNumber;
+                        lastNumber = currNumber;
+                    } else if(lastOperation == '-') {
+                        eval += lastNumber;
+                        lastNumber = -currNumber;
+                    } else if(lastOperation == '*') {
+                        lastNumber = lastNumber*currNumber;
+                    } else if(lastOperation == '/') {
+                        lastNumber = lastNumber/currNumber;
+                    }
+                    currNumber = 0;
+                    if(s[i] == ')') {
+                        eval += lastNumber;
+                        currNumber = eval;
+                        lastOperation = number[stack.top()]; stack.pop();
+                        lastNumber = stack.top(); stack.pop();
+                        eval = stack.top(); stack.pop();
+                    } else {
+                        lastOperation = s[i];
+                    }
+                }
+            }
         }
-        for(int k : nums)
-            cout << k << " ";
-        cout << endl;
-        return accumulate(nums.begin(), nums.end(), 0);
-    }
-    
-    long parseNum(string& s, int& i) {  // change to long to pass case like "0-2147483648"
-        long n = 0;
-        while(i < s.length() && isdigit(s[i]))
-            n = s[i++] - '0' + 10 * n;
-        return n;
+
+        return eval + lastNumber;
     }
 };
