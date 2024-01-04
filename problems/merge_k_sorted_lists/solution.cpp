1e75bcfb34b33f64acfc42dfd6d4790dc3aa57e9
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2024-01-04 00:32:13 +0000

    Sync LeetCode submission - Merge k Sorted Lists (cpp)

--- problems/merge_k_sorted_lists/solution.cpp
+++ problems/merge_k_sorted_lists/solution.cpp
@@ -10,22 +10,37 @@
  */
 class Solution {
 public:
-    ListNode* merge2Lists(ListNode* l1, ListNode* l2) {
-        if (!l1) return l2;
-        if (!l2) return l1;
-        ListNode* head = l1->val <= l2->val? l1 : l2;
-        head->next = l1->val <= l2->val ? merge2Lists(l1->next, l2) : merge2Lists(l1, l2->next);
-        return head;
+    ListNode* merge2List(ListNode* l1, ListNode* l2) {
+        ListNode* head = new ListNode(0);
+        ListNode* point = head;
+        while(l1 && l2) {
+            if(l1->val <= l2->val) {
+                head->next = l1;
+                l1 = l1->next;
+            } else {
+                head->next = l2;
+                l2 = l2->next;
+            }
+            head = head->next;
+        }
+
+        if(!l1) {
+            head->next = l2;
+        } else {
+            head->next = l1;
+        }
+
+        return point->next;
     }
-    
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        if (lists.size() == 0) return NULL;
-        
-        ListNode* head = lists[0];
-        
-        for (int i = 1; i < lists.size(); i++)
-            head = merge2Lists(head, lists[i]);
-        
-        return head;
+        int n= lists.size();
+        int interval = 1;
+        while(interval < n) {
+            for(int i = 0; i < n - interval; i += interval*2) {
+                lists[i] = merge2List(lists[i], lists[i+interval]);
+            }
+            interval *= 2;
+        }
+        return n > 0 ? lists[0] : NULL;
     }
 };
