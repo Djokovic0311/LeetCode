c637d59931e1ef091af758c9284f363f54b67519
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2024-01-04 20:08:29 +0000

    Sync LeetCode submission - Expression Add Operators (cpp)

--- problems/expression_add_operators/solution.cpp
+++ problems/expression_add_operators/solution.cpp
@@ -7,40 +7,23 @@ public:
     }
     void dfs(string &num , int target , int index , string path , long int res , long int prev)
     {
-        if(index == num.size() and res == target)
-        {
+        if(index == num.length() && res == target) {
             expressions.push_back(path);
             return;
         }
         
-        long int number = 0;
-        string sub_string = "";
-        
-        for(int i = index ; i < num.size() ; ++i)
-        {
-            sub_string += num[i];
-            number = (number * 10) + num[i] - '0';
-            
-            if(sub_string.size() > 1 and sub_string[0] == '0') // skipping numbers with leading 0's
-                break;
-            
-            if(index == 0) // first number of expression is picked without operator
-            {
-                dfs(num , target , i + 1 , sub_string , number, number);
-            }
-            else
-            {
-                // (1) + operation inserted
-                dfs(num , target , i + 1 , path  + "+" + sub_string , res  + number , number);
-                    
-                // (2) - operation inserted
-                dfs(num , target , i + 1 , path + "-" + sub_string , res  - number , -number);
-                
-                // (3) * operation inserted
-                dfs(num , target , i + 1 , path + "*" + sub_string , (res - prev)  + (prev * number) , prev * number);
+        for(int i = index; i < num.length(); i++) {
+            string s = num.substr(index, i-index+1);
+            if(s.length() > 1 && s[0] == '0') break;
+            long int curr = stol(s);
+            if(index == 0) {
+                dfs(num, target, i+1, s, curr, curr);
+            } else {
+                dfs(num, target, i+1, path+"+"+s, res + curr, curr);
+                dfs(num, target, i+1, path+"-"+s, res - curr, -curr);
+                dfs(num, target, i+1, path+"*"+s, res - prev + curr * prev, prev * curr);
             }
         }
-            
     }
     
 };
