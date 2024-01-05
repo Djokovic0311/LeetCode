ed7c9fab5db121cf0cda6ec93258eb1d8753875e
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2024-01-05 05:24:55 +0000

    [LeetCode Sync] Runtime - 38 ms (5.13%), Memory - 31.7 MB (100.00%)

--- my-folder/1059-missing-element-in-sorted-array/solution.cpp
+++ my-folder/1059-missing-element-in-sorted-array/solution.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
     int missingElement(vector<int>& nums, int k) {
-        int cnt = 0;
         int n = nums.size();
-        for(int i = 0; i < n-1; i++) {
-            if((cnt + nums[i+1] - nums[i] - 1) < k) {
-                // cout << cnt + nums[i+1] - nums[i] - 1 << endl;
-                cnt += nums[i+1] - nums[i] - 1;
+        int left = 0, right = n - 1;
+
+        while(left <= right){
+            int mid = left + (right -left) / 2;
+            int total_missing_number = nums[mid] - nums[0] - mid;
+            if(total_missing_number < k){
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
-            else {
-                return k-cnt + nums[i];
-            }
-           
         }
-        return k-cnt + nums[n-1];
+        // Skipped before l and between l i.e [l-1, l];
+        // i.e. nums[l-1] - num[0] - (l-1);
+        // the numbers already skipped before nums[l-1] can be subtracted
+        // from k to get the remaining left.
+
+        // add the remaining in nums[l-1] and that will be the answer;
+        int skipped = nums[left-1] - nums[0] - (left-1);
+        return nums[left-1] + (k-skipped);
     }
 };
