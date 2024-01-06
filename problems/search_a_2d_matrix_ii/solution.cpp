1651537364b2c16ac24d223577e4036b732b14c9
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2024-01-06 05:08:11 +0000

    Sync LeetCode submission - Search a 2D Matrix II (cpp)

--- problems/search_a_2d_matrix_ii/solution.cpp
+++ problems/search_a_2d_matrix_ii/solution.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
-
-    bool search(int left, int up, int right, int down, int target, vector<vector<int>>& matrix) {
-
-        if(left > right || up > down) return false;
-        else if (target < matrix[up][left] || target > matrix[down][right]) return false;
-        int mid = left + (right-left)/2;
-        int row = up;
-        // Locate `row` such that matrix[row-1][mid] < target < matrix[row][mid]
-        while(row <= down && matrix[row][mid] <= target) {
-            if(matrix[row][mid] == target) return true;
-            row++;
-        }
-        return search(left, row, mid-1, down, target, matrix) || search(mid+1,up,right,row-1, target, matrix);
-    }
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int m = matrix.size(), n = matrix[0].size();
         if(m == 0 || n == 0) return false;
+        int row = m-1, col = 0;
+        while(col < n && row >= 0) {
+            if(matrix[row][col] > target) {
+                row -= 1;
+            } else if(matrix[row][col] < target) {
+                col+=1;
+            } else return true;
+        }
 
-        return search(0,0,n-1, m-1, target, matrix);
-    }
+        return false;
+    } 
 };
