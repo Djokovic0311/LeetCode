45518c33e18012c62971bb69139d0fba438debc5
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2024-01-10 15:57:33 +0000

    Sync LeetCode submission - Longest Substring with At Least K Repeating Characters (cpp)

--- problems/longest_substring_with_at_least_k_repeating_characters/solution.cpp
+++ problems/longest_substring_with_at_least_k_repeating_characters/solution.cpp
@@ -2,13 +2,52 @@ class Solution {
 public:
     int longestSubstring(string s, int k) {
         int n = s.length();
-        if(n<k) return 0;
-        unordered_map<char,int> freq;
-        for(auto a:s) ++freq[a];
-        int i=0;
-        while(i<n and freq[s[i]]>=k) ++i;
-        if(i==n) return i;
-        if(i==0) return (n>1)?longestSubstring(s.substr(1,n-1),k):0;
-        return max(longestSubstring(s.substr(0,i),k),longestSubstring(s.substr(i,s.size()-i),k));
+        vector<int> o_freq(26, 0);
+        int unique = 0;
+        int res = 0;
+
+        for(char c : s) {
+            o_freq[c-'a']++;
+        }
+        for(int f : o_freq) {
+            if(f != 0)
+                unique++;
+        }
+
+
+        for(int curr_unique = 1; curr_unique <= unique; curr_unique++) {
+            int start = 0, end = 0;
+            vector<int> freq(26, 0);
+            int cnt = 0, cntk = 0;
+            while(end < n) {
+                if(cnt <= curr_unique) {
+                    int idx = s[end] - 'a';
+                    if(freq[idx] == 0) {
+                        cnt++;
+                    } 
+                    freq[idx]++;
+                    if(freq[idx] == k) {
+                        cntk++;
+                    }
+                    end++;
+                } else {
+                    int idx = s[start] - 'a';
+                    if(freq[idx] == k) {
+                        cntk--;
+                    }
+                    freq[idx]--;
+                    if(freq[idx] == 0) {
+                        cnt--;
+                    }
+                    start++;
+                }
+                if(cntk == curr_unique && cnt == curr_unique) {
+                    cout << start << " " << end << endl;
+                    res = max(res, end - start);
+                    cout << res << endl;
+                }
+            }
+        }
+        return res;
     }
 };
