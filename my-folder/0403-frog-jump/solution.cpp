00ebc88027022eac77c202261b270e0c3d3ef50f
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2024-01-10 19:51:28 +0000

    [LeetCode Sync] Runtime - 151 ms (25.10%), Memory - 52.2 MB (40.05%)

--- my-folder/0403-frog-jump/solution.cpp
+++ my-folder/0403-frog-jump/solution.cpp
@@ -3,7 +3,7 @@ public:
     bool canCross(vector<int>& stones) {
         unordered_map<int, unordered_set<int>> mp;
         mp[stones[0]+1] = {1};
-        for(int i = 0; i < stones.size(); i++) {
+        for(int i = 1; i < stones.size(); i++) {
             int pos = stones[i];
             for(auto it : mp[pos]) {
                 mp[pos+it].insert(it);
