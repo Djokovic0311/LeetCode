9d14cb93ab3f9985e6f6982824e53bbbaadd422c
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2024-01-11 20:10:19 +0000

    Sync LeetCode submission - Adding Spaces to a String (cpp)

--- problems/adding_spaces_to_a_string/solution.cpp
+++ problems/adding_spaces_to_a_string/solution.cpp
@@ -1,23 +1,12 @@
 class Solution {
 public:
     string addSpaces(string s, vector<int>& spaces) {
-        int n = s.length();
-        string res = "";
-        int size = spaces.size();
-        if(size == 1) {
-            if(spaces[0] == 0) return " " + s;
-            else return s.substr(0,spaces[0]) + " " + s.substr(spaces[0], n-spaces[0]);
+        string res = s.substr(0, spaces[0]);
+        for(int i = 0; i < spaces.size()-1; i++) {
+                res +=  " " + s.substr(spaces[i], spaces[i+1] - spaces[i]);
         }
-        for(int i = 0; i < size-1; i++) {
-            if(spaces[i] == 0) res = " " + res;
-            if(i == 0 && spaces[0] != 0) {res += s.substr(0,spaces[i]); res += " ";}
-            
-            res += s.substr(spaces[i],spaces[i+1] - spaces[i]);
-            res += " ";                
-            
+        res += " " + s.substr(spaces[spaces.size()-1]);
 
-        }
-        res += s.substr(spaces[size-1], n-spaces[size-1]);
         return res;
     }
 };
