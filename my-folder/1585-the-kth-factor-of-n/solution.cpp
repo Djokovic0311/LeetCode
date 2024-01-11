911ea350396cfafb968195955ac14f02f67bd036
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2024-01-11 15:46:29 +0000

    [LeetCode Sync] Runtime - 3 ms (2.24%), Memory - 6.3 MB (100.00%)

--- my-folder/1585-the-kth-factor-of-n/solution.cpp
+++ my-folder/1585-the-kth-factor-of-n/solution.cpp
@@ -1,19 +1,24 @@
 class Solution {
 public:
     int kthFactor(int n, int k) {
-        vector<int> res;
-        for(int i = 1; i * i <= n; i++) {
-            
-            if(n % i == 0 && i * i != n) {
-                
-                res.push_back(i);
-                res.push_back(n/i);
-            }
-            else if(i * i == n)
-            res.push_back(i);
+        vector<int> divisors;
+        int sqrtN = (int)sqrt(n);
+        for (int x = 1; x < sqrtN + 1; ++x) {
+            if (n % x == 0) {
+                --k;
+                divisors.push_back(x);
+                if (k == 0) {
+                    return x;    
+                }    
+            }    
         }
-        sort(res.begin(), res.end());
-        if(res.size() < k) return -1;
-        return res[k-1];
+        
+        // If n is a perfect square, skip the duplicate in the divisor list
+        if (sqrtN * sqrtN == n) {
+            ++k;    
+        }
+        
+        int nDiv = divisors.size();
+        return (k <= nDiv) ? n / divisors[nDiv - k] : -1;
     }
 };
