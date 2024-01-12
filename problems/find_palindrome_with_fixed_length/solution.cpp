4c3ca3f40ce12e1e44f1bbc45a9d3ff35bf39067
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2024-01-12 19:48:01 +0000

    Sync LeetCode submission - Find Palindrome With Fixed Length (cpp)

--- problems/find_palindrome_with_fixed_length/solution.cpp
+++ problems/find_palindrome_with_fixed_length/solution.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
-    vector<long long> kthPalindrome(vector<int>& q, int l) {
-        int k=(l+1)/2,start=pow(10,k-1),end=pow(10,k)-1;
-        vector<long long> A;
-        for(int i=0;i<q.size();i++)
-        {
-            int a=start+q[i]-1;     //Since the palindromes are in order 'a' is the half palindeome of the required palindrome 
-            if(a>end)               //We want the number of length 'k', if it crosses the length 'k' then our output will be '-1'
-            {
-                A.push_back(-1);
+    vector<long long> kthPalindrome(vector<int>& queries, int intLength) {
+        int k = (intLength + 1) /2;
+        vector<long long> res;
+        int start = pow(10, k-1), end = pow(10, k) - 1;
+        for(auto q : queries) {
+            int a = start + q - 1;
+            cout << a << " ";
+            if(a > end) {
+                res.push_back(-1);
                 continue;
+            } else if (intLength % 2 == 0){
+                string tmp = to_string(a);
+                string reversed = tmp;
+                reverse(reversed.begin(), reversed.end());
+                res.push_back(stoll(tmp + reversed));
+            } else {
+                string tmp = to_string(a);
+                string reversed = tmp;
+                reverse(reversed.begin(), reversed.end());
+                res.push_back(stoll(tmp + reversed.substr(1)));               
             }
-            string s=to_string(a),t=s;
-            reverse(t.begin(),t.end());
-            if(l%2==0)
-            {
-                s+=t;               //Number is "321" then palindrome of even length will be "321" "123"
-                A.push_back(stoll(s));
-            }
-            else
-            {
-                s+=t.substr(1);     //Number is "321" then palindrome of odd length will be "321" "_23"
-                A.push_back(stoll(s));
-            }
+
         }
-        return A;   
+        return res;
     }
 };
