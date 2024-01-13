9cd1646a50eec67cc2a4922eb98cff71075cfea7
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2024-01-13 23:16:31 +0000

    Sync LeetCode submission - Product of the Last K Numbers (cpp)

--- problems/product_of_the_last_k_numbers/solution.cpp
+++ problems/product_of_the_last_k_numbers/solution.cpp
@@ -1,44 +1,22 @@
 class ProductOfNumbers {
 public:
-    
-    // store prefix product of numbers
-    vector<int>products;
-    
-    // store position of last zero number
-    int last_zero;
-    
+    vector<int> prefix = {1};
     ProductOfNumbers() {
         
-        // insert initial value product value 1
-        products.push_back(1);
-        
-        // initially no zero value
-        last_zero  = -1;
     }
     
     void add(int num) {
-        
-        if(num==0 || products.back()==0)
-        {
-            if(num==0) last_zero = products.size();
-            
-            products.push_back(num);
-        }
-        else
-        {
-            products.push_back(products.back()*num);
+        if(num) {
+            prefix.push_back(prefix.back() * num);
+        } else {
+            prefix = {1};
         }
     }
     
     int getProduct(int k) {
-        
-        int limit = products.size()-k;
-        
-        if(last_zero>=limit) return 0;
-        
-        if(products[limit-1]==0) return products.back();
-        
-        return products.back()/products[limit-1];
+        if(prefix.size() > k) {
+            return prefix.back() / prefix[prefix.size()-1-k];
+        } else return 0;
     }
 };
 
