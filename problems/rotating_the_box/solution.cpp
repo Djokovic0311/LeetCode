aaf8b13d32731298315d2d4ba6adcce39c3a485c
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2024-01-13 04:06:04 +0000

    Sync LeetCode submission - Rotating the Box (cpp)

--- problems/rotating_the_box/solution.cpp
+++ problems/rotating_the_box/solution.cpp
@@ -3,23 +3,29 @@ public:
     vector<vector<char>> rotateTheBox(vector<vector<char>>& box) {
         int m = box.size(), n = box[0].size();
         vector<vector<char>> res(n, vector<char>(m,'.'));
-
+        // traverse the box by row from the last row
         for(int i = m-1; i >= 0; i--) {
+            // cnt of # until now
             int cnt = 0;
+            // the column in result
             int y = m - i -1;
+            // traverse the box by col from the first column
             for(int j = 0; j < n; j++) {
                 if(box[i][j] == '#') {
                     cnt++;
                 }
+                // meet obstacle
                 else if(box[i][j]=='*'){
-                    //Obstacle
                     res[j][y]='*';
+                    // mark the cells in the same column in upper rows in result
                     for(int c=1; c<=cnt; ++c) 
                         res[j-c][y]='#';
                     cnt=0;
                 }
             }
-            for(int c=1; c<=cnt; ++c) res[n-c][y]='#';
+            // meet the bottom
+            for(int c=1; c<=cnt; ++c) 
+                res[n-c][y]='#';
         }
         return res;
     }
