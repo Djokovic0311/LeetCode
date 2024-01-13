a2288a25d86a735d25a94b19159bd2baaf6d888c
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2024-01-13 23:53:15 +0000

    Sync LeetCode submission - Next Greater Element III (cpp)

--- problems/next_greater_element_iii/solution.cpp
+++ problems/next_greater_element_iii/solution.cpp
@@ -1,10 +1,24 @@
 class Solution {
 public:
     int nextGreaterElement(int n) {
-        long res = 0; string dig = to_string(n);
-        next_permutation(dig.begin(),dig.end());
-        res = stol(dig);
-        if(res <= n || res > INT_MAX) return -1;
-        return res;
+        string s = to_string(n);
+        int i = INT_MAX;
+        for(i = s.length()-1; i > 0; i--) {
+            if(s[i] > s[i-1]) break;
+        }
+        
+        if(i == 0) return -1;
+        
+        reverse(s.begin()+i, s.end());
+
+        int pos = upper_bound(s.begin()+i-1, s.end(), s[i-1]) - s.begin();
+        // cout << pos << endl;
+        swap(s[i-1], s[pos]);
+        
+        
+        long long res = stoll(s);
+        // cout << res;
+        if(res > INT_MAX) return -1;
+        return stoi(s);
     }
 };
