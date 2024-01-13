d85244089add2d1f93f1f9f0a8954b2be67aeff5
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2024-01-13 19:16:42 +0000

    [LeetCode Sync] Runtime - 276 ms (8.13%), Memory - 127.5 MB (5.24%)

--- my-folder/0863-sum-of-distances-in-tree/solution.cpp
+++ my-folder/0863-sum-of-distances-in-tree/solution.cpp
@@ -1,39 +1,36 @@
 class Solution {
 public:
-	vector<vector<int>> tree;
-	map<pair<int,int>,int> m;
-	map<pair<int,int>,int> under;
-
-	pair<int,int> getDfs(int i,int p) {
-
-		int ans=0,c=1;
-		for(int j: tree[i]) {
-			if(j==p) continue;
-			if(m.find({i,j})==m.end()) {
-				auto p= getDfs(j,i);
-				m[{i,j}]=p.first;
-				under[{i,j}]=p.second;
-			}
-			ans+= m[{i,j}]+under[{i,j}];
-			c+=under[{i,j}];
-		}
-		return {ans,c};
-	}
-
-	vector<int> sumOfDistancesInTree(int n, vector<vector<int>>& edges) {
-		m.clear();
-		under.clear();
-
-		tree.resize(n);
-		vector<int> ans;
-		for(auto i: edges) {
-			tree[i[0]].push_back(i[1]);
-			tree[i[1]].push_back(i[0]);
-		}
-
-		for(int i=0;i<n;i++) {
-			ans.push_back(getDfs(i,-1).first);
-		}
-		return ans;
-	}
+    vector<int> res, count;
+    vector<set<int>> graph;
+    int N;
+    vector<int> sumOfDistancesInTree(int n, vector<vector<int>>& edges) {
+        graph.resize(n);
+        res.resize(n);
+        count.resize(n,1);
+        N = n;
+        for(auto e : edges) {
+            graph[e[0]].insert(e[1]);
+            graph[e[1]].insert(e[0]);
+        }
+        dfs(0,-1);
+        dfs2(0,-1);
+        return res;
+    }
+    void dfs(int node, int parent) {
+        for(int child : graph[node]) {
+            if(child != parent) {
+                dfs(child, node);
+                count[node] += count[child];
+                res[node] += res[child] + count[child];
+            }
+        }
+    }
+    void dfs2(int node, int parent) {
+        for(int child : graph[node]) {
+            if(child != parent) {
+                res[child] = res[node] - count[child] + N - count[child];
+                dfs2(child, node);
+            }
+        }
+    }
 };
