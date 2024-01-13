99d9c5ef8fe0c86716e519d78b737cad11ef7660
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2024-01-13 03:50:36 +0000

    Sync LeetCode submission - Amount of New Area Painted Each Day (cpp)

--- problems/amount_of_new_area_painted_each_day/solution.cpp
+++ problems/amount_of_new_area_painted_each_day/solution.cpp
@@ -1,40 +1,28 @@
 class Solution {
 public:
-    
-    int InsertInterval(map<int, int> &mp, int start, int end) 
-    {
-        auto up_it = mp.upper_bound(end);
-        auto lo_it = std::prev(mp.lower_bound(start));
-        // cout << up_it->first <<' ' <<  up_it->second << endl;
-        // cout << lo_it->first <<' ' <<  lo_it->second << endl;        
-        lo_it = lo_it->second < start ? std::next(lo_it) : lo_it;              // First overlapping interval
-        // cout << up_it->first <<' ' <<  up_it->second << endl;
-        // cout << lo_it->first <<' ' <<  lo_it->second << endl;
-        int paint = end - start;        
-        for(auto it = lo_it; it != up_it; ++it) {
-            paint -= std::min(end, it->second) - std::max(start, it->first);   // Substract overlapping interval from the paint
-            
-            start = std::min(start, it->first);                                // Update new interval to be stored in the map
-            end   = std::max(end  , it->second);
-        }
-        // cout << start << ' ' << end << endl;
-        mp.erase(lo_it, up_it);                                               // Remove overlapping intervals
-        
-        mp[start] = end;                                                      // Insert new interval
-        return paint;
-    }
-    
-    
     vector<int> amountPainted(vector<vector<int>>& paint) {
-        map<int, int> mp;
-        mp[INT_MIN] = INT_MIN;           // Insert dummy Interval
-        // mp[INT_MAX] = INT_MAX;
-        
-        vector<int> ans;
-        for(auto p : paint) {
-            ans.push_back(InsertInterval(mp, p[0], p[1]));
+        int n = paint.size();
+        vector<int> painted(n, 0);
+        auto comp = [](const vector<int>& a, const vector<int>& b) { return a[1] < b[1]; };
+        int farthest_pt = (*max_element(paint.begin(), paint.end(), comp))[1];
+        vector<vector<pair<int, bool>>> points(farthest_pt+1);
+        for(int i = 0;i < n; i++) {
+            points[paint[i][0]].push_back({i, true});
+            points[paint[i][1]].push_back({i, false});
         }
-        
-        return ans;
+        set<int> sweep_line;
+        for(int x = 0; x <= farthest_pt; x++) {
+            // if(points[x].size()) cout << x << endl;
+            for(auto p : points[x]) {
+                // cout << p.first << " " << p.second << endl;
+                if(p.second)
+                    sweep_line.insert(p.first);
+                else
+                    sweep_line.erase(p.first);
+            }
+            if(!sweep_line.empty())
+                painted[*sweep_line.begin()] += 1;
+        }
+        return painted;
     }
 };
