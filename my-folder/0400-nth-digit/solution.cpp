8f453b723b50fd1e2badf5bb332e37419fa30135
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2024-01-14 00:47:24 +0000

    [LeetCode Sync] Runtime - 2 ms (3.65%), Memory - 6.6 MB (100.00%)

--- my-folder/0400-nth-digit/solution.cpp
+++ my-folder/0400-nth-digit/solution.cpp
@@ -1,18 +1,53 @@
 class Solution {
 public:
-    int findNthDigit(int n) {
-        if (n <= 9) return n;
-        
-        long size = 1, count = 9;
-        while (n > size * count) {
-            n -= size * count;
-            size++;
-            count *= 10;
+    int noOfDigits(int x)
+    {
+        int count = 0;
+        while(x)
+        {
+            count += 1;
+            x /= 10;
         }
+        return max(1, count);
+    }
+    long long int totalDigits(int n)
+    {
+        if(n == 0)
+            return 0;
+        int digNum = noOfDigits(n);
+        long long int i = 1, count = 0;
         
-        n--; // zero indexed
-
-        int num = pow(10, size-1) + n/size;
-        return to_string(num)[n%size] - '0';        
+        while(i < digNum)
+        {
+            count += i * (pow(10, i) - pow(10, i - 1));
+            i += 1;
+        }
+        count += (n - pow(10, i-1) + 1) * i;
+        return count;
+    }
+    int findNthDigit(int n) {
+        long long int i, r, d, x, low = 0, mid, high = n;
+        while(low <= high)
+        {
+            mid = low + (high-low)/2;
+            d = noOfDigits(mid);
+            x = totalDigits(mid-1);
+            
+            if(x >= n)
+                high = mid - 1;
+            else if(n > x && n <= x + d)
+            {
+                n = n - x;
+                break;
+            }
+            else
+                low = mid + 1;
+        }
+        for(i=0;i<=d-n;i++)
+        {
+            r = mid % 10;
+            mid /= 10;
+        }
+        return r;        
     }
 };
