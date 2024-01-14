0c837443690f8c788f0300a390c449b17a046e84
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2024-01-14 17:20:41 +0000

    [LeetCode Sync] Runtime - 163 ms (11.26%), Memory - 80.5 MB (100.00%)

--- my-folder/0883-car-fleet/solution.cpp
+++ my-folder/0883-car-fleet/solution.cpp
@@ -1,10 +1,25 @@
 class Solution {
 public:
-    int carFleet(int target, vector<int>& pos, vector<int>& speed) {
-        map<int, double> m;
-        for (int i = 0; i < pos.size(); i++) m[-pos[i]] = (double)(target - pos[i]) / speed[i];
-        int res = 0; double cur = 0;
-        for (auto it : m) if (it.second > cur) cur = it.second, res++;
-        return res;        
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        int n = position.size();
+        vector<pair<int,int>> pairs;
+        for(int i = 0; i < n; i++) {
+            pairs.push_back({position[i], speed[i]});
+        }
+        sort(pairs.begin(), pairs.end());
+        vector<float> time(n);
+        for(int i = 0; i < n; i++) {
+            time[i] = (float)(target - pairs[i].first) / pairs[i].second;
+        }
+
+        int fleets = n;
+        for(int i = n-1; i > 0; i--) {
+            if(time[i] >= time[i-1]) {
+                time[i-1] = time[i];
+                fleets--;
+            }
+        }
+
+        return fleets;
     }
 };
