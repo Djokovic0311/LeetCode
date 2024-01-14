6451af7b308f0d6f729efecc64b01b13ae4d5a22
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2024-01-14 00:37:51 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 6.5 MB (100.00%)

--- my-folder/1304-longest-happy-string/solution.cpp
+++ my-folder/1304-longest-happy-string/solution.cpp
@@ -1,14 +1,32 @@
 class Solution {
 public:
-    string longestDiverseString(int a, int b, int c, char aa = 'a', char bb = 'b', char cc = 'c') {
-        if (a < b)
-            return longestDiverseString(b, a, c, bb, aa, cc);
-        if (b < c)
-            return longestDiverseString(a, c, b, aa, cc, bb);
-        if (b == 0)
-            return string(min(2, a), aa);
-        auto use_a = min(2, a), use_b = a - use_a >= b ? 1 : 0; 
-        return string(use_a, aa) +  string(use_b, bb) + 
-            longestDiverseString(a - use_a, b - use_b, c, aa, bb, cc);
+    string longestDiverseString(int a, int b, int c) {
+        priority_queue<pair<int, char>>pq;
+        if(a) pq.push({a,'a'});
+        if(b) pq.push({b,'b'});
+        if(c) pq.push({c,'c'});
+        
+        string res;
+        while(!pq.empty()){
+            auto [f, ch] = pq.top();
+            pq.pop();
+            if(res.size() < 2 || ch != res.back() || (res.size() >= 2 and ch == res.back() and ch != res[(int)res.size() - 2])){
+                res.push_back(ch);
+                if(--f > 0){
+                    pq.push({f, ch});
+                }
+            }
+            else{
+                if(pq.empty()) break;
+                auto [fDash, chDash] = pq.top();
+                pq.pop();
+                res.push_back(chDash);
+                if(--fDash > 0){
+                    pq.push({fDash, chDash});
+                }
+                pq.push({f, ch});
+            }
+        }
+        return res;
     }
 };
