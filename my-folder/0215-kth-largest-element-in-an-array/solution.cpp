f611ed3d1e59e93d63756775ebfe70e7b039918f
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2024-01-15 05:06:52 +0000

    [LeetCode Sync] Runtime - 97 ms (5.78%), Memory - 57.9 MB (100.00%)

--- my-folder/0215-kth-largest-element-in-an-array/solution.cpp
+++ my-folder/0215-kth-largest-element-in-an-array/solution.cpp
@@ -1,29 +1,11 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-       return quickSelect(nums, k); 
-    }
-    int quickSelect(vector<int>& nums, int k) {
-        int pivot = nums[rand() % nums.size()];
-
-        vector<int> left, mid, right;
-        for (int num: nums) {
-            if (num > pivot) {
-                left.push_back(num);
-            } else if (num < pivot) {
-                right.push_back(num);
-            } else {
-                mid.push_back(num);
-            }
+        priority_queue<int,vector<int>,greater<int>> pq;
+        for(int i=0;i<nums.size();++i){
+            pq.push(nums[i]);
+            if(pq.size()>k) pq.pop();
         }
-        if (k <= left.size()) {
-            return quickSelect(left, k);
-        }
-        
-        if (left.size() + mid.size() < k) {
-            return quickSelect(right, k - left.size() - mid.size());
-        }
-        
-        return pivot;
+        return pq.top();         
     }
 };
