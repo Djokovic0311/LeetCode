ba44c7338e8ddfb27b49bb717d33324ad5dd6664
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2024-01-15 05:29:45 +0000

    [LeetCode Sync] Runtime - 3 ms (9.70%), Memory - 11.9 MB (100.00%)

--- my-folder/2210-find-target-indices-after-sorting-array/solution.cpp
+++ my-folder/2210-find-target-indices-after-sorting-array/solution.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     vector<int> targetIndices(vector<int>& nums, int target) {
-        int n = nums.size();
-        int lowcount = 0;
-        int targetcount = 0;
-        vector<int> res;
-        for(int i = 0; i < n; i++) {
-            if(nums[i] == target) targetcount++;
-            if(nums[i] < target) lowcount++;
+        int count = 0;
+        int small = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i] == target) count++;
+            if(nums[i] < target) small++;
         }
-        while(targetcount--) {
-            res.push_back(lowcount++);
+        vector<int> res;
+        for(int i = small; i < small+count; i++) {
+            res.push_back(i);
         }
         return res;
     }
