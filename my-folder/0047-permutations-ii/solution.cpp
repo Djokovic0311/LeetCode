94882f6df1845377f9f5b22e48c8bb9a804f2c1c
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2024-01-15 05:59:01 +0000

    [LeetCode Sync] Runtime - 6 ms (41.84%), Memory - 9.7 MB (100.00%)

--- my-folder/0047-permutations-ii/solution.cpp
+++ my-folder/0047-permutations-ii/solution.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    void generatePermutations(vector<int> nums, vector<vector<int>>& output, int idx) {
+    void dfs(vector<int> nums, vector<vector<int>>& output, int idx) {
         if(idx == nums.size()) {
             output.push_back(nums);
+            return;
         }
         for(int i = idx; i < nums.size(); i++) {
-            if(i!=idx && nums[i] == nums[idx]) continue;
+            if(i != idx && nums[i] == nums[idx]) continue;
             swap(nums[i], nums[idx]);
-            generatePermutations(nums, output, idx+1);
-            // swap(nums[i], nums[idx]);
+            dfs(nums, output, idx+1);
         }
     }
     vector<vector<int>> permuteUnique(vector<int>& nums) {
         vector<vector<int>> res;
         sort(begin(nums), end(nums));
-        generatePermutations(nums, res, 0);
-        return res;
+        dfs(nums, res, 0);
+        return res;        
     }
 };
