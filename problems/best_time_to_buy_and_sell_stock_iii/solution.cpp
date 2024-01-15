51c260e7f8d8ad1dafbe385d4ff2513412973f92
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2024-01-15 18:43:09 +0000

    Sync LeetCode submission - Best Time to Buy and Sell Stock III (cpp)

--- problems/best_time_to_buy_and_sell_stock_iii/solution.cpp
+++ problems/best_time_to_buy_and_sell_stock_iii/solution.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        if(!prices.size())
-            return 0;
-        int buy1    = INT_MAX;
-        int profit1 = INT_MIN;
-        int buy2    = INT_MAX;
-        int profit2 = INT_MIN;
-        for(int i = 0; i < prices.size(); i++){
-            buy1    = min(buy1, prices[i]);
-            profit1 = max(profit1, prices[i] - buy1);
-            buy2    = min(buy2, prices[i] - profit1);
-            profit2 = max(profit2, prices[i] - buy2);
+        int n = prices.size();
+        int minBuyPrice1 = INT_MAX, minBuyPrice2 = INT_MAX;
+        int maxProfit1 = INT_MIN, maxProfit2 = INT_MIN;
+        for(int i = 0; i < n; i++) {
+            minBuyPrice1 = min(minBuyPrice1, prices[i]);
+            maxProfit1 = max(maxProfit1, prices[i] - minBuyPrice1);
+            minBuyPrice2 = min(minBuyPrice2, prices[i] - maxProfit1);
+            maxProfit2 = max(maxProfit2, prices[i] - minBuyPrice2);
         }
-        return profit2;
+        return maxProfit2;
     }
 };
