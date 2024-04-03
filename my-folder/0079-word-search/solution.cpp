67f57fd6da5a8e309d711da543aa55bf8b64171f
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2024-04-03 04:36:40 +0000

    [LeetCode Sync] Runtime - 209 ms (72.83%), Memory - 9.5 MB (100.00%)

--- my-folder/0079-word-search/solution.cpp
+++ my-folder/0079-word-search/solution.cpp
@@ -4,7 +4,6 @@ public:
         bool status;
         int m = board.size(), n = board[0].size();
         if(idx == word.length()) {
-            cout << i << " " << j << endl;
             return true;
         }
         if(i < 0 || i >= board.size() || j < 0 || j >= board[0].size() || board[i][j] != word[idx])
@@ -24,8 +23,6 @@ public:
             for(int j = 0; j < n; j++) {
                 if(dfs(board,word,i,j,0)) return true;
             }
-
-                
         }
 
         return false;
