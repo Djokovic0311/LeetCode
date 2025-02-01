7431fe3ad681607a4182efcba4a487c9fd36a24f
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2025-02-01 00:33:31 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 27.6 MB (88.91%)

---
+++ my-folder/3429-special-array-i/solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isArraySpecial(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 1) return true;
+        for(int i = 1; i < n; i++) {
+            if((nums[i] ^ nums[i-1]) % 2 != 1) return false;
+        }
+        return true;
+    }
+};
