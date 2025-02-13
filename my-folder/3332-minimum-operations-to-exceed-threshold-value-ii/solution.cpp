4cafca6e2d972419fd0bdcd8592d344153505a2b
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2025-02-13 01:48:54 +0000

    [LeetCode Sync] Runtime - 84 ms (90.75%), Memory - 91.5 MB (77.02%)

---
+++ my-folder/3332-minimum-operations-to-exceed-threshold-value-ii/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        int result = 0;
+        priority_queue<long, vector<long>, greater<long>> min_heap(nums.begin(), nums.end());
+        while(min_heap.top() < k) {
+            long x = min_heap.top();
+            min_heap.pop();
+            long y = min_heap.top();
+            min_heap.pop();
+            min_heap.push(min(x, y) * 2 + max(x, y));
+            result++;           
+        }
+        return result;
+    }
+};
