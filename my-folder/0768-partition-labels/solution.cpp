d5bea99f8402be93039bc3fea71b8175eeb6f518
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2025-03-30 01:14:09 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.7 MB (96.46%)

--- my-folder/0768-partition-labels/solution.cpp
+++ my-folder/0768-partition-labels/solution.cpp
@@ -1,31 +1,26 @@
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
-        int n = s.size();
-        vector<int> ans;
-        
-        if(n == 0) return ans;
+        int last_pos[26] = {0};
+        int n = s.length();
+        vector<int> res;
+        int len = 0;
+        int min_pos = -1;
 
-        vector<int> last_pos (26, -1);
-        
-        for(int i=n-1; i>=0; --i) {
-            if(last_pos[s[i]-'a'] == -1)
-                last_pos[s[i]-'a'] = i;
+        for(int i = 0; i < n; i++) {
+            last_pos[s[i]-'a'] = i;
         }
-        int minp = -1, plen = 0;
         
-        for(int i=0; i<n; ++i) {
+        for(int i = 0; i < n; i++) {
             int lp = last_pos[s[i]-'a'];
-            minp = max(minp, lp);
-            ++plen;
-            
-            if(i == minp) {
-                ans.push_back(plen);
-                minp = -1;
-                plen = 0;
+            min_pos = max(min_pos, lp);
+            len++;
+            if(i == min_pos) {
+                res.push_back(len);
+                len = 0;
+                min_pos = -1;
             }
         }
-        
-        return ans;    
+        return res;
     }
 };
