1c25c7fe1933b4232b3bd08fdbf717f20a38d6e1
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2025-05-16 04:31:40 +0000

    [LeetCode Sync] Runtime - 37 ms (78.95%), Memory - 34.3 MB (94.12%)

--- my-folder/3142-longest-unequal-adjacent-groups-subsequence-ii/solution.cpp
+++ my-folder/3142-longest-unequal-adjacent-groups-subsequence-ii/solution.cpp
@@ -1,54 +1,43 @@
 class Solution {
 public:
-    int hammingDistance(const string& s1, const string& s2) {
-        int distance = 0;
-        for (int i = 0; i < s1.size(); ++i) {
-            if (s1[i] != s2[i]) {
-                distance++;
-            }
-        }
-        return distance;
-    }
-
-    vector<string> getWordsInLongestSubsequence(int n, vector<string>& words, vector<int>& groups) {
-        // Step 1: Compute pair validity
-        vector<vector<bool>> validPairs(n, vector<bool>(n, false));
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                if (i != j && 
-                    words[i].size() == words[j].size() && 
-                    groups[i] != groups[j] &&
-                    hammingDistance(words[i], words[j]) == 1) 
-                {
-                    validPairs[i][j] = true;
-                }
-            }
-        }
-
-        // Step 2: Dynamic Programming
-        vector<int> dp(n, 1), prev(n, -1);  // dp[i] = length of longest subsequence ending at i, prev[i] = previous index
-        int maxLenIdx = 0;  // Index of the maximum length subsequence
-        
+    vector<string> getWordsInLongestSubsequence(vector<string>& words,
+                                                vector<int>& groups) {
+        int n = groups.size();
+        vector<int> dp(n, 1);
+        vector<int> prev(n, -1);
+        int maxIndex = 0;
         for (int i = 1; i < n; i++) {
             for (int j = 0; j < i; j++) {
-                if (validPairs[j][i] && dp[i] < dp[j] + 1) {
+                if (check(words[i], words[j]) == 1 && dp[j] + 1 > dp[i] &&
+                    groups[i] != groups[j]) {
                     dp[i] = dp[j] + 1;
                     prev[i] = j;
                 }
             }
-            if (dp[i] > dp[maxLenIdx]) {
-                maxLenIdx = i;
+            if (dp[i] > dp[maxIndex]) {
+                maxIndex = i;
             }
         }
 
-        // Step 3: Backtrack to retrieve the words
-        vector<string> result;
-        for (int i = maxLenIdx; i >= 0; i = prev[i]) {
-            result.push_back(words[i]);
+        vector<string> ans;
+        for (int i = maxIndex; i >= 0; i = prev[i]) {
+            ans.emplace_back(words[i]);
         }
-        reverse(result.begin(), result.end());
-        
-        return result;
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+
+    bool check(string& s1, string& s2) {
+        if (s1.size() != s2.size()) {
+            return false;
+        }
+        int diff = 0;
+        for (int i = 0; i < s1.size(); i++) {
+            diff += s1[i] != s2[i];
+            if (diff > 1) {
+                return false;
+            }
+        }
+        return diff == 1;
     }
 };
-
